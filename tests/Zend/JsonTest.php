e203c4fbe010637e5c4c19fb73ad3339af51fdad
matthew <matthew@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2006-04-05 19:26:53 +0000

    * Revised (passing) tests
    git-svn-id: http://framework.zend.com/svn/framework/trunk@93 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/JsonTest.php
+++ tests/Zend/JsonTest.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package    Zend_Json
  * @subpackage UnitTests
@@ -25,42 +26,45 @@ class Zend_JsonTest extends PHPUnit2_Framework_TestCase
 {
 	public function testNull()
 	{
-		$this->_testEncodeDecode( array(null) );
+		$this->_testEncodeDecode(array(null));
 	}
 
 	
     public function testBoolean()
     {
-		$this->_testEncodeDecode( array(true,
-								 	    false) );
+        $this->assertTrue(Zend_Json::decode(Zend_Json::encode(true)));
+        $this->assertFalse(Zend_Json::decode(Zend_Json::encode(false)));
     }
 
 
 	public function testInteger()
 	{
-		$this->_testEncodeDecode( array(-2,
-			                             0,
-			                            -1) );
+		$this->_testEncodeDecode(array(-2));
+		$this->_testEncodeDecode(array(-1));
+
+        $zero = Zend_Json::decode(Zend_Json::encode(0));
+		$this->assertEquals(0, $zero, 'Failed 0 integer test. Encoded: ' . serialize(Zend_Json::encode(0)));
 	}
 
 
 	public function testFloat()
 	{
-		$this->_testEncodeDecode( array( -2.1,
-			                              1.2) );
+		$this->_testEncodeDecode(array(-2.1, 1.2));
 	}
 	
 	
 	public function testString()
 	{
-		$this->_testEncodeDecode( array( '',
-		                                 'string') );
+		$this->_testEncodeDecode(array('string'));
+		$this->assertEquals('', Zend_Json::decode(Zend_Json::encode('')), 'Empty string encoded: ' . serialize(Zend_Json::encode('')));
 	}
 	
 	
 	public function testArray()
 	{
-        $this->_testEncodeDecode(array(array(1, 'one', 2, 'two')));
+        $array = array(1, 'one', 2, 'two');
+        $encoded = Zend_Json::encode($array);
+        $this->assertSame($array, Zend_Json::decode($encoded), 'Decoded array does not match: ' . serialize($encoded));
 	}
 
     public function testAssocArray() 
@@ -75,20 +79,24 @@ class Zend_JsonTest extends PHPUnit2_Framework_TestCase
         $value->one = 1;
         $value->two = 2;
 
-        $array = array('one' => 1, 'two' => 2);
+        $array = array('__className' => 'stdClass', 'one' => 1, 'two' => 2);
 
         $encoded = Zend_Json::encode($value);
-        $this->assertEquals($array, Zend_Json::decode($array));
+        $this->assertSame($array, Zend_Json::decode($encoded));
 	}
 
     public function testObjectAsObject()
     {
-        $value = new StdClass();
+        $value = new stdClass();
         $value->one = 1;
         $value->two = 2;
 
         $encoded = Zend_Json::encode($value);
-        $this->assertEquals($array, Zend_Json::decode($array, Zend_Json::TYPE_OBJECT));
+        $decoded = Zend_Json::decode($encoded, Zend_Json::TYPE_OBJECT);
+        $this->assertTrue(is_object($decoded), 'Not decoded as an object');
+        $this->assertTrue($decoded instanceof StdClass, 'Not a StdClass object');
+        $this->assertTrue(isset($decoded->one), 'Expected property not set');
+        $this->assertEquals($value->one, $decoded->one, 'Unexpected value');
     }
 	
 	/**
