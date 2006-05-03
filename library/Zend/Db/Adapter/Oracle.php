5c54122119827702780d0338fcad446d325fd152
mike <mike@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2006-05-03 02:36:47 +0000

    Updated license.
    git-svn-id: http://framework.zend.com/svn/framework/trunk@339 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Db/Adapter/Oracle.php
+++ library/Zend/Db/Adapter/Oracle.php
@@ -4,23 +4,19 @@
  *
  * LICENSE
  *
- * This source file is subject to version 1.0 of the Zend Framework
- * license, that is bundled with this package in the file LICENSE, and
- * is available through the world-wide-web at the following URL:
- * http://www.zend.com/license/framework/1_0.txt. If you did not receive
- * a copy of the Zend Framework license and are unable to obtain it
- * through the world-wide-web, please send a note to license@zend.com
- * so we can mail you a copy immediately.
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
  *
  * @package    Zend_Db
  * @subpackage Adapter
- * @copyright  Copyright (c) 2005-2006 Zend Technologies USA Inc. (http://www.zend.com)
- * @license    http://www.zend.com/license/framework/1_0.txt Zend Framework License version 1.0
- *
- *
- * @todo XXX - Needs to be updated to use Zend_Db_Adapter_Abstract
- *
- */
+ * @copyright  Copyright (c) 2006 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */ 
 
 
 /** Zend_Db_Adapter_Abstract */
@@ -35,8 +31,9 @@ require_once 'Zend/Db/Statement/Oracle.php';
 
 /**
  * @package    Zend_Db
- * @copyright  Copyright (c) 2005-2006 Zend Technologies Inc. (http://www.zend.com)
- * @license    Zend Framework License version 1.0
+ * @subpackage Adapter
+ * @copyright  Copyright (c) 2006 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 class Zend_Db_Adapter_Oracle extends Zend_Db_Adapter_Abstract
 {
--- library/Zend/Db/Table/Exception.php
+++ library/Zend/Db/Table/Exception.php
@@ -1,24 +1,22 @@
 <?php
 /**
- *  +----------------------------------------------------------------------+
- *  | Zend Framework                                                       |
- *  +----------------------------------------------------------------------+
- *  | Copyright (c) 2005-2006 Zend Technologies Inc. (http://www.zend.com) |
- *  +----------------------------------------------------------------------+
- *  | This source file is subject to version 1.0 of the Zend Framework     |
- *  | license, that is bundled with this package in the file LICENSE, and  |
- *  | is available through the world-wide-web at the following url:        |
- *  | http://www.zend.com/license/framework/1_0.txt.                       |
- *  | If you did not receive a copy of the Zend license and are unable to  |
- *  | obtain it through the world-wide-web, please send a note to          |
- *  | license@zend.com so we can mail you a copy immediately.              |
- *  +----------------------------------------------------------------------+
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
  *
  * @package    Zend_Db
  * @subpackage Table
- * @copyright  Copyright (c) 2005-2006 Zend Technologies Inc. (http://www.zend.com)
- * @license    Zend Framework License version 1.0
- */
+ * @copyright  Copyright (c) 2006 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */ 
 
 
 /**
@@ -29,8 +27,8 @@ require_once 'Zend/Db/Exception.php';
 /**
  * @package    Zend_Db
  * @subpackage Table
- * @copyright  Copyright (c) 2005-2006 Zend Technologies Inc. (http://www.zend.com)
- * @license    Zend Framework License version 1.0
+ * @copyright  Copyright (c) 2006 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 class Zend_Db_Table_Exception extends Zend_Db_Exception
 {}
