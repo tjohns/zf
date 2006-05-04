6e7a8ba4b073174e830cf925fc73f098365ae1c7
mike <mike@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2006-05-04 00:33:04 +0000

    Updated license.
    git-svn-id: http://framework.zend.com/svn/framework/trunk@353 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Pdf.php
+++ library/Zend/Pdf.php
@@ -4,17 +4,17 @@
  *
  * LICENSE
  *
- * This source file is subject to version 1.0 of the Zend Framework
- * license, that is bundled with this package in the file LICENSE, and
- * is available through the world-wide-web at the following URL:
- * http://www.zend.com/license/framework/1_0.txt. If you did not receive
- * a copy of the Zend Framework license and are unable to obtain it
- * through the world-wide-web, please send a note to license@zend.com
- * so we can mail you a copy immediately.
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
  *
  * @package    Zend_Pdf
- * @copyright  Copyright (c) 2005-2006 Zend Technologies USA Inc. (http://www.zend.com)
- * @license    http://www.zend.com/license/framework/1_0.txt Zend Framework License version 1.0
+ * @copyright  Copyright (c) 2006 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 
 
@@ -78,6 +78,7 @@ require_once 'Zend/Pdf/Resource/Image/TIFF.php';
 /** Zend_Pdf_Image_PNG */
 require_once 'Zend/Pdf/Resource/Image/PNG.php';
 
+
 /**
  * General entity which describes PDF document.
  * It implements document abstraction with a document level operations.
@@ -89,8 +90,8 @@ require_once 'Zend/Pdf/Resource/Image/PNG.php';
  * document level actions, attachments, form object, etc)
  *
  * @package    Zend_Pdf
- * @copyright  Copyright (c) 2005-2006 Zend Technologies USA Inc. (http://www.zend.com)
- * @license    http://www.zend.com/license/framework/1_0.txt Zend Framework License version 1.0
+ * @copyright  Copyright (c) 2006 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 class Zend_Pdf
 {
