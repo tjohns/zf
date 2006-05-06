77dd9b28460c811aa3e6ea0d5622dde86a1fcdbd
fab <fab@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2006-05-06 16:43:51 +0000

    change license to new BSD
    git-svn-id: http://framework.zend.com/svn/framework/trunk@373 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- incubator/library/Zend/Cache/Backend/File.php
+++ incubator/library/Zend/Cache/Backend/File.php
@@ -5,18 +5,18 @@
  *
  * LICENSE
  *
- * This source file is subject to version 1.0 of the Zend Framework
- * license, that is bundled with this package in the file LICENSE, and
- * is available through the world-wide-web at the following URL:
- * http://www.zend.com/license/framework/1_0.txt. If you did not receive
- * a copy of the Zend Framework license and are unable to obtain it
- * through the world-wide-web, please send a note to license@zend.com
- * so we can mail you a copy immediately.
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
  *
  * @package    Zend_Cache
  * @subpackage Backend
  * @copyright  Copyright (c) 2006 Fabien MARTY, Mislav MAROHNIC
- * @license    http://www.zend.com/license/framework/1_0.txt Zend Framework License version 1.0
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 
  
@@ -30,7 +30,7 @@ require_once 'Zend/Cache/Backend/Interface.php';
  * @package    Zend_Cache
  * @subpackage Backend
  * @copyright  Copyright (c) 2006 Fabien MARTY, Mislav MAROHNIC
- * @license    http://www.zend.com/license/framework/1_0.txt Zend Framework License version 1.0
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 class Zend_Cache_Backend_File implements Zend_Cache_Backend_Interface 
 {
