77289cfaa2fa6299d0ae95b6e6fddfcbe85828ca
mike <mike@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2006-05-11 17:54:28 +0000

    added @category tags
    git-svn-id: http://framework.zend.com/svn/framework/trunk@419 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Http/Client/File.php
+++ library/Zend/Http/Client/File.php
@@ -12,6 +12,7 @@
  * obtain it through the world-wide-web, please send an email
  * to license@zend.com so we can send you a copy immediately.
  *
+ * @category   Zend
  * @package    Zend_Http
  * @subpackage Client
  * @copyright  Copyright (c) 2006 Zend Technologies USA Inc. (http://www.zend.com)
@@ -27,6 +28,7 @@ require_once 'Zend/Http/Client/Abstract.php';
  * HTTP client implementation that reads from files and fakes HTTP responses.
  * This may be useful for testing.
  *
+ * @category   Zend
  * @package    Zend_Http
  * @subpackage Client
  * @copyright  Copyright (c) 2006 Zend Technologies USA Inc. (http://www.zend.com)
--- library/Zend/Http/Exception.php
+++ library/Zend/Http/Exception.php
@@ -12,6 +12,7 @@
  * obtain it through the world-wide-web, please send an email
  * to license@zend.com so we can send you a copy immediately.
  *
+ * @category   Zend
  * @package    Zend_Http
  * @subpackage Client
  * @copyright  Copyright (c) 2006 Zend Technologies USA Inc. (http://www.zend.com)
@@ -24,6 +25,7 @@ require_once 'Zend/Exception.php';
 
 
 /**
+ * @category   Zend
  * @package    Zend_Http
  * @subpackage Client
  * @copyright  Copyright (c) 2006 Zend Technologies USA Inc. (http://www.zend.com)
