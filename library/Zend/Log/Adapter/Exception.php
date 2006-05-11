b6989b5ce76483947bb6d4527a40df0b4f3e81d8
mike <mike@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2006-05-11 17:57:10 +0000

    added @category tags
    git-svn-id: http://framework.zend.com/svn/framework/trunk@421 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Log/Adapter/Exception.php
+++ library/Zend/Log/Adapter/Exception.php
@@ -12,6 +12,7 @@
  * obtain it through the world-wide-web, please send an email
  * to license@zend.com so we can send you a copy immediately.
  *
+ * @category   Zend
  * @package    Zend_Log
  * @subpackage Adapter
  * @copyright  Copyright (c) 2006 Zend Technologies USA Inc. (http://www.zend.com)
@@ -26,6 +27,7 @@ require_once 'Zend/Log/Exception.php';
 
 
 /**
+ * @category   Zend
  * @package    Zend_Log
  * @subpackage Adapter
  * @copyright  Copyright (c) 2006 Zend Technologies USA Inc. (http://www.zend.com)
