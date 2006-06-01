9158056a6f45d7e5e591aa0b31d8547f573c2dfe
darby <darby@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2006-06-01 15:48:25 +0000

    added AllTests scripts to incubator tests tree; includes Zend_Config tests
    git-svn-id: http://framework.zend.com/svn/framework/trunk@532 44c647ce-9c0f-0410-b52a-842ac1e357ba

---
+++ incubator/tests/AllTests.php
@@ -0,0 +1,39 @@
+<?php
+if (!defined('PHPUnit2_MAIN_METHOD')) {
+    define('PHPUnit2_MAIN_METHOD', 'AllTests::main');
+}
+
+require_once 'PHPUnit2/Framework/TestSuite.php';
+require_once 'PHPUnit2/TextUI/TestRunner.php';
+
+/**
+ * Read in user-defined test configuration if available; otherwise, read default test configuration
+ */
+if (is_readable('TestConfiguration.php')) {
+    require_once 'TestConfiguration.php';
+} else {
+    require_once 'TestConfiguration.php.dist';
+}
+
+require_once 'Zend/AllTests.php';
+
+class AllTests
+{
+    public static function main()
+    {
+        PHPUnit2_TextUI_TestRunner::run(self::suite());
+    }
+
+    public static function suite()
+    {
+        $suite = new PHPUnit2_Framework_TestSuite('Zend Framework');
+
+        $suite->addTest(Zend_AllTests::suite());
+
+        return $suite;
+    }
+}
+
+if (PHPUnit2_MAIN_METHOD == 'AllTests::main') {
+    AllTests::main();
+}
---
+++ incubator/tests/Zend/AllTests.php
@@ -0,0 +1,32 @@
+<?php
+if (!defined('PHPUnit2_MAIN_METHOD')) {
+    define('PHPUnit2_MAIN_METHOD', 'Zend_AllTests::main');
+}
+
+require_once 'PHPUnit2/Framework/TestSuite.php';
+require_once 'PHPUnit2/TextUI/TestRunner.php';
+
+require_once 'Zend/ConfigTest.php';
+require_once 'Zend/Config/AllTests.php';
+
+class Zend_AllTests
+{
+    public static function main()
+    {
+        PHPUnit2_TextUI_TestRunner::run(self::suite());
+    }
+
+    public static function suite()
+    {
+        $suite = new PHPUnit2_Framework_TestSuite('Zend Framework - Zend');
+
+        $suite->addTestSuite('Zend_ConfigTest');
+        $suite->addTest(Zend_Config_AllTests::suite());
+
+        return $suite;
+    }
+}
+
+if (PHPUnit2_MAIN_METHOD == 'Zend_AllTests::main') {
+    Zend_AllTests::main();
+}
