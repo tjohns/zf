619d8f62237746dae77aa7a1159a52e0d4a67f49
sebastian <sebastian@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2006-06-14 06:05:48 +0000

    Some filtering applied.
    git-svn-id: http://framework.zend.com/svn/framework/trunk@635 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/View/_stubs/HelperDir1/StubEmpty.php
+++ tests/Zend/View/_stubs/HelperDir1/StubEmpty.php
@@ -1,3 +1,6 @@
 <?php
+require_once 'PHPUnit2/Util/Filter.php';
+
+PHPUnit2_Util_Filter::addFileToFilter(__FILE__);
 
 // used to test when helper file is loaded but class is not within
--- tests/Zend/View/_stubs/HelperDir2/Stub2.php
+++ tests/Zend/View/_stubs/HelperDir2/Stub2.php
@@ -1,4 +1,7 @@
 <?php
+require_once 'PHPUnit2/Util/Filter.php';
+
+PHPUnit2_Util_Filter::addFileToFilter(__FILE__);
 
 class Zend_View_Helper_Stub2 
 {
