a3a31c216377f18c6ddc88b836b9613d1e835644
fab <fab@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2006-08-25 16:34:32 +0000

    better code coverage
    git-svn-id: http://framework.zend.com/svn/framework/trunk@997 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/Cache/MemcachedBackendTest.php
+++ tests/Zend/Cache/MemcachedBackendTest.php
@@ -60,12 +60,47 @@ class Zend_Cache_MemcachedBackendTest extends Zend_Cache_CommonBackendTest {
         $test = new Zend_Cache_Backend_Memcached(); 
     }
     
+    public function testCleanModeOld() 
+    {
+        $this->_instance->clean('old');
+        // do nothing, just to see if an error occured
+    }
+    
+    public function testCleanModeMatchingTags() 
+    {
+        $this->_instance->clean('matchingTag', array('tag1'));
+        // do nothing, just to see if an error occured
+    }
+    
+    public function testCleanModeNotMatchingTags() 
+    {
+        $this->_instance->clean('notMatchingTag', array('tag1'));
+        // do nothing, just to see if an error occured
+    }
+    
+    public function testGetWithCompression() 
+    {
+        $this->_instance->setOption('compression', true);
+        $this->testGetWithAnExistingCacheIdAndUTFCharacters();
+    }
+    
+    public function testConstructorWithAnAlternativeSyntax()
+    {
+        $server = array(
+            'host' => TESTS_ZEND_CACHE_MEMCACHED_HOST,
+            'port' => TESTS_ZEND_CACHE_MEMCACHED_PORT,
+            'persistent' => TESTS_ZEND_CACHE_MEMCACHED_PERSISTENT
+        );
+        $options = array(
+            'servers' => $server
+        );
+        $this->_instance = new Zend_Cache_Backend_Memcached($options);
+        $this->testGetWithAnExistingCacheIdAndUTFCharacters();
+    }
+    
     // Because of limitations of this backend...
-    public function testCleanModeOld() {}
     public function testGetWithAnExpiredCacheId() {}
-    public function testCleanModeMatchingTags() {}
     public function testCleanModeMatchingTags2() {}
-    public function testCleanModeNotMatchingTags() {}
     public function testCleanModeNotMatchingTags2() {}
     public function testCleanModeNotMatchingTags3() {}
     
