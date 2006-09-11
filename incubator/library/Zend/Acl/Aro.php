e6ed62219a279bade5efbaf06941310f02eea5de
peptolab <peptolab@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2006-09-11 23:09:00 +0000

    Initial setup for new component: Zend_Acl
    git-svn-id: http://framework.zend.com/svn/framework/trunk@1053 44c647ce-9c0f-0410-b52a-842ac1e357ba

---
+++ incubator/library/Zend/Acl/Aro.php
@@ -0,0 +1,160 @@
+<?php
+
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Acl
+ * @copyright  Copyright (c) 2006 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */ 
+
+
+/**
+ * @category   Zend
+ * @package    Zend_Acl
+ * @copyright  Copyright (c) 2006 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+class Zend_Acl_Aro
+{
+    /**
+     * Unique id of ARO
+     * @var string
+     */
+    protected $_id;
+
+    /**
+     * Collection of parents
+     * @var array
+     */
+    protected $_parent = array();
+
+    /**
+     * Class constructor
+     *
+     * If $inherit contains a string or array of values, then iterate through
+     * the parents, retrieving their ids and store in LIFO order. The first
+     * element of the $_parent array will always refer to the current object
+     *
+     * @param string $id
+     * @param mixed $inherit
+     * @return void
+     */
+    public function __construct($id, $inherit = null)
+    {
+        $this->_id = $id;
+        $this->_addParent($this);
+
+        if (!is_null($inherit)) {
+            if (!is_array($inherit)) {
+                $inherit = array($inherit);
+            }
+            
+            foreach ($inherit as $parent) {
+    
+                if (!($parent instanceof self)) {
+                    $parent = Zend_Acl_Aro_Registry::getInstance()->find($parent);
+                }
+    
+                foreach($parent->getParent() as $aro) {
+                    $this->_addParent($aro);
+                }
+                $this->_addParent($parent);
+            }
+        }
+        array_push($this->_parent, Zend_Acl::ARO_DEFAULT);
+    }
+
+    /**
+     * Retrieves ARO id
+     *
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->_id;
+    }
+
+    /**
+     * Retrieves ARO parent ids
+     *
+     * @return array
+     */
+    public function getParent()
+    {
+        return $this->_parent;
+    }
+
+    /**
+     * Performs a validation on an ACO using the current ARO
+     *
+     * @return boolean
+     */
+    public function canAccess(Zend_Acl $aco, $context = null, $path = null)
+    {
+        return $aco->valid($this, $context, $path);
+    }
+
+    /**
+     * Returns an ACO tree that the current ARO has access to
+     *
+     * This function will determine which AROs - from either a list of AROs or
+     * the entire ARO registry - have access to the current ARO.
+     *
+     * AROs can be supplied either an an ARO object, an array of ARO objects,
+     * a string id or an array of string ids. If the ARO parameter is left empty
+     * then the ARO registry is used to return all members.
+     *
+     * To allow fine-grain control, a specific context can also be used to
+     * validate the AROs
+     *
+     * An array of ARO objects is returned upon success or empty
+     *
+     * @param mixed $aro
+     * @param string $context
+     * @return array
+     */
+    public function getValidAco(Zend_Acl $aco, $context = null)
+    {
+        return $this->_getValidAco(clone $aco, $context);
+    }
+    
+    protected function _getValidAco(Zend_Acl $aco, $context)
+    {
+        foreach($aco->getChildren() as $node) {
+            if (!$this->canAccess($node, $context)) {
+                $aco->remove($node->getPath());
+            }
+        }
+        return $aco;
+    }
+
+    /**
+     * Add parent to current ARO
+     *
+     * @return Zend_Acl_Aro
+     */
+    protected function _addParent($parent)
+    {
+        if (!($parent instanceof self)) {
+            $parent = Zend_Acl_Aro_Registry::getInstance()->find($parent);
+        }
+
+        if (!in_array($parent->getId(), $this->_parent)) {
+            array_push($this->_parent, $parent->getId());
+        }
+        
+        return $parent;
+    }
+}
---
+++ incubator/library/Zend/Acl/Aro/Registry.php
@@ -0,0 +1,157 @@
+<?php
+
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Acl
+ * @copyright  Copyright (c) 2006 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */ 
+
+
+/**
+ * @category   Zend
+ * @package    Zend_Acl
+ * @copyright  Copyright (c) 2006 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+class Zend_Acl_Aro_Registry
+{
+    /**
+     * ARO registry
+     * @var array
+     */
+    protected $_aro;
+    
+    /**
+     * Parent Aco
+     * @var Zend_Acl
+     */
+    protected $_aco;
+    
+    /**
+     * Registry instance
+     * @var array
+     */
+    static protected $_instance;
+    
+    /**
+     * Instance of Zend_Acl_Aro_Registry 
+     * @param Zend_Acl_Aro $group
+     */
+    static public function getInstance()
+    {
+        if (is_null(self::$_instance)) {
+            self::$_instance = new self;
+        }
+        return self::$_instance;
+    }
+
+    /**
+     * Class constructor
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->add(new Zend_Acl_Aro('_default'));
+    }
+
+    /**
+     * Public access to ARO registry
+     *
+     * @return void
+     */
+    public function __get($aro)
+    {
+        return $this->find($aro);
+    }
+
+    /**
+     * Add unique ARO to registry 
+     *
+     * @param Zend_Acl_Aro $aro
+     * @return Zend_Acl_Aro_Registry for fluent interface
+     */
+    public function add($aro, $inherit = null)
+    {
+        if (!($aro instanceof Zend_Acl_Aro)) {
+            $aro = new Zend_Acl_Aro($aro, $inherit);
+        }
+
+        $this->_aro[$aro->getId()] = $aro;
+        return $this;
+    }
+
+    /**
+     * Remove ARO from registry 
+     *
+     * @param Zend_Acl_Aro $aro
+     * @return boolean
+     */
+    public function remove($aro, $aco)
+    {
+        $id = $this->find($aro)->getId();
+
+        if ($id == Zend_Acl::ARO_DEFAULT) {
+            return false;
+        }
+
+        $children = array($id => $id);
+
+        foreach($this->_aro as $aro) {
+            if (in_array($id, $aro->getParent())) {
+                $children[$aro->getId()] =  $aro->getId();
+                $aco->removeAro($aro, $aco);
+            }
+        }
+        
+        foreach($children as $aro) {
+            unset($this->_aro[$aro]);
+        }
+
+        return true;
+    }
+
+    /**
+     * FInd group in registry 
+     *
+     * If the named group does not exist, the default ARO is returned
+     *
+     * @param string $aro
+     * @return Zend_Acl_Aro
+     */
+    public function find($aro)
+    {
+        if ($aro instanceof Zend_Acl_Aro) {
+            $aro = $aro->getId();
+           }
+
+        if (isset($this->_aro[$aro])) {
+            return $this->_aro[$aro];
+        } else {
+            return $this->_aro[Zend_Acl::ARO_DEFAULT];
+        }
+    }
+
+    /**
+     * Return registry as an array of ARO objects 
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return $this->_aro;
+    }
+}
