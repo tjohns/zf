6d2b0a3ee1051a23504a3798c6b86bbce7a428df
nico <nico@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2006-10-11 14:54:09 +0000

    added Maildir to testsuite, but made test optional as maildir can't work on Windows, because of the colons in filename issue
    git-svn-id: http://framework.zend.com/svn/framework/trunk@1239 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- incubator/tests/Zend/Mail/AllTests.php
+++ incubator/tests/Zend/Mail/AllTests.php
@@ -15,6 +15,7 @@ require_once 'PHPUnit/Framework/TestSuite.php';
 require_once 'PHPUnit/TextUI/TestRunner.php';
 
 require_once 'Zend/Mail/MboxTest.php';
+require_once 'Zend/Mail/MaildirTest.php';
 require_once 'Zend/Mail/Pop3Test.php';
 require_once 'Zend/Mail/InterfaceTest.php';
 
@@ -33,6 +34,15 @@ class Zend_Mail_AllTests
         $suite->addTestSuite('Zend_Mail_MboxTest');
         if(TESTS_ZEND_MAIL_POP3_ENABLED) {
             $suite->addTestSuite('Zend_Mail_Pop3Test');
+        }
+        if(TESTS_ZEND_MAIL_MAILDIR_ENABLED) {
+            if(file_exists(dirname(__FILE__) . '/_files/test.maildir/cur/messages.tar')) {
+                // TODO: I couldn't find a method to add an error or warning in TestSuite. But
+                // I also don't like throwing this simple exception and make the whole suite fail.
+                throw new Exception('You have to unpack and remove messages.tag in the maildir '. 
+                                    'directory before enabling the maildir test');
+            }
+            $suite->addTestSuite('Zend_Mail_MaildirTest');
         }
         
         return $suite;
