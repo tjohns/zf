cf24dd06093d1fbab4280a55863f95e49073f3be
matthew <matthew@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2006-10-11 19:11:51 +0000

    * Modified Router Interface to follow MVC proposal
    git-svn-id: http://framework.zend.com/svn/framework/trunk@1247 44c647ce-9c0f-0410-b52a-842ac1e357ba

---
+++ incubator/library/Zend/Controller/Router/Interface.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @package    Zend_Controller
+ * @subpackage Router
+ * @copyright  Copyright (c) 2006 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */ 
+
+
+/** Zend_Controller_Router_Exception */
+require_once 'Zend/Controller/Router/Exception.php';
+
+/** Zend_Controller_Request_Interface */
+require_once 'Zend/Controller/Request/Interface.php';
+
+/**
+ * @package    Zend_Controller
+ * @subpackage Router
+ * @copyright  Copyright (c) 2006 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+interface Zend_Controller_Router_Interface
+{
+    /**
+     * Processes a request and sets its controller and action.  If
+     * no route was possible, an exception is thrown.
+     *
+	 * @param  Zend_Controller_Request_Interface
+     * @throws Zend_Controller_Router_Exception
+     * @return Zend_Controller_Request_Interface|boolean
+     */
+    public function route(Zend_Controller_Request_Interface $dispatcher);
+}
