709c0d073d4f4ea7cf04efc712edc885f5574a39
matthew <matthew@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2006-11-02 07:53:15 +0000

    * Specify UTF-8 as XML charset (addresses [ZF-466])
    git-svn-id: http://framework.zend.com/svn/framework/trunk@1435 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/XmlRpc/Client.php
+++ library/Zend/XmlRpc/Client.php
@@ -368,7 +368,7 @@ class Zend_XmlRpc_Client
      */
     protected function _buildRequest($method, $args = null)
     {
-        $dom = new DOMDocument('1.0', 'ISO-8859-1');
+        $dom = new DOMDocument('1.0', 'UTF-8');
         $mCall = $dom->appendChild($dom->createElement('methodCall'));
         $mName = $mCall->appendChild($dom->createElement('methodName', $method));
 
