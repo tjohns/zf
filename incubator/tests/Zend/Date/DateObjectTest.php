0bf40e80783b2bd89533b96b0a47c9b3ea5269a1
thomas <thomas@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2006-11-15 07:54:42 +0000

    Zend_Date:  - added test suite for subclasses
    git-svn-id: http://framework.zend.com/svn/framework/trunk@1621 44c647ce-9c0f-0410-b52a-842ac1e357ba

---
+++ incubator/tests/Zend/Date/DateObjectTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package    Zend_Date
+ * @subpackage UnitTests
+ */
+
+
+/**
+ * Zend_Date
+ */
+require_once 'Zend/Date/DateObject.php';
+
+/**
+ * PHPUnit test case
+ */
+require_once 'PHPUnit/Framework/TestCase.php';
+
+
+/**
+ * @package    Zend_Date
+ * @subpackage UnitTests
+ */
+class Zend_Date_DateObjectTest extends PHPUnit_Framework_TestCase
+{
+}
---
+++ incubator/tests/Zend/DateTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package    Zend_Date
+ * @subpackage UnitTests
+ */
+
+
+/**
+ * Zend_Date
+ */
+require_once 'Zend.php';
+Zend::loadClass('Zend_Date');
+
+/**
+ * PHPUnit test case
+ */
+require_once 'PHPUnit/Framework/TestCase.php';
+
+
+/**
+ * @package    Zend_Date
+ * @subpackage UnitTests
+ */
+class Zend_DateTest extends PHPUnit_Framework_TestCase
+{
+}
