a78109afa7db021c495f5378d421a65198eefd01
thomas <thomas@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2006-11-20 22:26:35 +0000

    Zend_Date:  - new tests added
    git-svn-id: http://framework.zend.com/svn/framework/trunk@1790 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- incubator/tests/Zend/Date/DateObjectTest.php
+++ incubator/tests/Zend/Date/DateObjectTest.php
@@ -62,4 +62,56 @@ class Zend_Date_DateObjectTest extends PHPUnit_Framework_TestCase
             return true;
         }
     }
+
+	/**
+	 * Test for setTimestamp
+	 */
+    public function testSetTimestamp()
+    {
+      	$date = new Zend_Date_DateObject();
+       	$this->assertTrue($date->setTimestamp(0));
+    }
+
+	/**
+	 * Test for setTimestampLong
+	 */
+    public function testSetTimestampLong()
+    {
+      	$date = new Zend_Date_DateObject();
+       	$this->assertTrue($date->setTimestamp("12345678901234567890"));
+    }
+
+	/**
+	 * Test for setTimestampFailed
+	 */
+    public function testSetTimestampFailed()
+    {
+        try {
+        	$date = new Zend_Date_DateObject();
+        	$date->setTimestamp("notimestamp");
+        	$this->assertFalse($date instanceof Zend_Date_DateObject);
+        } catch (Exception $e) {
+            return true;
+        }
+    }
+
+	/**
+	 * Test for getTimestamp
+	 */
+    public function testGetTimestamp()
+    {
+      	$date = new Zend_Date_DateObject();
+      	$result = $date->getTimestamp();
+       	$this->assertSame($result, time());
+    }
+    
+	/**
+	 * Test for mktime
+	 */
+    public function testMkTime()
+    {
+      	$date = new Zend_Date_DateObject();
+      	$result = $date->mktime(0,0,0);
+       	$this->assertTrue($result < time());
+    }
 }
