b3b626fd2153dcc5a5bf68969e127d0b276f4a6c
matthew <matthew@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2006-11-28 15:54:10 +0000

    * Reverted previous patch; caused regression tests to fail
    git-svn-id: http://framework.zend.com/svn/framework/trunk@1895 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- incubator/library/Zend/Controller/Request/Http.php
+++ incubator/library/Zend/Controller/Request/Http.php
@@ -462,7 +462,7 @@ class Zend_Controller_Request_Http extends Zend_Controller_Request_Abstract
             } 
              
             if ((null !== $baseUrl)
-                && (false === ($pathInfo = substr($requestUri, strlen($baseUrl) - 1)))) 
+                && (false === ($pathInfo = substr($requestUri, strlen($baseUrl))))) 
             { 
                 // If substr() returns false then PATH_INFO is set to an empty string 
                 $pathInfo = ''; 
