0fa3ae1a268b7b3f7c97d7c95d32702473f4470b
bkarwin <bkarwin@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2006-12-11 01:33:46 +0000

    Adding non-authenticated tests to Base and Calendar.  Add copyright header to other Gdata test files.
    git-svn-id: http://framework.zend.com/svn/framework/trunk@2237 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- incubator/tests/Zend/Gdata/BaseTest.php
+++ incubator/tests/Zend/Gdata/BaseTest.php
@@ -1,13 +1,24 @@
 <?php
 /**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
  * @package    Zend_Gdata
  * @subpackage UnitTests
+ * @copyright  Copyright (c) 2006 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 
-
-/**
- * Zend_Gdata
- */
 require_once 'Zend/Gdata/Base.php';
 require_once 'Zend/Http/Client.php';
 
@@ -23,43 +34,173 @@ class Zend_Gdata_BaseTest extends PHPUnit_Framework_TestCase
         $this->gdata = new Zend_Gdata_Base(new Zend_Http_Client());
     }
 
-    public function testQuery()
+    public function testQueryParam()
     {
-        // echo "\n----\n";
-        $this->gdata->query = 'digital camera';
+        $this->gdata->resetParameters();
+        $query = 'digital camera';
+        $this->gdata->setQuery($query);
+        $this->assertTrue(isset($this->gdata->query));
+        $this->assertEquals($query, $this->gdata->getQuery());
         $feed = $this->gdata->getFeed();
         foreach ($feed as $feedEntry) {
             $linkList = $feedEntry->link();
             $href = $linkList[0]->getAttribute('href');
-            // echo "Feed: " . substr($feedEntry->title(), 0, 60) . "\n  <" . substr($href, 0, 60) . ">\n";
+            $this->assertRegExp('|http://.*|', $href);
+        }
+        unset($this->gdata->query);
+        $this->assertFalse(isset($this->gdata->query));
+    }
+
+    public function testAltRssParam()
+    {
+        $this->gdata->resetParameters();
+        $query = 'digital camera';
+        $alt = 'rss';
+        $this->gdata->setQuery($query);
+        $this->gdata->setAlt($alt);
+        $this->assertTrue(isset($this->gdata->alt));
+        $this->assertEquals($alt, $this->gdata->getAlt());
+        $channel = $this->gdata->getFeed();
+        foreach ($channel as $item) {
+            $link = $item->link();
+            $this->assertRegExp('|http://.*|', $link);
+        }
+        unset($this->gdata->alt);
+        $this->assertFalse(isset($this->gdata->alt));
+    }
+
+    public function testCategoryParam()
+    {
+        $this->gdata->resetParameters();
+        $query = 'nikon';
+        $category = 'camera';
+        $this->gdata->setQuery($query);
+        $this->gdata->setCategory($category);
+        $this->assertTrue(isset($this->gdata->category));
+        $this->assertEquals($category, $this->gdata->getCategory());
+        $feed = $this->gdata->getFeed();
+        foreach ($feed as $feedEntry) {
+            $linkList = $feedEntry->link();
+            $href = $linkList[0]->getAttribute('href');
+            $this->assertRegExp('|http://.*|', $href);
+        }
+        unset($this->gdata->category);
+        $this->assertFalse(isset($this->gdata->category));
+    }
+
+    public function testMaxResultsParam()
+    {
+        $this->gdata->resetParameters();
+        $query = 'digital camera';
+        $max = 3;
+        $this->gdata->setQuery($query);
+        $this->gdata->setMaxResults($max);
+        $this->assertTrue(isset($this->gdata->maxResults));
+        $this->assertEquals($max, $this->gdata->getMaxResults());
+        $feed = $this->gdata->getFeed();
+        $this->assertEquals($max, $feed->count());
+        foreach ($feed as $feedEntry) {
+            $linkList = $feedEntry->link();
+            $href = $linkList[0]->getAttribute('href');
+            $this->assertRegExp('|http://.*|', $href);
+        }
+        unset($this->gdata->maxResults);
+        $this->assertFalse(isset($this->gdata->maxResults));
+    }
+
+    public function testStartIndexParam()
+    {
+        $this->gdata->resetParameters();
+        $query = 'digital camera';
+        $start = 3;
+        $this->gdata->setQuery($query);
+        $this->gdata->setStartIndex($start);
+        $this->assertTrue(isset($this->gdata->startIndex));
+        $this->assertEquals($start, $this->gdata->getStartIndex());
+        $feed = $this->gdata->getFeed();
+        foreach ($feed as $feedEntry) {
+            $linkList = $feedEntry->link();
+            $href = $linkList[0]->getAttribute('href');
+            $this->assertRegExp('|http://.*|', $href);
+        }
+        unset($this->gdata->startIndex);
+        $this->assertFalse(isset($this->gdata->startIndex));
+    }
+
+    public function testMetadataItemTypes()
+    {
+        $locale = 'en_US';
+        $itemType = 'jobs';
+
+        // get all item types
+        $this->gdata->resetParameters();
+        $feed = $this->gdata->getItemTypeFeed($locale);
+        $titleArray = array();
+        foreach ($feed as $feedEntry) {
+            $titleArray[] = $feedEntry->title();
+        }
+        $this->assertContains($itemType, $titleArray);
+
+        // get info for specific item type
+        $this->gdata->resetParameters();
+        $feed = $this->gdata->getItemTypeFeed($locale, $itemType);
+        foreach ($feed as $feedEntry) {
+            $gm = 'gm:item_type';
+            $this->assertEquals($itemType, $feedEntry->$gm());
         }
     }
 
-    public function testMaxResults()
+    public function testMetadataStatistics()
     {
-        // echo "\n----\n";
-        $this->gdata->query = 'digital camera';
-        $this->gdata->maxResults = 3;
-        $feed = $this->gdata->getFeed();
+        $itemType = 'jobs';
+        // get statistics for specific item type
+        $this->gdata->resetParameters();
+        $feed = $this->gdata->getItemTypeAttributesFeed($itemType);
         foreach ($feed as $feedEntry) {
-            $linkList = $feedEntry->link();
-            $href = $linkList[0]->getAttribute('href');
-            // echo "Feed: " . substr($feedEntry->title(), 0, 60) . "\n  <" . substr($href, 0, 60) . ">\n";
+            $gm = 'gm:attribute';
+            $elt = $feedEntry->$gm;
+            $count = $elt->offsetGet('count');
+            $this->assertTrue(intval($count) >= 0);
         }
     }
 
-    public function testCategory()
+    public function testAddAttributeQuery()
     {
-        // echo "\n----\n";
-        $this->gdata->query = 'nikon';
-        $this->gdata->category = 'camera';
-        $this->gdata->maxResults = 3;
+        $this->gdata->resetParameters();
+        $query = 'digital camera';
+        $attrib = 'price';
+        $op = '<';
+        $attribValue = '50 USD';
+        $this->gdata->setQuery('digital camera');
+        $this->gdata->addAttributeQuery($attrib, $attribValue, $op);
+        $this->gdata->maxResults = 25;
         $feed = $this->gdata->getFeed();
         foreach ($feed as $feedEntry) {
-            $linkList = $feedEntry->link();
-            $href = $linkList[0]->getAttribute('href');
-            // echo "Feed: " . substr($feedEntry->title(), 0, 60) . "\n  <" . substr($href, 0, 60) . ">\n";
+            $g = 'g:price';
+            $this->assertThat(intval($feedEntry->$g()), $this->lessThan(intval($attribValue)));
         }
+
+        $this->gdata->unsetAttributeQuery($attrib);
+        $feed = $this->gdata->getFeed();
+        $prices = array();
+        foreach ($feed as $feedEntry) {
+            $g = 'g:price';
+            $price = intval($feedEntry->$g());
+            if ($price >= intval($attribValue)) {
+                $prices[] = $price;
+            }
+        }
+        $this->assertThat(count($prices), $this->greaterThan(0));
+
+        $this->gdata->unsetAttributeQuery();
+    }
+
+    public function testUnsetAttributeQuery()
+    {
+    }
+
+    public function testMetadataQuery()
+    {
     }
 
 }
--- incubator/tests/Zend/Gdata/CalendarTest.php
+++ incubator/tests/Zend/Gdata/CalendarTest.php
@@ -1,16 +1,27 @@
 <?php
 /**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
  * @package    Zend_Gdata
  * @subpackage UnitTests
+ * @copyright  Copyright (c) 2006 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 
-
-/**
- * Zend_Gdata
- */
 require_once 'Zend/Gdata/Calendar.php';
 require_once 'Zend/Http/Client.php';
-
+// require_once 'XML/Beautifier.php';
 
 /**
  * @package Zend_Gdata
@@ -18,15 +29,222 @@ require_once 'Zend/Http/Client.php';
  */
 class Zend_Gdata_CalendarTest extends PHPUnit_Framework_TestCase
 {
+    const ZEND_CONFERENCE_GROUP = 'ogr93sav88fmf2ssnv851osqm4@group.calendar.google.com';
+    const ZEND_CONFERENCE_EVENT_PIRATE_RECEPTION = 'turhun9osc2ajfoie57gb7tcs4';
 
     public function setUp()
     {
         $this->gdata = new Zend_Gdata_Calendar(new Zend_Http_Client());
+        // $this->xml = new XML_Beautifier();
     }
 
-    public function testCalendar()
+    public function testUpdatedMinParam()
     {
-        $this->assertTrue(true);
+        $updatedMin = '2006-10-27T11:03';
+        $this->gdata->resetParameters();
+        $this->gdata->setUser(self::ZEND_CONFERENCE_GROUP);
+        $this->gdata->setUpdatedMin($updatedMin);
+        $this->assertTrue(isset($this->gdata->updatedMin));
+        $this->assertEquals($this->gdata->formatTimestamp($updatedMin), $this->gdata->getUpdatedMin());
+        $feed = $this->gdata->getFeed();
+        $this->assertEquals(3, $feed->count());
+        unset($this->gdata->updatedMin);
+        $this->assertFalse(isset($this->gdata->updatedMin));
     }
+
+    public function testUpdatedMaxParam()
+    {
+        $updatedMax = '2006-10-27T11:02';
+        $this->gdata->resetParameters();
+        $this->gdata->setUser(self::ZEND_CONFERENCE_GROUP);
+        $this->gdata->setUpdatedMax($updatedMax);
+        $this->assertTrue(isset($this->gdata->updatedMax));
+        $this->assertEquals($this->gdata->formatTimestamp($updatedMax), $this->gdata->getUpdatedMax());
+        $feed = $this->gdata->getFeed();
+        $this->assertEquals(1, $feed->count());
+        unset($this->gdata->updatedMax);
+        $this->assertFalse(isset($this->gdata->updatedMax));
+    }
+
+    /*
+     * @todo: Calendar does not seem to recognize the published-min parameter
+     *
+    public function testPublishedMinParam()
+    {
+        $publishedMin = '2006-10-27T11:03';
+        $this->gdata->resetParameters();
+        $this->gdata->setUser(self::ZEND_CONFERENCE_GROUP);
+        $this->gdata->setPublishedMin($publishedMin);
+        $this->assertTrue(isset($this->gdata->publishedMin));
+        $this->assertEquals($this->gdata->formatTimestamp($publishedMin), $this->gdata->getPublishedMin());
+        $feed = $this->gdata->getFeed();
+        $this->assertEquals(3, $feed->count());
+        foreach ($feed as $feedItem) {
+            // echo $this->xml->formatString($feedItem->saveXML());
+        }
+        unset($this->gdata->publishedMin);
+        $this->assertFalse(isset($this->gdata->publishedMin));
+    }
+
+    public function testPublishedMaxParam()
+    {
+        $publishedMax = '2006-10-27T11:02';
+        $this->gdata->resetParameters();
+        $this->gdata->setUser(self::ZEND_CONFERENCE_GROUP);
+        // $this->gdata->setPublishedMax($publishedMax);
+        // $this->assertTrue(isset($this->gdata->publishedMax));
+        // $this->assertEquals($this->gdata->formatTimestamp($publishedMax), $this->gdata->getPublishedMax());
+        $feed = $this->gdata->getFeed();
+        // $this->assertEquals(1, $feed->count());
+        foreach ($feed as $feedItem) {
+            // echo $this->xml->formatString($feedItem->saveXML());
+        }
+        unset($this->gdata->publishedMax);
+        $this->assertFalse(isset($this->gdata->publishedMax));
+    }
+     */
+
+    public function testUserParam()
+    {
+        $this->gdata->resetParameters();
+        $this->gdata->setUser(self::ZEND_CONFERENCE_GROUP);
+        $this->assertTrue(isset($this->gdata->user));
+        $this->assertEquals(self::ZEND_CONFERENCE_GROUP, $this->gdata->getUser());
+        $feed = $this->gdata->getFeed();
+        $this->assertEquals(6, $feed->count());
+        unset($this->gdata->user);
+        $this->assertFalse(isset($this->gdata->user));
+    }
+
+    public function testStartMinParam()
+    {
+        $this->gdata->resetParameters();
+        $startMin = '2006-10-31';
+        $this->gdata->setUser(self::ZEND_CONFERENCE_GROUP);
+        $this->gdata->setStartMin($startMin);
+        $this->assertTrue(isset($this->gdata->startMin));
+        $this->assertEquals($this->gdata->formatTimestamp($startMin), $this->gdata->getStartMin());
+        $feed = $this->gdata->getFeed();
+        $this->assertEquals(6, $feed->count());
+        unset($this->gdata->startMin);
+        $this->assertFalse(isset($this->gdata->startMin));
+        unset($this->gdata->user);
+        $this->assertFalse(isset($this->gdata->user));
+    }
+
+    public function testStartMaxParam()
+    {
+        $this->gdata->resetParameters();
+        $startMax = '2006-10-31';
+        $this->gdata->setUser(self::ZEND_CONFERENCE_GROUP);
+        $this->gdata->setStartMax($startMax);
+        $this->assertTrue(isset($this->gdata->startMax));
+        $this->assertEquals($this->gdata->formatTimestamp($startMax), $this->gdata->getStartMax());
+        $feed = $this->gdata->getFeed();
+        $this->assertEquals(1, $feed->count());
+        unset($this->gdata->startMax);
+        $this->assertFalse(isset($this->gdata->startMax));
+    }
+
+    public function testVisibilityParam()
+    {
+        $this->gdata->resetParameters();
+        $visibility = 'private';
+        $this->gdata->setUser(self::ZEND_CONFERENCE_GROUP);
+        $this->gdata->setVisibility($visibility);
+        $this->assertTrue(isset($this->gdata->visibility));
+        $this->assertEquals($visibility, $this->gdata->getVisibility());
+        try {
+            $feed = $this->gdata->getFeed();
+        } catch (Zend_Feed_Exception $e) {
+            $this->assertContains('response code 401', $e->getMessage());
+        }
+        unset($this->gdata->visibility);
+        $this->assertFalse(isset($this->gdata->visibility));
+    }
+
+    public function testProjectionParam()
+    {
+        $this->gdata->resetParameters();
+        $projection = 'composite';
+        $this->gdata->setUser(self::ZEND_CONFERENCE_GROUP);
+        $this->gdata->setProjection($projection);
+        $this->assertTrue(isset($this->gdata->projection));
+        $this->assertEquals($projection, $this->gdata->getProjection());
+        $feed = $this->gdata->getFeed();
+        foreach ($feed as $feedItem) {
+            // echo $this->xml->formatString($feedItem->saveXML());
+            $gdc = 'gd:comments';
+            $comments = $feedItem->$gdc;
+            $gdf = 'gd:feedLink';
+            $feedLink = $comments->$gdf;
+            $feedElt = $feedLink->feed;
+            $this->assertTrue(isset($feedElt));
+        }
+
+        unset($this->gdata->projection);
+        $this->assertFalse(isset($this->gdata->projection));
+    }
+
+    public function testOrderbyParam()
+    {
+        $this->gdata->resetParameters();
+        $orderby = 'starttime';
+        $this->gdata->setUser(self::ZEND_CONFERENCE_GROUP);
+        $this->gdata->setOrderby($orderby);
+        $this->assertTrue(isset($this->gdata->orderby));
+        $this->assertEquals($orderby, $this->gdata->getOrderby());
+        $feed = $this->gdata->getFeed();
+        $prevTs = time();
+        foreach ($feed as $feedItem) {
+            // echo $this->xml->formatString($feedItem->saveXML());
+            $gdw = 'gd:when';
+            $elt = $feedItem->$gdw;
+            $startTime = $elt->offsetGet('startTime');
+            $ts = strtotime($startTime);
+            $this->assertThat($ts, $this->logicalNot($this->greaterThan($prevTs)));
+            $prevTs = $ts;
+        }
+
+        unset($this->gdata->orderby);
+        $this->assertFalse(isset($this->gdata->orderby));
+    }
+
+    public function testEventParam()
+    {
+        $this->gdata->resetParameters();
+        $this->gdata->setUser(self::ZEND_CONFERENCE_GROUP);
+        $this->gdata->setEvent(self::ZEND_CONFERENCE_EVENT_PIRATE_RECEPTION);
+        $this->assertTrue(isset($this->gdata->event));
+        $this->assertEquals(self::ZEND_CONFERENCE_EVENT_PIRATE_RECEPTION, $this->gdata->getEvent());
+        $feed = $this->gdata->getFeed();
+        foreach ($feed as $feedItem) {
+            // echo $this->xml->formatString($feedItem->saveXML());
+            $this->assertContains(self::ZEND_CONFERENCE_EVENT_PIRATE_RECEPTION, $feedItem->id());
+        }
+        unset($this->gdata->event);
+        $this->assertFalse(isset($this->gdata->event));
+    }
+
+    /*
+     * @todo: Need a calendar entry with comments for test data.
+     *
+    public function testCommentSubfeed()
+    {
+        $this->gdata->resetParameters();
+        $this->gdata->setUser(self::ZEND_CONFERENCE_GROUP);
+        $this->gdata->setEvent(self::ZEND_CONFERENCE_EVENT_PIRATE_RECEPTION);
+        $commentId = '???';
+        $this->gdata->setComments($commentId);
+        $this->assertTrue(isset($this->gdata->comments));
+        $this->assertEquals($commentId, $this->gdata->getComments());
+        $feed = $this->gdata->getFeed();
+        foreach ($feed as $feedItem) {
+            // echo $this->xml->formatString($feedItem->saveXML());
+        }
+        unset($this->gdata->comments);
+        $this->assertFalse(isset($this->gdata->comments));
+    }
+     */
 
 }
