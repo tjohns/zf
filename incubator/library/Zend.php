8c4af12d2f41154d955078b81125b358858b051b
gavin <gavin@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2006-12-15 03:01:40 +0000

    restore accidentally removed dump() method
    git-svn-id: http://framework.zend.com/svn/framework/trunk@2345 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- incubator/library/Zend.php
+++ incubator/library/Zend.php
@@ -345,4 +345,43 @@ final class Zend
 
         return self::$_registry;
     }
+
+
+    /**
+     * Debug helper function.  This is a wrapper for var_dump() that adds
+     * the <pre /> tags, cleans up newlines and indents, and runs
+     * htmlentities() before output.
+     *
+     * @param  mixed  $var The variable to dump.
+     * @param  string $label An optional label.
+     * @return string
+     */
+    static public function dump($var, $label=null, $echo=true)
+    {
+        // format the label
+        $label = ($label===null) ? '' : rtrim($label) . ' ';
+    
+        // var_dump the variable into a buffer and keep the output
+        ob_start();
+        var_dump($var);
+        $output = ob_get_clean();
+    
+        // neaten the newlines and indents
+        $output = preg_replace("/\]\=\>\n(\s+)/m", "] => ", $output);
+        if (PHP_SAPI == 'cli') {
+            $output = PHP_EOL . $label
+                    . PHP_EOL . $output
+                    . PHP_EOL;
+        } else {
+            $output = '<pre>'
+                    . $label
+                    . htmlentities($output, ENT_QUOTES)
+                    . '</pre>';
+        }
+    
+        if ($echo) {
+            echo($output);
+        }
+        return $output;
+    }
 }
