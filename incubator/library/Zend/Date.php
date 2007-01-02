8b5ce36a159c9ad5c8a36a8e9d7573d3ca54d6c8
thomas <thomas@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2007-01-02 23:22:26 +0000

    Zend_Date:  - fixed copyright
    git-svn-id: http://framework.zend.com/svn/framework/trunk@2578 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- incubator/library/Zend/Date.php
+++ incubator/library/Zend/Date.php
@@ -14,7 +14,7 @@
  *
  * @category   Zend
  * @package    Zend_Date
- * @copyright  Copyright (c) 2006 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright  Copyright (c) 2007 Zend Technologies USA Inc. (http://www.zend.com)
  * @version    $Id$
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
