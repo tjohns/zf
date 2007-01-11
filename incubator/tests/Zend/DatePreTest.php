48f7712dd9459b344a4ea3a918c4cc492789f031
gavin <gavin@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2007-01-11 20:37:33 +0000

    enhanced debugging info to aid in ZF-737
    git-svn-id: http://framework.zend.com/svn/framework/trunk@2698 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- incubator/tests/Zend/DatePreTest.php
+++ incubator/tests/Zend/DatePreTest.php
@@ -65,35 +65,43 @@ class Zend_DatePreTest extends PHPUnit_Framework_TestCase
         // When specifying a timestamp, 0 always means 1970/01/01 00:00:00
         $date = new Zend_Date(0, Zend_Date::TIMESTAMP, true);
         echo "Timestamp = ", $date->get(), " new Zend_Date(0, Zend_Date::TIMESTAMP, true)\n";
-        echo $date->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date->toString(), "\n";
+        echo "Timestamp (GMT) = ", $date->get(null, true), " new Zend_Date(0, null, false)\n";
+        echo "toString(null, GMT -i.e. true) = ", $date->toString(null, true), "\n";
+
+        $date = new Zend_Date(0, Zend_Date::TIMESTAMP, false);
+        echo "Timestamp = ", $date->get(), " new Zend_Date(0, Zend_Date::TIMESTAMP, false)\n";
+        echo "Line#:", __LINE__, " ", $date->toString(), "\n";
+        echo "Timestamp (GMT) = ", $date->get(null, true), " new Zend_Date(0, null, false)\n";
+        echo "toString(null, GMT -i.e. true) = ", $date->toString(null, true), "\n";
 
         $date = new Zend_Date(0);
         echo "Timestamp = ", $date->get(), " new Zend_Date(0)\n";
-        echo $date->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date->toString(), "\n";
 
         $date = new Zend_Date(0, null, true);
         echo "Timestamp = ", $date->get(), " new Zend_Date(0, null, true)\n";
-        echo $date->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date->toString(), "\n";
 
         $date = new Zend_Date(0, null, false);
         echo "Timestamp = ", $date->get(), " new Zend_Date(0, null, false)\n";
-        echo $date->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date->toString(), "\n";
 
         echo "Timestamp (GMT) = ", $date->get(null, true), " new Zend_Date(0, null, false)\n";
         echo "toString(null, GMT -i.e. true) = ", $date->toString(null, true), "\n";
 
         $date = new Zend_Date('7',Zend_Date::HOUR, null, false);
         echo "Timestamp = ", $date->get(), " new Zend_Date('7',Zend_Date::HOUR, null, false)\n";
-        echo $date->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date->toString(), "\n";
 
         $date = new Zend_Date('7',Zend_Date::HOUR, null, true);
         echo "Timestamp = ", $date->get(), " new Zend_Date('7',Zend_Date::HOUR, null, true)\n";
-        echo $date->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date->toString(), "\n";
 
         $locale = new Zend_Locale('en_US');
         $date = new Zend_Date('01.01.2006', Zend_Date::DATES, $locale);
         echo "Timestamp = ", $date->get(), " new Zend_Date('01.01.2006', Zend_Date::DATES, $locale)\n";
-        echo $date->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date->toString(), "\n";
     }
 
     public function testWeek()
@@ -121,57 +129,57 @@ class Zend_DatePreTest extends PHPUnit_Framework_TestCase
         echo "\nAfter addWeek(1):\n";
         $date1->addWeek(1);
         echo "Timestamp 1 = ", $date1->get(), " ->addWeek(1)\n";
-        echo $date1->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date1->toString(), "\n";
 
         $date2->addWeek(1,false);
         echo "Timestamp 2 = ", $date2->get(), " ->addWeek(1,false)\n";
-        echo $date2->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date2->toString(), "\n";
 
         $date3->addWeek(1,true);
         echo "Timestamp 3 = ", $date3->get(), " ->addWeek(1,true)\n";
-        echo $date3->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date3->toString(), "\n";
 
 
         echo "\nAfter addWeek(0):\n";
         $date1->addWeek(0);
         echo "Timestamp 1 = ", $date1->get(), " ->addWeek(0) - should not change from above\n";
-        echo $date1->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date1->toString(), "\n";
 
         $date2->addWeek(0,false);
         echo "Timestamp 2 = ", $date2->get(), " ->addWeek(0,false) - should not change from above\n";
-        echo $date2->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date2->toString(), "\n";
 
         $date3->addWeek(0,true);
         echo "Timestamp 3 = ", $date3->get(), " ->addWeek(0,true) - should not change from above\n";
-        echo $date3->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date3->toString(), "\n";
 
 
         echo "\nAfter subWeek(1):\n";
         $date1->subWeek(1);
         echo "Timestamp 1 = ", $date1->get(), " ->subWeek(1) - should be same as start time\n";
-        echo $date1->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date1->toString(), "\n";
 
         $date2->subWeek(1,false);
         echo "Timestamp 2 = ", $date2->get(), " ->subWeek(1,false) - should be same as start time\n";
-        echo $date2->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date2->toString(), "\n";
 
         $date3->subWeek(1,true);
         echo "Timestamp 3 = ", $date3->get(), " ->subWeek(1,true) - should be same as start time\n";
-        echo $date3->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date3->toString(), "\n";
 
 
         echo "\nAfter addWeek(4):\n";
         $date1->addWeek(4);
         echo "Timestamp 1 = ", $date1->get(), " ->addWeek(4) - should be exactly start time + 4 weeks\n";
-        echo $date1->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date1->toString(), "\n";
 
         $date2->addWeek(4,false);
         echo "Timestamp 2 = ", $date2->get(), " ->addWeek(4,false) - should be exactly start time + 4 weeks\n";
-        echo $date2->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date2->toString(), "\n";
 
         $date3->addWeek(4,true);
         echo "Timestamp 3 = ", $date3->get(), "->addWeek(4,true) - should be exactly start time + 4 weeks\n";
-        echo $date3->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date3->toString(), "\n";
     }
 
     public function testDay()
@@ -199,57 +207,57 @@ class Zend_DatePreTest extends PHPUnit_Framework_TestCase
         echo "\nAfter addDay(1):\n";
         $date1->addDay(1);
         echo "Timestamp 1 = ", $date1->get(), " ->addDay(1)\n";
-        echo $date1->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date1->toString(), "\n";
 
         $date2->addDay(1,false);
         echo "Timestamp 2 = ", $date2->get(), " ->addDay(1,false)\n";
-        echo $date2->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date2->toString(), "\n";
 
         $date3->addDay(1,true);
         echo "Timestamp 3 = ", $date3->get(), " ->addDay(1,true)\n";
-        echo $date3->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date3->toString(), "\n";
 
 
         echo "\nAfter addDay(0):\n";
         $date1->addDay(0);
         echo "Timestamp 1 = ", $date1->get(), " ->addDay(0) - should not change from above\n";
-        echo $date1->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date1->toString(), "\n";
 
         $date2->addDay(0,false);
         echo "Timestamp 2 = ", $date2->get(), " ->addDay(0,false) - should not change from above\n";
-        echo $date2->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date2->toString(), "\n";
 
         $date3->addDay(0,true);
         echo "Timestamp 3 = ", $date3->get(), " ->addDay(0,true) - should not change from above\n";
-        echo $date3->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date3->toString(), "\n";
 
 
         echo "\nAfter subDay(1):\n";
         $date1->subDay(1);
         echo "Timestamp 1 = ", $date1->get(), " ->subDay(1) - should be same as start time\n";
-        echo $date1->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date1->toString(), "\n";
 
         $date2->subDay(1,false);
         echo "Timestamp 2 = ", $date2->get(), " ->subDay(1,false) - should be same as start time\n";
-        echo $date2->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date2->toString(), "\n";
 
         $date3->subDay(1,true);
         echo "Timestamp 3 = ", $date3->get(), " ->subDay(1,true) - should be same as start time\n";
-        echo $date3->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date3->toString(), "\n";
 
 
         echo "\nAfter addDay(4):\n";
         $date1->addDay(4);
         echo "Timestamp 1 = ", $date1->get(), " ->addDay(4) - should be exactly start time + 4 weeks\n";
-        echo $date1->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date1->toString(), "\n";
 
         $date2->addDay(4,false);
         echo "Timestamp 2 = ", $date2->get(), " ->addDay(4,false) - should be exactly start time + 4 weeks\n";
-        echo $date2->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date2->toString(), "\n";
 
         $date3->addDay(4,true);
         echo "Timestamp 3 = ", $date3->get(), "->addDay(4,true) - should be exactly start time + 4 weeks\n";
-        echo $date3->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date3->toString(), "\n";
     }
 
     public function testHour()
@@ -277,57 +285,57 @@ class Zend_DatePreTest extends PHPUnit_Framework_TestCase
         echo "\nAfter addHour(1):\n";
         $date1->addHour(1);
         echo "Timestamp 1 = ", $date1->get(), " ->addHour(1)\n";
-        echo $date1->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date1->toString(), "\n";
 
         $date2->addHour(1,false);
         echo "Timestamp 2 = ", $date2->get(), " ->addHour(1,false)\n";
-        echo $date2->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date2->toString(), "\n";
 
         $date3->addHour(1,true);
         echo "Timestamp 3 = ", $date3->get(), " ->addHour(1,true)\n";
-        echo $date3->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date3->toString(), "\n";
 
 
         echo "\nAfter addHour(0):\n";
         $date1->addHour(0);
         echo "Timestamp 1 = ", $date1->get(), " ->addHour(0) - should not change from above\n";
-        echo $date1->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date1->toString(), "\n";
 
         $date2->addHour(0,false);
         echo "Timestamp 2 = ", $date2->get(), " ->addHour(0,false) - should not change from above\n";
-        echo $date2->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date2->toString(), "\n";
 
         $date3->addHour(0,true);
         echo "Timestamp 3 = ", $date3->get(), " ->addHour(0,true) - should not change from above\n";
-        echo $date3->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date3->toString(), "\n";
 
 
         echo "\nAfter subHour(1):\n";
         $date1->subHour(1);
         echo "Timestamp 1 = ", $date1->get(), " ->subHour(1) - should be same as start time\n";
-        echo $date1->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date1->toString(), "\n";
 
         $date2->subHour(1,false);
         echo "Timestamp 2 = ", $date2->get(), " ->subHour(1,false) - should be same as start time\n";
-        echo $date2->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date2->toString(), "\n";
 
         $date3->subHour(1,true);
         echo "Timestamp 3 = ", $date3->get(), " ->subHour(1,true) - should be same as start time\n";
-        echo $date3->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date3->toString(), "\n";
 
 
         echo "\nAfter addHour(4):\n";
         $date1->addHour(4);
         echo "Timestamp 1 = ", $date1->get(), " ->addHour(4) - should be exactly start time + 4 weeks\n";
-        echo $date1->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date1->toString(), "\n";
 
         $date2->addHour(4,false);
         echo "Timestamp 2 = ", $date2->get(), " ->addHour(4,false) - should be exactly start time + 4 weeks\n";
-        echo $date2->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date2->toString(), "\n";
 
         $date3->addHour(4,true);
         echo "Timestamp 3 = ", $date3->get(), "->addHour(4,true) - should be exactly start time + 4 weeks\n";
-        echo $date3->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date3->toString(), "\n";
     }
     
     public function testMonth()
@@ -355,57 +363,57 @@ class Zend_DatePreTest extends PHPUnit_Framework_TestCase
         echo "\nAfter addMonth(1):\n";
         $date1->addMonth(1);
         echo "Timestamp 1 = ", $date1->get(), " ->addMonth(1)\n";
-        echo $date1->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date1->toString(), "\n";
 
         $date2->addMonth(1,false);
         echo "Timestamp 2 = ", $date2->get(), " ->addMonth(1,false)\n";
-        echo $date2->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date2->toString(), "\n";
 
         $date3->addMonth(1,true);
         echo "Timestamp 3 = ", $date3->get(), " ->addMonth(1,true)\n";
-        echo $date3->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date3->toString(), "\n";
 
 
         echo "\nAfter addMonth(0):\n";
         $date1->addMonth(0);
         echo "Timestamp 1 = ", $date1->get(), " ->addMonth(0) - should not change from above\n";
-        echo $date1->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date1->toString(), "\n";
 
         $date2->addMonth(0,false);
         echo "Timestamp 2 = ", $date2->get(), " ->addMonth(0,false) - should not change from above\n";
-        echo $date2->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date2->toString(), "\n";
 
         $date3->addMonth(0,true);
         echo "Timestamp 3 = ", $date3->get(), " ->addMonth(0,true) - should not change from above\n";
-        echo $date3->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date3->toString(), "\n";
 
 
         echo "\nAfter subMonth(1):\n";
         $date1->subMonth(1);
         echo "Timestamp 1 = ", $date1->get(), " ->subMonth(1) - should be same as start time\n";
-        echo $date1->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date1->toString(), "\n";
 
         $date2->subMonth(1,false);
         echo "Timestamp 2 = ", $date2->get(), " ->subMonth(1,false) - should be same as start time\n";
-        echo $date2->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date2->toString(), "\n";
 
         $date3->subMonth(1,true);
         echo "Timestamp 3 = ", $date3->get(), " ->subMonth(1,true) - should be same as start time\n";
-        echo $date3->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date3->toString(), "\n";
 
 
         echo "\nAfter addMonth(4):\n";
         $date1->addMonth(4);
         echo "Timestamp 1 = ", $date1->get(), " ->addMonth(4) - should be exactly start time + 4 weeks\n";
-        echo $date1->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date1->toString(), "\n";
 
         $date2->addMonth(4,false);
         echo "Timestamp 2 = ", $date2->get(), " ->addMonth(4,false) - should be exactly start time + 4 weeks\n";
-        echo $date2->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date2->toString(), "\n";
 
         $date3->addMonth(4,true);
         echo "Timestamp 3 = ", $date3->get(), "->addMonth(4,true) - should be exactly start time + 4 weeks\n";
-        echo $date3->toString(), "\n";
+        echo "Line#:", __LINE__, " ", $date3->toString(), "\n";
     }
     
 }
