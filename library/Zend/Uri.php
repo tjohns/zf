65f3d514f45ab4fad9e970e17ac20da468431c02
bkarwin <bkarwin@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2007-01-16 01:40:44 +0000

    Update copyright date to 2005-2007.
    git-svn-id: http://framework.zend.com/svn/framework/trunk@2811 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Uri.php
+++ library/Zend/Uri.php
@@ -14,7 +14,7 @@
  *
  * @category   Zend
  * @package    Zend_Uri
- * @copyright  Copyright (c) 2006 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright  Copyright (c) 2005-2007 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 
@@ -42,7 +42,7 @@ require_once 'Zend/Uri/Mailto.php';
 /**
  * @category   Zend
  * @package    Zend_Uri
- * @copyright  Copyright (c) 2006 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright  Copyright (c) 2005-2007 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 abstract class Zend_Uri
