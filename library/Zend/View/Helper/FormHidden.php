8718e5b553c259b534aa4203120b6708562e4cf9
matthew <matthew@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2007-01-17 15:47:35 +0000

    * ZF-706 : allow ID attribute to propagate to hidden elements
    git-svn-id: http://framework.zend.com/svn/framework/trunk@2856 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/View/Helper/FormHidden.php
+++ library/Zend/View/Helper/FormHidden.php
@@ -45,17 +45,21 @@ class Zend_View_Helper_FormHidden extends Zend_View_Helper_FormElement {
      * @param string|array $name If a string, the element name.  If an
      * array, all other parameters are ignored, and the array elements
      * are extracted in place of added parameters.
-     * 
      * @param mixed $value The element value.
-     * 
      * @param array $attribs Attributes for the element tag.
-     * 
      * @return string The element XHTML.
      */
-    public function formHidden($name, $value = null, $attribs = null)
+    public function formHidden($name, $value = null, array $attribs = null)
     {
         $info = $this->_getInfo($name, $value, $attribs);
         extract($info); // name, value, attribs, options, listsep, disable
+        if (isset($id)) {
+            if (isset($attribs) && is_array($attribs)) {
+                $attribs['id'] = $id;
+            } else {
+                $attribs = array('id' => $id);
+            }
+        }
         return $this->_hidden($name, $value, $attribs);
     }
 }
