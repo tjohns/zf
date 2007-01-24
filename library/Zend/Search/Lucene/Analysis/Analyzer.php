c55036ea76e6ef667032805fa5de00baf40924c4
alexander <alexander@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2007-01-24 22:03:16 +0000

    Zend_Search_Lucene: * alpha-numeric UTF-8 text analyzer
    git-svn-id: http://framework.zend.com/svn/framework/trunk@2983 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Search/Lucene/Analysis/Analyzer.php
+++ library/Zend/Search/Lucene/Analysis/Analyzer.php
@@ -26,6 +26,9 @@ require_once 'Zend/Search/Lucene/Analysis/Token.php';
 /** Zend_Search_Lucene_Analysis_Analyzer_Common_Utf8 */
 require_once 'Zend/Search/Lucene/Analysis/Analyzer/Common/Utf8.php';
 
+/** Zend_Search_Lucene_Analysis_Analyzer_Common_Utf8Num */
+require_once 'Zend/Search/Lucene/Analysis/Analyzer/Common/Utf8Num.php';
+
 /** Zend_Search_Lucene_Analysis_Analyzer_Common_Text */
 require_once 'Zend/Search/Lucene/Analysis/Analyzer/Common/Text.php';
 
