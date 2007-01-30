6177a9f08326bc23d6b101b10de42daab777e78a
bkarwin <bkarwin@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2007-01-30 21:50:35 +0000

    [ZF-826] fix typo in variable name.
    git-svn-id: http://framework.zend.com/svn/framework/trunk@3101 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Db/Adapter/Pdo/Mysql.php
+++ library/Zend/Db/Adapter/Pdo/Mysql.php
@@ -87,7 +87,7 @@ class Zend_Db_Adapter_Pdo_Mysql extends Zend_Db_Adapter_Pdo_Abstract
      */
     public function describeTable($tableName, $schemaName = null)
     {
-        $sql = "DESCRIBE $table";
+        $sql = "DESCRIBE $tableName";
         $result = $this->fetchAll($sql);
         $desc = array();
         foreach ($result as $key => $row) {
