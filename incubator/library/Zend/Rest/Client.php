acdeb6562d86f31609447882c93c619822894383
matthew <matthew@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2007-02-06 18:28:44 +0000

    * Zend_Rest_Client test coverage and minor fixes
    git-svn-id: http://framework.zend.com/svn/framework/trunk@3240 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- incubator/library/Zend/Rest/Client.php
+++ incubator/library/Zend/Rest/Client.php
@@ -71,132 +71,184 @@ class Zend_Rest_Client extends Zend_Service_Abstract
      */
     public function setUri($uri)
     {
-    	if ($uri instanceof Zend_Uri_Http) {
-    		$this->_uri = $uri;
-    	} else {
-    		$this->_uri = Zend_Uri::factory($uri);
-    	}
+        if ($uri instanceof Zend_Uri_Http) {
+            $this->_uri = $uri;
+        } else {
+            $this->_uri = Zend_Uri::factory($uri);
+        }
 
         return $this;
     }
 
-	/**
-	 * Call a remote REST web service URI and return the Zend_Http_Response object
-	 *
-	 * @param  string $path            The path to append to the URI
-	 * @throws Zend_Rest_Exception
-	 * @return void
-	 */
-	final private function _prepareRest($path, $query = null)
-	{
-		// Get the URI object and configure it
-		if (!$this->_uri instanceof Zend_Uri_Http) {
+    /**
+     * Retrieve the current request URI object
+     * 
+     * @return Zend_Uri_Http
+     */
+    public function getUri()
+    {
+        return $this->_uri;
+    }
+
+    /**
+     * Call a remote REST web service URI and return the Zend_Http_Response object
+     *
+     * @param  string $path            The path to append to the URI
+     * @throws Zend_Rest_Exception
+     * @return void
+     */
+    final private function _prepareRest($path)
+    {
+        // Get the URI object and configure it
+        if (!$this->_uri instanceof Zend_Uri_Http) {
             require_once 'Zend/Rest/Client/Exception.php';
-		    throw new Zend_Rest_Client_Exception('URI object must be set before performing call');
-		}
-		
-		$uri = $this->_uri->getUri();
-		
-		if ($path[0] != '/' && $uri[strlen($uri)-1] != '/') {
-			$path = '/' . $path;
-		}
+            throw new Zend_Rest_Client_Exception('URI object must be set before performing call');
+        }
+        
+        $uri = $this->_uri->getUri();
+        
+        if ($path[0] != '/' && $uri[strlen($uri)-1] != '/') {
+            $path = '/' . $path;
+        }
 
-		$this->_uri->setPath($path);
-		if (!is_null($query)) {
-			$this->_uri->setQuery($query);
-		}
-		
-		/**
-		 * Get the HTTP client and configure it for the endpoint URI.  Do this each time
-		 * because the Zend_Http_Client instance is shared among all Zend_Service_Abstract subclasses.
-		 */
+        $this->_uri->setPath($path);
+       
+        /**
+         * Get the HTTP client and configure it for the endpoint URI.  Do this each time
+         * because the Zend_Http_Client instance is shared among all Zend_Service_Abstract subclasses.
+         */
         self::getHttpClient()->setUri($this->_uri);
-	}
+    }
 
-	/**
-	 * Performs an HTTP GET request to the $path.
-	 *
-	 * @param string $path
-	 * @return Zend_Http_Response
-	 */
-	final public function restGet($path, $query = null)
-	{
-	   $this->_prepareRest($path, $query);
-	   return self::getHttpClient()->request('GET');
-	}
+    /**
+     * Performs an HTTP GET request to the $path.
+     *
+     * @param string $path
+     * @param array  $query Array of GET parameters
+     * @return Zend_Http_Response
+     */
+    final public function restGet($path, array $query = null)
+    {
+        $this->_prepareRest($path);
+        $client = self::getHttpClient();
+        $client->setParameterGet($query);
+        return $client->request('GET');
+    }
 
-	/**
-	 * Performs an HTTP POST request to $path.
-	 *
-	 * @param string $path
-	 * @param array $data
-	 * @return Zend_Http_Response
-	 */
-	final public function restPost($path, $data)
-	{
-	   $this->_prepareRest($path);
-	   return self::getHttpClient()->request('POST');
-	}
+    /**
+     * Perform a POST or PUT
+     *
+     * Performs a POST or PUT request. Any data provided is set in the HTTP 
+     * client. String data is pushed in as raw POST data; array or object data 
+     * is pushed in as POST parameters.
+     * 
+     * @param mixed $method 
+     * @param mixed $data 
+     * @return Zend_Http_Response
+     */
+    protected function _performPost($method, $data = null)
+    {
+        $client = self::getHttpClient();
+        if (is_string($data)) {
+            $client->setRawData($data);
+        } elseif (is_array($data) || is_object($data)) {
+            $client->setParameterPost((array) $data);
+        }
+        return $client->request($method);
+    }
 
-	/**
-	 * Performs an HTTP PUT request to $path.
-	 *
-	 * @param string $path
-	 * @param array $data
-	 * @return Zend_Http_Response
-	 */
-	final public function restPut($path, $data)
-	{
-	   $this->_prepareRest($path);
-	   return self::getHttpClient()->request('PUT');;
-	}
+    /**
+     * Performs an HTTP POST request to $path.
+     *
+     * @param string $path
+     * @param mixed $data Raw data to send
+     * @return Zend_Http_Response
+     */
+    final public function restPost($path, $data = null)
+    {
+        $this->_prepareRest($path);
+        return $this->_performPost('POST', $data);
+    }
 
-	/**
-	 * Performs an HTTP DELETE request to $path.
-	 *
-	 * @param string $path
-	 * @return Zend_Http_Response
-	 */
-	final public function restDelete($path)
-	{
-	   $this->_prepareRest($path);
-	   return self::getHttpClient()->request('DELETE');
-	}
-	
-	/**
-	 * Method call overload
-	 *
-	 * @param string $method Method name
-	 * @param array $args Method args
-	 * @return Zend_Rest_Client_Result|Zend_Rest_Client
-	 */
-	public function __call($method, $args)
-	{
-		$methods = array('post', 'get', 'delete', 'put');
-		
-		if (in_array(strtolower($method), $methods)) {
-			if (!isset($args[0])) {
-				$args[0] = $this->_uri->getPath();
-			}
-			$this->_data['rest'] = 1;
-			$response = $this->{'rest' . $method}($args[0], $this->_data);
-			$sxml = new Zend_Rest_Client_Result($response->getBody());
-			return $sxml;
-		} else {
-			if (sizeof($args) == 1) {
-				// More than one arg means it's definitely a Zend_WebService_Rest_Server
-				$this->_data[$method] = $args[0];
-				$this->_data['arg1'] = $args[0];
-			} else {
-				$this->_data['method'] = $method;
-				if (sizeof($args) > 0) {
-					foreach ($args as $key => $arg) {
-						$key = 'arg' . $key;
-						$this->_data[$key] = $arg;
-					}
-				}
-			}
-			return $this;
-		}
-	}
+    /**
+     * Performs an HTTP PUT request to $path.
+     *
+     * @param string $path
+     * @param mixed $data Raw data to send in request
+     * @return Zend_Http_Response
+     */
+    final public function restPut($path, $data = null)
+    {
+        $this->_prepareRest($path);
+        return $this->_performPost('PUT', $data);
+    }
+
+    /**
+     * Performs an HTTP DELETE request to $path.
+     *
+     * @param string $path
+     * @return Zend_Http_Response
+     */
+    final public function restDelete($path)
+    {
+        $this->_prepareRest($path);
+        return self::getHttpClient()->request('DELETE');
+    }
+
+    /**
+     * Method call overload
+     *
+     * Allows calling REST actions as object methods; however, you must 
+     * follow-up by chaining the request with a request to an HTTP request 
+     * method (post, get, delete, put):
+     * <code>
+     * $response = $rest->sayHello('Foo', 'Manchu')->get();
+     * </code>
+     *
+     * You can also use an HTTP request method as a calling method, using the 
+     * path as the first argument:
+     * <code>
+     * $rest->get('/sayHello', 'Foo', 'Manchu');
+     * </code>
+     *
+     * Or use them together, but in sequential calls:
+     * <code>
+     * $rest->sayHello('Foo', 'Manchu');
+     * $response = $rest->get();
+     * </code>
+     *
+     * @param string $method Method name
+     * @param array $args Method args
+     * @return Zend_Rest_Client_Result|Zend_Rest_Client Zend_Rest_Client if using 
+     * a remote method, Zend_Rest_Client_Result if using an HTTP request method
+     */
+    public function __call($method, $args)
+    {
+        $methods = array('post', 'get', 'delete', 'put');
+        
+        if (in_array(strtolower($method), $methods)) {
+            if (!isset($args[0])) {
+                $args[0] = $this->_uri->getPath();
+            }
+            $this->_data['rest'] = 1;
+            $data = array_slice($args, 1) + $this->_data;
+            $response = $this->{'rest' . $method}($args[0], $data);
+            return new Zend_Rest_Client_Result($response->getBody());
+        } else {
+            // More than one arg means it's definitely a Zend_Rest_Server
+            if (sizeof($args) == 1) {
+                $this->_data[$method] = $args[0];
+                $this->_data['arg1']  = $args[0];
+            } else {
+                $this->_data['method'] = $method;
+                if (sizeof($args) > 0) {
+                    foreach ($args as $key => $arg) {
+                        $key = 'arg' . $key;
+                        $this->_data[$key] = $arg;
+                    }
+                }
+            }
+            return $this;
+        }
+    }
 }
