c26fcc617a6ea1857f331bf1bf7af2873a137494
darby <darby@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2007-02-06 19:13:01 +0000

    Zend_Validate_EmailAddress * removed local domain host option; to be addressed with hostname validator configuration * small refactor frees main code from precondition wrapper * added unit tests; 100% code coverage
    git-svn-id: http://framework.zend.com/svn/framework/trunk@3241 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- incubator/library/Zend/Validate/EmailAddress.php
+++ incubator/library/Zend/Validate/EmailAddress.php
@@ -46,47 +46,33 @@ class Zend_Validate_EmailAddress implements Zend_Validate_Interface
      * @var array
      */
     protected $_messages = array();
-    
+
     /**
      * Local object for validating the hostname part of an email address
      *
      * @var Zend_Validate_Hostname
      */
-    protected $_hostnameValidator = null;
-    
+    protected $_hostnameValidator;
+
     /**
-     * Sets validator options
+     * Instantiates hostname validator for local use
      *
-     * @todo Not sure what $domainLocalAllowed is intended for - ask Darby!
-     * @param boolean $domainLocalAllowed
+     * @return void
      */
-    public function __construct($domainLocalAllowed = false)
+    public function __construct()
     {
         /**
-          * @todo ZF-42 Check what hostnames are allowed via RFC 2822
-          */
-        // Instantiate Zend_Validate_Hostname allowing for all hostnames at present
-        $this->_hostnameValidator = new Zend_Validate_Hostname(); 
+         * @todo ZF-42 Check what hostnames are allowed via RFC 2822
+         */
+        $this->_hostnameValidator = new Zend_Validate_Hostname();
     }
-
-    /**
-     * @todo for ZF 0.8
-     */    
-    public function getDomainLocalAllowed()
-    {}
-
-    /**
-     * @todo for ZF 0.8
-     */    
-    public function setDomainLocalAllowed($domainLocalAllowed)
-    {}
 
     /**
      * Defined by Zend_Validate_Interface
      *
-     * Returns true if and only if $value is a valid email address 
-     * according to RFC2822 
-     * 
+     * Returns true if and only if $value is a valid email address
+     * according to RFC2822
+     *
      * @link http://www.ietf.org/rfc/rfc2822.txt RFC2822
      * @link http://www.columbia.edu/kermit/ascii.html US-ASCII characters
      * @param string $value
@@ -95,67 +81,66 @@ class Zend_Validate_EmailAddress implements Zend_Validate_Interface
     public function isValid($value)
     {
         $this->_messages = array();
-        
+
         // Split email address up
-        if (preg_match('/^([^@]+)@([^@]+)$/', $value, $matches)) {
-            $localPart	= $matches[1];
-            $hostname 	= $matches[2];
-            
-            /**
-             * @todo ZF-42 check isHostname against RFC spec
-             * @todo ZF-42 implement basic MX check on hostname via dns_get_record()
-             */
-            // Match hostname part
-            $hostnameResult = $this->_hostnameValidator->isValid($hostname);
-            if (!$hostnameResult) {
-                $this->_messages[] = "'$hostname' is not a valid hostname for email address '$value'";
-                
-                // Get messages from hostnameValidator
-                foreach ($this->_hostnameValidator->getMessages() as $message) {
-                    $this->_messages[] = $message;
-                }
+        if (!preg_match('/^([^@]+)@([^@]+)$/', $value, $matches)) {
+            $this->_messages[] = "'$value' is not a valid email address in the basic format local-part@hostname";
+            return false;
+        }
+
+        $localPart	= $matches[1];
+        $hostname 	= $matches[2];
+
+        /**
+         * @todo ZF-42 check isHostname against RFC spec
+         * @todo ZF-42 implement basic MX check on hostname via dns_get_record()
+         */
+        // Match hostname part
+        $hostnameResult = $this->_hostnameValidator->isValid($hostname);
+        if (!$hostnameResult) {
+            $this->_messages[] = "'$hostname' is not a valid hostname for email address '$value'";
+
+            // Get messages from hostnameValidator
+            foreach ($this->_hostnameValidator->getMessages() as $message) {
+                $this->_messages[] = $message;
             }
-            
-            // First try to match the local part on the common dot-atom format
-            $localResult = false;
-            
-            // Dot-atom characters are: 1*atext *("." 1*atext)
-            // atext: ALPHA / DIGIT / and "!", "#", "$", "%", "&", "'", "*", 
-            //        "-", "/", "=", "?", "^", "_", "`", "{", "|", "}", "~"
-            $atext = 'a-zA-Z0-9\x21\x23\x24\x25\x26\x27\x2a\x2b\x2d\x2f';
-            $atext .= '\x3d\x3f\x5e\x5f\x60\x7b\x7c\x7d';
-            if (preg_match('/^[' . $atext . ']+(\x2e+[' . $atext . ']+)*$/', $localPart)) {
+        }
+
+        // First try to match the local part on the common dot-atom format
+        $localResult = false;
+
+        // Dot-atom characters are: 1*atext *("." 1*atext)
+        // atext: ALPHA / DIGIT / and "!", "#", "$", "%", "&", "'", "*",
+        //        "-", "/", "=", "?", "^", "_", "`", "{", "|", "}", "~"
+        $atext = 'a-zA-Z0-9\x21\x23\x24\x25\x26\x27\x2a\x2b\x2d\x2f';
+        $atext .= '\x3d\x3f\x5e\x5f\x60\x7b\x7c\x7d';
+        if (preg_match('/^[' . $atext . ']+(\x2e+[' . $atext . ']+)*$/', $localPart)) {
+            $localResult = true;
+        }
+
+        // If not matched, try quoted string format
+        if (!$localResult) {
+
+            // Quoted-string characters are: DQUOTE *(qtext/quoted-pair) DQUOTE
+            // qtext: Non white space controls, and the rest of the US-ASCII characters not
+            //   including "\" or the quote character
+            // quoted-pair: "\" characters
+            $qtext      = '\x01-\x08\x0b\x0c\x0e-\x1f\x7f';
+            $qtext      .= '\x21\x23-\x5b\x5d-\x7e';
+            $quotedPair = '\x22\x5c\x22';
+            if (preg_match('/^\x22([' . $qtext . ']|' . $quotedPair . ')*\x22$/', $localPart)) {
                 $localResult = true;
             }
-            
-            // If not matched, try quoted string format
-            if (!$localResult) {
-                
-                // Quoted-string characters are: DQUOTE *(qtext/quoted-pair) DQUOTE
-                // qtext: Non white space controls, and the rest of the US-ASCII characters not 
-                //   including "\" or the quote character
-                // quoted-pair: "\" characters
-                $qtext      = '\x01-\x08\x0b\x0c\x0e-\x1f\x7f';
-                $qtext      .= '\x21\x23-\x5b\x5d-\x7e';
-                $quotedPair = '\x22\x5c\x22';
-                if (preg_match('/^\x22([' . $qtext . ']|' . $quotedPair . ')*\x22$/', $localPart)) {
-                    $localResult = true;
-                }
-            }
-            
-            if (!$localResult) {
-                 $this->_messages[] = "'$localPart' is not a valid local part for email address '$value'";
-            }
-            
-            // If both parts valid, return true
-            if ($localResult && $hostnameResult) {
-                return true;
-            } else {
-                return false;
-            }
-            
+        }
+
+        if (!$localResult) {
+             $this->_messages[] = "'$localPart' is not a valid local part for email address '$value'";
+        }
+
+        // If both parts valid, return true
+        if ($localResult && $hostnameResult) {
+            return true;
         } else {
-            $this->_messages[] = "'$value' is not a valid email address in the basic format local-part@hostname";
             return false;
         }
     }
@@ -172,5 +157,5 @@ class Zend_Validate_EmailAddress implements Zend_Validate_Interface
     {
         return $this->_messages;
     }
-    
+
 }
--- incubator/tests/Zend/Validate/EmailAddressTest.php
+++ incubator/tests/Zend/Validate/EmailAddressTest.php
@@ -44,12 +44,103 @@ require_once 'PHPUnit/Framework/TestCase.php';
 class Zend_Validate_EmailAddressTest extends PHPUnit_Framework_TestCase
 {
     /**
-     * Test stub
+     * Default instance created for all test methods
+     *
+     * @var Zend_Validate_EmailAddress
+     */
+    protected $_validator;
+
+    /**
+     * Creates a new Zend_Validate_EmailAddress object for each test method
      *
      * @return void
      */
-    public function testStub()
+    public function setUp()
     {
-        $this->assertTrue(true);
+        $this->_validator = new Zend_Validate_EmailAddress();
+    }
+
+    /**
+     * Ensures that a basic valid e-mail address passes validation
+     *
+     * @return void
+     */
+    public function testBasic()
+    {
+        $this->assertTrue($this->_validator->isValid('username@example.com'));
+    }
+
+    /**
+     * Ensures that localhost address is valid by default
+     *
+     * @return void
+     */
+    public function testLocalhostAllowed()
+    {
+        $this->assertTrue($this->_validator->isValid('username@localhost'));
+    }
+
+    /**
+     * Ensures that local domain names are valid by default
+     *
+     * @return void
+     */
+    public function testLocaldomainAllowed()
+    {
+        $this->assertTrue($this->_validator->isValid('username@localhost.localdomain'));
+    }
+
+    /**
+     * Ensures that validation fails when the local part is missing
+     *
+     * @return void
+     */
+    public function testLocalPartMissing()
+    {
+        $this->assertFalse($this->_validator->isValid('@example.com'));
+        $messages = $this->_validator->getMessages();
+        $this->assertTrue(isset($messages[0]));
+        $this->assertContains('local-part@hostname', $messages[0]);
+    }
+
+    /**
+     * Ensures that validation fails when the hostname is invalid
+     *
+     * @return void
+     */
+    public function testHostnameInvalid()
+    {
+        $this->assertFalse($this->_validator->isValid('username@ example . com'));
+        $messages = $this->_validator->getMessages();
+        $this->assertTrue(isset($messages[0]));
+        $this->assertContains('not a valid hostname', $messages[0]);
+    }
+
+    /**
+     * Ensures that quoted-string local part is considered valid
+     *
+     * @return void
+     */
+    public function testQuotedString()
+    {
+        $this->assertTrue($this->_validator->isValid('"username"@example.com'));
+    }
+
+    /**
+     * Ensures that validation fails when the e-mail is given as for display,
+     * with angle brackets around the actual address
+     *
+     * @return void
+     */
+    public function testEmailDisplay()
+    {
+        $this->assertFalse($this->_validator->isValid('User Name <username@example.com>'));
+        $messages = $this->_validator->getMessages();
+        $this->assertTrue(isset($messages[0]));
+        $this->assertContains('not a valid hostname', $messages[0]);
+        $this->assertTrue(isset($messages[1]));
+        $this->assertContains('not appear to be a valid local network name', $messages[1]);
+        $this->assertTrue(isset($messages[2]));
+        $this->assertContains('not a valid local part', $messages[2]);
     }
 }
