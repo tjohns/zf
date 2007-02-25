40220066c465f7d18f6ebeb85c62e3f3f2b88cb4
shahar <shahar@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2007-02-25 15:01:41 +0000

    "Attempt to fix ZF-930: Mishandling of relative paths on Windows (removed call to dirname() which causes the problem)"
    git-svn-id: http://framework.zend.com/svn/framework/trunk@3618 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Http/Client.php
+++ library/Zend/Http/Client.php
@@ -776,7 +776,8 @@ class Zend_Http_Client
                         // Else, assume we have a relative path
                     } else {
                         // Get the current path directory, removing any trailing slashes
-                        $path = rtrim(dirname($this->uri->getPath()), "/");
+                        $path = $this->uri->getPath();
+                        $path = rtrim(substr($path, 0, strrpos($path, '/')), "/");
                         $this->uri->setPath($path . '/' . $location);
                     }
                 }
