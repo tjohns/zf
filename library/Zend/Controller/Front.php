561b03e9893ec6906caba3af11cba074ffbe0339
matthew <matthew@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2007-04-02 17:08:29 +0000

    [ZF-1151]: Fix typehint
    git-svn-id: http://framework.zend.com/svn/framework/trunk@4309 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Controller/Front.php
+++ library/Zend/Controller/Front.php
@@ -447,7 +447,7 @@ class Zend_Controller_Front
     /**
      * Return the dispatcher object.
      *
-     * @return Zend_Controller_DispatcherInteface
+     * @return Zend_Controller_Dispatcher_Inteface
      */
     public function getDispatcher()
     {
