c52014215c14fa06944502a09b3dc33f92d981d7
matthew <matthew@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2007-04-03 15:36:40 +0000

    [ZF-841]: Fix XML-RPC client demo
    git-svn-id: http://framework.zend.com/svn/framework/trunk@4324 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- demos/Zend/WebServices/Protocols/xmlrpc-upc-lookup.php
+++ demos/Zend/WebServices/Protocols/xmlrpc-upc-lookup.php
@@ -3,4 +3,6 @@ require_once 'Zend/XmlRpc/Client.php';
 
 $server = new Zend_XmlRpc_Client('http://www.upcdatabase.com/rpc');
 
-print_r( $server->lookupUPC('071641301627') );
+$client = $server->getProxy();
+
+print_r( $client->lookupUPC('071641301627') );
