16cf6536622b577ad13bca24b2107fed1a792b50
mike <mike@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2007-04-28 01:29:42 +0000

    Simple boolean filter.  Other names Chuck and I considered:  - Zend_Log_Filter_EaterOfAll  - Zend_Log_Filter_HungryHungryHippo
    git-svn-id: http://framework.zend.com/svn/framework/trunk@4627 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/Log/AllTests.php
+++ tests/Zend/Log/AllTests.php
@@ -31,6 +31,7 @@ require_once 'Zend/Log/LogTest.php';
 require_once 'Zend/Log/Filter/ChainingTest.php';
 require_once 'Zend/Log/Filter/PriorityTest.php';
 require_once 'Zend/Log/Filter/MessageTest.php';
+require_once 'Zend/Log/Filter/SuppressTest.php';
 require_once 'Zend/Log/Formatter/SimpleTest.php';
 require_once 'Zend/Log/Formatter/XmlTest.php';
 require_once 'Zend/Log/Writer/DbTest.php';
@@ -64,6 +65,7 @@ class Zend_Log_AllTests
         $suite->addTestSuite('Zend_Log_Filter_ChainingTest');
         $suite->addTestSuite('Zend_Log_Filter_PriorityTest');
         $suite->addTestSuite('Zend_Log_Filter_MessageTest');
+        $suite->addTestSuite('Zend_Log_Filter_SuppressTest');
         $suite->addTestSuite('Zend_Log_Formatter_SimpleTest');
         $suite->addTestSuite('Zend_Log_Formatter_XmlTest');
         $suite->addTestSuite('Zend_Log_Writer_DbTest');
