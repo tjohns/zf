b95c5be8caaa7237c798be5ddb4881409ef73617
alexander <alexander@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2007-05-02 18:52:27 +0000

    Zend_Zearch_Lucene: * unit tests implementation update [ZF-1161];
    git-svn-id: http://framework.zend.com/svn/framework/trunk@4669 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/Search/Lucene/AllTests.php
+++ tests/Zend/Search/Lucene/AllTests.php
@@ -16,9 +16,10 @@ require_once 'Zend/Search/Lucene/PriorityQueueTest.php';
 require_once 'Zend/Search/Lucene/Storage/DirectoryTest.php';
 require_once 'Zend/Search/Lucene/Storage/FileTest.php';
 
-require_once 'Zend/Search/Lucene/Index/TermTest.php';
-require_once 'Zend/Search/Lucene/Index/TermInfoTest.php';
 require_once 'Zend/Search/Lucene/Index/FieldInfoTest.php';
+require_once 'Zend/Search/Lucene/Index/SegmentInfoTest.php';
+require_once 'Zend/Search/Lucene/Index/TermInfoTest.php';
+require_once 'Zend/Search/Lucene/Index/TermTest.php';
 
 
 class Zend_Search_Lucene_AllTests
@@ -42,9 +43,11 @@ class Zend_Search_Lucene_AllTests
         $suite->addTestSuite('Zend_Search_Lucene_Storage_DirectoryTest');
         $suite->addTestSuite('Zend_Search_Lucene_Storage_FileTest');
 
-        $suite->addTestSuite('Zend_Search_Lucene_Index_TermTest');
-        $suite->addTestSuite('Zend_Search_Lucene_Index_TermInfoTest');
         $suite->addTestSuite('Zend_Search_Lucene_Index_FieldInfoTest');
+        $suite->addTestSuite('Zend_Search_Lucene_Index_SegmentInfoTest');
+        $suite->addTestSuite('Zend_Search_Lucene_Index_TermInfoTest');
+        $suite->addTestSuite('Zend_Search_Lucene_Index_TermTest');
+
 
         return $suite;
     }
