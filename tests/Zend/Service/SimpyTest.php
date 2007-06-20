fcb4b93a48f3be0e2826cdb476bc033f701bbbd5
darby <darby@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2007-06-20 21:16:06 +0000

    Zend_Service_Simpy * troubleshooting unit test reliability; interstitial commit * minor changes for coding standards compliance and lazy loading
    git-svn-id: http://framework.zend.com/svn/framework/trunk@5393 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/Service/SimpyTest.php
+++ tests/Zend/Service/SimpyTest.php
@@ -49,25 +49,42 @@ require_once 'PHPUnit/Framework/TestCase.php';
  */
 class Zend_Service_SimpyTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * Simpy service consumer
+     *
+     * @var Zend_Service_Simpy
+     */
     protected $_simpy;
 
+    /**
+     * Sample link data
+     *
+     * @var array
+     */
     protected $_link = array(
-    	'title' => 'Zend Framework',
-    	'href' => 'http://framework.zend.com',
-    	'accessType' => 'public',
-    	'tags' => array('zend', 'framework', 'php'),
+        'title'       => 'Zend Framework',
+        'href'        => 'http://framework.zend.com',
+        'accessType'  => 'public',
+        'tags'        => array('zend', 'framework', 'php'),
         'urlNickname' => 'Zend Framework web site',
-        'note' => 'This web site rules!'
+        'note'        => 'This web site rules!'
     );
 
+    /**
+     * Sample note data
+     *
+     * @var array
+     */
     protected $_note = array(
-    	'title' => 'Test Note',
-        'tags' => array('test'),
+        'title'       => 'Test Note',
+        'tags'        => array('test'),
         'description' => 'This is a test note.'
     );
 
     public function setUp()
     {
+        sleep(5);
+
         $this->_simpy = new Zend_Service_Simpy('syapizend', 'mgt37ge');
 
         /**
@@ -77,29 +94,19 @@ class Zend_Service_SimpyTest extends PHPUnit_Framework_TestCase
         date_default_timezone_set('America/New_York');
     }
 
-    protected function getTagsArray()
-    {
-    	$tagSet = $this->_simpy->getTags();
-        $tagArray = array();
-
-        foreach ($tagSet as $tag) {
-        	$tagArray[] = $tag->getTag();
-        }
-
-        return $tagArray;
-    }
-
     public function testException()
     {
-    	try {
-    		$this->_simpy->deleteNote(null);
+        try {
+            $this->_simpy->deleteNote(null);
             $this->fail('Exception not thrown');
-    	} catch(Zend_Service_Exception $e) { }
+        } catch (Zend_Service_Exception $e) {
+            // success
+        }
     }
 
     public function testSaveLink()
     {
-    	try {
+        try {
             extract($this->_link);
 
             /**
@@ -108,17 +115,24 @@ class Zend_Service_SimpyTest extends PHPUnit_Framework_TestCase
              */
             try {
                 $this->_simpy->deleteLink($href);
-            } catch(Zend_Service_Exception $e) { }
+            } catch (Zend_Service_Exception $e) {
+                // ignore exception
+            }
 
-	    	$this->_simpy->saveLink(
-	            $title,
-	            $href,
-	            Zend_Service_Simpy_Link::ACCESSTYPE_PUBLIC,
-	            $tags,
-	            $urlNickname,
-	            $note
-	        );
-        } catch(Zend_Service_Exception $e) {
+            /**
+             * @see Zend_Service_Simpy_Link
+             */
+            require_once 'Zend/Service/Simpy/Link.php';
+
+            $this->_simpy->saveLink(
+                $title,
+                $href,
+                Zend_Service_Simpy_Link::ACCESSTYPE_PUBLIC,
+                $tags,
+                $urlNickname,
+                $note
+            );
+        } catch (Zend_Service_Exception $e) {
             $this->fail('Could not save link: ' . $e->getMessage());
         }
     }
@@ -127,103 +141,108 @@ class Zend_Service_SimpyTest extends PHPUnit_Framework_TestCase
     {
         $linkSet = $this->_simpy->getLinks();
 
-		$this->assertEquals(
-			$linkSet->getLength(),
-			1,
-			'Link set does not have expected size'
-		);
+        $this->assertEquals(
+            $linkSet->getLength(),
+            1,
+            'Link set does not have expected size'
+        );
 
         $link = $linkSet->getIterator()->current();
         $date = date('Y-m-d');
         extract($this->_link);
 
         $this->assertEquals(
-        	$link->getAccessType(),
-			$accessType,
-			'Access type does not match'
-		);
-
-		$this->assertEquals(
-			$link->getUrl(),
-			$href,
-			'URL does not match'
-		);
-
-		$this->assertEquals(
-			substr($link->getModDate(), 0, 10),
-			$date,
-			'Mod date does not match'
-		);
-
-		$this->assertEquals(
-			substr($link->getAddDate(), 0, 10),
-			$date,
-			'Add date does not match'
-		);
-
-        $this->assertEquals(
-        	$link->getTitle(),
-        	$title,
-        	'Title does not match'
+            $link->getAccessType(),
+            $accessType,
+            'Access type does not match'
         );
 
         $this->assertEquals(
-        	$link->getNickname(),
-        	$urlNickname,
-        	'Nickname does not match'
+            $link->getUrl(),
+            $href,
+            'URL does not match'
         );
 
         $this->assertEquals(
-        	$link->getTags(),
-        	$tags,
-        	'Tags do not match'
+            substr($link->getModDate(), 0, 10),
+            $date,
+            'Mod date does not match'
         );
 
         $this->assertEquals(
-        	$link->getNote(),
-        	$note,
-        	'Note does not match'
+            substr($link->getAddDate(), 0, 10),
+            $date,
+            'Add date does not match'
+        );
+
+        $this->assertEquals(
+            $link->getTitle(),
+            $title,
+            'Title does not match'
+        );
+
+        $this->assertEquals(
+            $link->getNickname(),
+            $urlNickname,
+            'Nickname does not match'
+        );
+
+        $this->assertEquals(
+            $link->getTags(),
+            $tags,
+            'Tags do not match'
+        );
+
+        $this->assertEquals(
+            $link->getNote(),
+            $note,
+            'Note does not match'
         );
     }
 
     public function testLinkQuery()
     {
-    	$date = date('Y-m-d');
-    	$linkQuery = new Zend_Service_Simpy_LinkQuery;
-    	$linkQuery->setQueryString($this->_link['title']);
-    	$linkQuery->setBeforeDate($date);
+        $date = date('Y-m-d');
 
-    	$this->assertNull(
-    		$linkQuery->getDate(),
-    		'Date has been initialized'
-    	);
+        /**
+         * @see Zend_Service_Simpy_LinkQuery
+         */
+        require_once 'Zend/Service/Simpy/LinkQuery.php';
+        $linkQuery = new Zend_Service_Simpy_LinkQuery;
+        $linkQuery->setQueryString($this->_link['title']);
+        $linkQuery->setBeforeDate($date);
 
-    	$linkQuery->setAfterDate($date);
+        $this->assertNull(
+            $linkQuery->getDate(),
+            'Date has been initialized'
+        );
 
-    	$this->assertNull(
-    		$linkQuery->getDate(),
-    		'Date has been initialized'
-    	);
+        $linkQuery->setAfterDate($date);
 
-    	$linkQuery->setDate($date);
+        $this->assertNull(
+            $linkQuery->getDate(),
+            'Date has been initialized'
+        );
 
-    	$this->assertNull(
-    		$linkQuery->getBeforeDate(),
-    		'Before date has retained its value'
-    	);
+        $linkQuery->setDate($date);
 
-    	$this->assertNull(
-    		$linkQuery->getAfterDate(),
-    		'After date has retained its value'
-    	);
+        $this->assertNull(
+            $linkQuery->getBeforeDate(),
+            'Before date has retained its value'
+        );
 
-    	$linkQuery->setLimit(1);
+        $this->assertNull(
+            $linkQuery->getAfterDate(),
+            'After date has retained its value'
+        );
 
-    	$this->assertEquals(
-    		$linkQuery->getLimit(),
-    		1,
-    		'Limit was not set'
-    	);
+        $linkQuery->setLimit(1);
+
+        $this->assertEquals(
+            $linkQuery->getLimit(),
+            1,
+            'Limit was not set'
+        );
 
         $linkQuery->setLimit(array());
 
@@ -232,13 +251,13 @@ class Zend_Service_SimpyTest extends PHPUnit_Framework_TestCase
             'Invalid limit value was accepted'
         );
 
-    	$linkSet = $this->_simpy->getLinks($linkQuery);
+        $linkSet = $this->_simpy->getLinks($linkQuery);
 
-    	$this->assertEquals(
-    		$linkSet->getLength(),
-    		1,
-    		'Link set does not have the expected size'
-    	);
+        $this->assertEquals(
+            $linkSet->getLength(),
+            1,
+            'Link set does not have the expected size'
+        );
     }
 
     public function testGetTags()
@@ -246,63 +265,63 @@ class Zend_Service_SimpyTest extends PHPUnit_Framework_TestCase
         $tagSet = $this->_simpy->getTags();
 
         $this->assertEquals(
-        	$tagSet->getLength(),
-        	count($this->_link['tags']),
-        	'Tag set does not have the expected size'
+            $tagSet->getLength(),
+            count($this->_link['tags']),
+            'Tag set does not have the expected size'
         );
 
         foreach ($tagSet as $tag) {
-        	$this->assertContains(
-        		$tag->getTag(),
-        		$this->_link['tags'],
-        		'Tag ' . $tag->getTag() . ' does not exist'
-        	);
+            $this->assertContains(
+                $tag->getTag(),
+                $this->_link['tags'],
+                'Tag ' . $tag->getTag() . ' does not exist'
+            );
 
-        	$this->assertEquals(
-        		$tag->getCount(),
-        		1,
-        		'Tag count does not match'
-        	);
+            $this->assertEquals(
+                $tag->getCount(),
+                1,
+                'Tag count does not match'
+            );
         }
     }
 
-	public function testRenameTag()
-	{
+    public function testRenameTag()
+    {
         $fromTag = 'framework';
         $toTag = 'frameworks';
 
-        $tagsArray = $this->getTagsArray();
+        $tagsArray = $this->_getTagsArray();
 
         $this->assertContains(
-        	$fromTag,
-			$tagsArray,
-        	'Source tag was not found'
+            $fromTag,
+            $tagsArray,
+            'Source tag was not found'
         );
 
         $this->assertNotContains(
-        	$toTag,
-        	$tagsArray,
-        	'Destination tag already exists'
+            $toTag,
+            $tagsArray,
+            'Destination tag already exists'
         );
 
         $this->_simpy->renameTag($fromTag, $toTag);
 
-		$tagsArray = $this->getTagsArray();
+        $tagsArray = $this->_getTagsArray();
 
         $this->assertContains(
-        	$toTag,
-        	$tagsArray,
-			'Destination tag was not found'
-		);
-	}
+            $toTag,
+            $tagsArray,
+            'Destination tag was not found'
+        );
+    }
 
-	public function testSplitTag()
-	{
+    public function testSplitTag()
+    {
         $fromTag = 'frameworks';
         $toTag1 = 'framework';
         $toTag2 = 'frameworks';
 
-        $tagsArray = $this->getTagsArray();
+        $tagsArray = $this->_getTagsArray();
 
         $this->assertContains(
             $fromTag,
@@ -312,28 +331,28 @@ class Zend_Service_SimpyTest extends PHPUnit_Framework_TestCase
 
         $this->_simpy->splitTag($fromTag, $toTag1, $toTag2);
 
-        $tagsArray = $this->getTagsArray();
+        $tagsArray = $this->_getTagsArray();
 
         $this->assertContains(
-        	$toTag1,
-        	$tagsArray,
-        	'First destination tag was not found'
-		);
+            $toTag1,
+            $tagsArray,
+            'First destination tag was not found'
+        );
 
         $this->assertContains(
-        	$toTag2,
-        	$tagsArray,
-        	'Second destination tag was not found'
-		);
-	}
+            $toTag2,
+            $tagsArray,
+            'Second destination tag was not found'
+        );
+    }
 
-	public function testMergeTags()
-	{
+    public function testMergeTags()
+    {
         $fromTag1 = 'framework';
         $fromTag2 = 'frameworks';
         $toTag = 'framework';
 
-        $tagsArray = $this->getTagsArray();
+        $tagsArray = $this->_getTagsArray();
 
         $this->assertContains(
             $fromTag1,
@@ -349,20 +368,20 @@ class Zend_Service_SimpyTest extends PHPUnit_Framework_TestCase
 
         $this->_simpy->mergeTags($fromTag1, $fromTag2, $toTag);
 
-        $tagsArray = $this->getTagsArray();
+        $tagsArray = $this->_getTagsArray();
 
         $this->assertContains(
-        	$toTag,
-        	$tagsArray,
-        	'Destination tag was not found'
+            $toTag,
+            $tagsArray,
+            'Destination tag was not found'
         );
-	}
+    }
 
     public function testRemoveTag()
     {
-    	$tag = 'zend';
+        $tag = 'zend';
 
-        $tagsArray = $this->getTagsArray();
+        $tagsArray = $this->_getTagsArray();
 
         $this->assertContains(
             $tag,
@@ -370,9 +389,9 @@ class Zend_Service_SimpyTest extends PHPUnit_Framework_TestCase
             'Tag to remove was not found'
         );
 
-    	$this->_simpy->removeTag($tag);
+        $this->_simpy->removeTag($tag);
 
-        $tagsArray = $this->getTagsArray();
+        $tagsArray = $this->_getTagsArray();
 
         $this->assertNotContains(
             $tag,
@@ -381,17 +400,17 @@ class Zend_Service_SimpyTest extends PHPUnit_Framework_TestCase
         );
     }
 
-	public function testDeleteLink()
-	{
+    public function testDeleteLink()
+    {
         $this->_simpy->deleteLink($this->_link['href']);
 
-		$linkSet = $this->_simpy->getLinks();
+        $linkSet = $this->_simpy->getLinks();
 
-		$this->assertEquals(
-        	$linkSet->getLength(),
-			0,
-			'Link was not deleted'
-		);
+        $this->assertEquals(
+            $linkSet->getLength(),
+            0,
+            'Link was not deleted'
+        );
     }
 
     public function testSaveNote()
@@ -399,12 +418,12 @@ class Zend_Service_SimpyTest extends PHPUnit_Framework_TestCase
         try {
             extract($this->_note);
 
-	    	$this->_simpy->saveNote(
-	    		$title,
-	    		$tags,
-	    		$description
-	    	);
-        } catch(Zend_Service_Exception $e) {
+            $this->_simpy->saveNote(
+                $title,
+                $tags,
+                $description
+            );
+        } catch (Zend_Service_Exception $e) {
             $this->fail('Could not save note: ' . $e->getMessage());
         }
     }
@@ -415,9 +434,9 @@ class Zend_Service_SimpyTest extends PHPUnit_Framework_TestCase
         $noteSet = $this->_simpy->getNotes();
 
         $this->assertEquals(
-        	$noteSet->getLength(),
-        	1,
-        	'Note set does not have the expected size'
+            $noteSet->getLength(),
+            1,
+            'Note set does not have the expected size'
         );
 
         $note = $noteSet->getIterator()->current();
@@ -436,21 +455,21 @@ class Zend_Service_SimpyTest extends PHPUnit_Framework_TestCase
         );
 
         $this->assertEquals(
-        	$note->getTitle(),
-        	$title,
-        	'Title does not match'
-       	);
+            $note->getTitle(),
+            $title,
+            'Title does not match'
+           );
 
         $this->assertEquals(
-        	$note->getTags(),
-        	$tags,
-        	'Tags do not match'
+            $note->getTags(),
+            $tags,
+            'Tags do not match'
         );
 
         $this->assertEquals(
-        	$note->getDescription(),
-        	$description,
-        	'Description does not match'
+            $note->getDescription(),
+            $description,
+            'Description does not match'
         );
 
         $this->assertEquals(
@@ -468,12 +487,12 @@ class Zend_Service_SimpyTest extends PHPUnit_Framework_TestCase
 
     public function testDeleteNote()
     {
-    	$noteSet = $this->_simpy->getNotes();
+        $noteSet = $this->_simpy->getNotes();
         $noteId = $noteSet->getIterator()->current()->getId();
         $this->_simpy->deleteNote($noteId);
-    	$noteSet = $this->_simpy->getNotes();
+        $noteSet = $this->_simpy->getNotes();
         foreach ($noteSet as $note) {
-        	$this->assertNotEquals(
+            $this->assertNotEquals(
                 $note->getId(),
                 $noteId,
                 'Note was not deleted'
@@ -487,81 +506,93 @@ class Zend_Service_SimpyTest extends PHPUnit_Framework_TestCase
         $watchlist = $watchlistSet->getIterator()->current();
 
         $this->assertEquals(
-        	$watchlistSet->getLength(),
-        	1,
-        	'Watchlist set does not have the expected size'
+            $watchlistSet->getLength(),
+            1,
+            'Watchlist set does not have the expected size'
         );
 
         $this->assertNotNull(
-        	$watchlist,
-        	'Watchlist is invalid'
+            $watchlist,
+            'Watchlist is invalid'
         );
     }
 
     public function testGetWatchlist()
     {
-		$watchlist = $this->_simpy->getWatchlist('1331');
+        $watchlist = $this->_simpy->getWatchlist('1331');
 
         $this->assertEquals(
-        	$watchlist->getId(),
-        	'1331',
-        	'ID does not match'
+            $watchlist->getId(),
+            '1331',
+            'ID does not match'
         );
 
-		$this->assertEquals(
-			$watchlist->getName(),
-			'Test Watchlist',
-			'Name does not match'
-		);
+        $this->assertEquals(
+            $watchlist->getName(),
+            'Test Watchlist',
+            'Name does not match'
+        );
 
-		$this->assertEquals(
-			$watchlist->getDescription(),
-			'This is a watchlist for testing purposes. Please do not remove it.',
-			'Description does not match'
-		);
+        $this->assertEquals(
+            $watchlist->getDescription(),
+            'This is a watchlist for testing purposes. Please do not remove it.',
+            'Description does not match'
+        );
 
-    	$this->assertEquals(
-    		$watchlist->getAddDate(),
-    		'Fri Dec 08 21:40:56 EST 2006',
-    		'Add date does not match'
-    	);
+        $this->assertEquals(
+            $watchlist->getAddDate(),
+            'Fri Dec 08 21:40:56 EST 2006',
+            'Add date does not match'
+        );
 
-		$this->assertEquals(
-			$watchlist->getNewLinks(),
-			0,
-			'New link count does not match'
-		);
+        $this->assertEquals(
+            $watchlist->getNewLinks(),
+            0,
+            'New link count does not match'
+        );
 
-		$this->assertEquals(
-			$watchlist->getUsers(),
-			array('otis'),
-			'User list does not match'
-		);
+        $this->assertEquals(
+            $watchlist->getUsers(),
+            array('otis'),
+            'User list does not match'
+        );
     }
 
     public function testWatchlistFilters()
     {
-    	$filterSet = $this->_simpy->getWatchlist('1331')->getFilters();
+        $filterSet = $this->_simpy->getWatchlist('1331')->getFilters();
 
-    	$this->assertEquals(
-    		$filterSet->getLength(),
-    		1,
-    		'Filter set does not have the expected size'
-    	);
+        $this->assertEquals(
+            $filterSet->getLength(),
+            1,
+            'Filter set does not have the expected size'
+        );
 
-    	$filter = $filterSet->getIterator()->current();
+        $filter = $filterSet->getIterator()->current();
 
-    	$this->assertEquals(
-    		$filter->getName(),
-    		'Test Filter',
-    		'Name does not match'
-    	);
+        $this->assertEquals(
+            $filter->getName(),
+            'Test Filter',
+            'Name does not match'
+        );
 
-    	$this->assertEquals(
-    		$filter->getQuery(),
-    		'zend',
-    		'Query does not match'
-    	);
+        $this->assertEquals(
+            $filter->getQuery(),
+            'zend',
+            'Query does not match'
+        );
+    }
+
+    protected function _getTagsArray()
+    {
+        $tagSet = $this->_simpy->getTags();
+        $tagArray = array();
+
+        foreach ($tagSet as $tag) {
+            $tagArray[] = $tag->getTag();
+        }
+
+        return $tagArray;
     }
 }
 
