bc9c7378e538e2321d1175de46bfe37a98156d68
andries <andries@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2007-06-28 12:22:46 +0000

    Zend_Filter_Alnum: - Resolves ZF-1641 - Note: some distros like Red Hat Enterprise have probably forgat to include the build options PCRE with UTF-8 and Unicode stuff support, this is a workaround fix. - Todo: convince the RedHat guys to include these build options by default or ask them to take the enterprise out of their name.
    git-svn-id: http://framework.zend.com/svn/framework/trunk@5468 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Filter/Alnum.php
+++ library/Zend/Filter/Alnum.php
@@ -63,7 +63,16 @@ class Zend_Filter_Alnum implements Zend_Filter_Interface
      */
     public function filter($value)
     {
-        $pattern = '/[^\p{L}\p{N}' . ($this->allowWhiteSpace ? '\s' : '') . ']/u';
+        $whiteSpace = $this->allowWhiteSpace ? '\s' : '';
+
+        // Checks if PCRE is compiled with UTF-8 and Unicode support
+        if (!@preg_match('/\pL/u', 'a')) {
+            // POSIX named classes are not supported, use alternative a-zA-Z0-9 match
+            $pattern = '[^a-zA-Z0-9' . $whiteSpace . ']';
+        } else {
+            // Unicode safe filter for the value
+            $pattern = '/[^\p{L}\p{N}' . $whiteSpace . ']/u';
+        }
 
         return preg_replace($pattern, '', (string) $value);
     }
