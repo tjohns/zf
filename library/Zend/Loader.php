64993638de02f17662df0592e37fdcd7c123233c
bkarwin <bkarwin@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2007-06-30 16:37:41 +0000

    Minor fix to account for a directory named '0'
    git-svn-id: http://framework.zend.com/svn/framework/trunk@5532 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Loader.php
+++ library/Zend/Loader.php
@@ -132,7 +132,7 @@ class Zend_Loader
         /**
          * Search for the file in each of the dirs named in $dirs.
          */
-        if (empty($dirs)) {
+        if (is_null($dirs)) {
             $dirs = array();
         } elseif (is_string($dirs))  {
             $dirs = explode(PATH_SEPARATOR, $dirs);
