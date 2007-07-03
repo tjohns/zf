144bf30f31247d7fe4713555d67fe929756d0fdf
shahar <shahar@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2007-07-03 08:17:11 +0000

    - Fixing ZF-1669 - Changed the default SSL transport to 'ssl' instead of 'sslv2'
    git-svn-id: http://framework.zend.com/svn/framework/trunk@5566 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Http/Client/Adapter/Socket.php
+++ library/Zend/Http/Client/Adapter/Socket.php
@@ -57,7 +57,7 @@ class Zend_Http_Client_Adapter_Socket implements Zend_Http_Client_Adapter_Interf
      * @var array
      */
     protected $config = array(
-        'ssltransport' => 'sslv2'
+        'ssltransport' => 'ssl'
     );
     
     /**
@@ -217,7 +217,7 @@ class Zend_Http_Client_Adapter_Socket implements Zend_Http_Client_Adapter_Interf
                     $chunk .= $line;
 
                     $hexchunksize = chop($line);
-                    $hexchunksize = strlen($hexchunksize) ? $hexchunksize : 0;
+                    $hexchunksize = strlen($hexchunksize) ? strtolower($hexchunksize) : 0;
                     
                     $chunksize = hexdec(chop($line));
                     if (dechex($chunksize) != $hexchunksize) {            
