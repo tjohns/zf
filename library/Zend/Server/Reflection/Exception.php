b9da4b5daeb1ed371640bed17833ab26da7c7971
thomas <thomas@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2007-07-07 14:30:34 +0000

    Coding Standard:  - converted tabs to 4 spaces  - erased ending spaces
    git-svn-id: http://framework.zend.com/svn/framework/trunk@5592 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Server/Reflection/Exception.php
+++ library/Zend/Server/Reflection/Exception.php
@@ -16,7 +16,7 @@
  * @package    Zend_Controller
  * @copyright  Copyright (c) 2005-2007 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
- */ 
+ */
 
 /**
  * Zend_Exception
@@ -33,6 +33,6 @@ require_once 'Zend/Exception.php';
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  * @version $Id$
  */
-class Zend_Server_Reflection_Exception extends Zend_Exception 
+class Zend_Server_Reflection_Exception extends Zend_Exception
 {
 }
