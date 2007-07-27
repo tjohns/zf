c4cc7fa206971dd2cfced406e25958d274816806
rboyd <rboyd@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2007-07-27 21:04:40 +0000

    Merge from trunk changes in r5555
    git-svn-id: http://framework.zend.com/svn/framework/branch/release-1.0@5897 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- demos/Zend/Gdata/Spreadsheet-ClientLogin.php
+++ demos/Zend/Gdata/Spreadsheet-ClientLogin.php
@@ -1,18 +1,23 @@
 <?php
-#
-# Copyright (C) 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Gdata
+ * @copyright  Copyright (c) 2006-2007 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
 
 require_once 'Zend/Loader.php';
 Zend_Loader::loadClass('Zend_Gdata');
