b727c07096c2cc822f72e77ec0bda9b5ad966b26
bkarwin <bkarwin@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2007-08-04 00:15:59 +0000

    Patch from Sebastian for whitelisting ZF library directory. All files in ZF library will be included in coverage report, even if the files aren't invoked by the tests. Also improve some comments in this file.
    git-svn-id: http://framework.zend.com/svn/framework/trunk@5992 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/TestHelper.php
+++ tests/TestHelper.php
@@ -20,28 +20,41 @@
  * @version    $Id$
  */
 
-// Set error reporting to the level to which Zend Framework code must comply
+require_once 'PHPUnit/Runner/Version.php';
+require_once 'PHPUnit/Util/Filter.php';
+
+// Set error reporting to the level to which Zend Framework code must comply.
 error_reporting( E_ALL | E_STRICT );
 
-// Determine the root, library, and tests directories of the framework distribution
+// Determine the root, library, and tests directories of the framework distribution.
 $zfRoot    = dirname(dirname(__FILE__));
 $zfLibrary = $zfRoot . DIRECTORY_SEPARATOR . 'library';
 $zfTests   = $zfRoot . DIRECTORY_SEPARATOR . 'tests';
 
-/*
-Prepend the Zend Framework library/ and tests/ directories to the include_path. This allows the tests to run out of
-the box and helps prevent loading other copies of the framework code and tests that would supersede this copy.
-*/
+// Prepend the Zend Framework library/ and tests/ directories to the 
+// include_path. This allows the tests to run out of the box and helps prevent 
+// loading other copies of the framework code and tests that would supersede 
+// this copy.
 set_include_path($zfLibrary . PATH_SEPARATOR
                . $zfTests   . PATH_SEPARATOR
                . get_include_path());
 
-// Load the user-defined test configuration file, if it exists; otherwise, load the default configuration
+// Load the user-defined test configuration file, if it exists;
+// otherwise, load the default configuration.
 if (is_readable($zfTests . DIRECTORY_SEPARATOR . 'TestConfiguration.php')) {
     require_once 'TestConfiguration.php';
 } else {
     require_once 'TestConfiguration.php.dist';
 }
 
-// Unset global variables no longer needed
+// Add Zend Framework library/ directory to the PHPUnit code coverage
+// whitelist. This has the effect that only production code source files appear
+// in the code coverage report and that all production code source files, even
+// those that are not covered by a test yet, are processed.
+if (TESTS_GENERATE_REPORT === TRUE &&
+    version_compare(PHPUnit_Runner_Version::id(), '3.1.6', '>=')) {
+    PHPUnit_Util_Filter::addDirectoryToWhitelist($zfLibrary);
+}
+
+// Unset global variables that are no longer needed.
 unset($zfRoot, $zfLibrary, $zfTests);
