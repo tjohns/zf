3dc654658828ea708bb2a610e797c981f6a54161
bkarwin <bkarwin@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2007-08-05 05:04:03 +0000

    [ZF-1708] apply Salvador's patch after review.
    git-svn-id: http://framework.zend.com/svn/framework/trunk@6014 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/Db/Adapter/Db2Test.php
+++ tests/Zend/Db/Adapter/Db2Test.php
@@ -66,12 +66,107 @@ class Zend_Db_Adapter_Db2Test extends Zend_Db_Adapter_TestCommon
 
     public function testAdapterTransactionCommit()
     {
-        $this->markTestIncomplete($this->getDriver() . ' has a problem with concurrent access to the same table');
+        $bugs = $this->_db->quoteIdentifier('zfbugs');
+        $bug_id = $this->_db->quoteIdentifier('bug_id');
+
+        // use our default connection as the Connection1
+        $dbConnection1 = $this->_db;
+        
+        // create a second connection to the same database
+        $dbConnection2 = Zend_Db::factory($this->getDriver(), $this->_util->getParams());
+        $dbConnection2->getConnection();
+        $dbConnection2->query('SET ISOLATION LEVEL = UR');
+        
+        // notice the number of rows in connection 2
+        $count = $dbConnection2->fetchOne("SELECT COUNT(*) FROM $bugs");
+        $this->assertEquals(4, $count, 'Expecting to see 4 rows in bugs table (step 1)');
+
+        // start an explicit transaction in connection 1
+        $dbConnection1->beginTransaction();
+
+        // delete a row in connection 1
+        $rowsAffected = $dbConnection1->delete(
+            'zfbugs',
+            "$bug_id = 1"
+        );
+        $this->assertEquals(1, $rowsAffected);
+
+        // we should see one less row in connection 2
+        // because it is doing an uncommitted read
+        $count = $dbConnection2->fetchOne("SELECT COUNT(*) FROM $bugs");
+        $this->assertEquals(3, $count, 'Expecting to see 3 rows in bugs table (step 2) because conn2 is doing an uncommitted read');
+
+        // commit the DELETE
+        $dbConnection1->commit();
+
+        // now we should see one fewer rows in connection 2
+        $count = $dbConnection2->fetchOne("SELECT COUNT(*) FROM $bugs");
+        $this->assertEquals(3, $count, 'Expecting to see 3 rows in bugs table after DELETE (step 3)');
+
+        // delete another row in connection 1
+        $rowsAffected = $dbConnection1->delete(
+            'zfbugs',
+            "$bug_id = 2"
+        );
+        $this->assertEquals(1, $rowsAffected);
+
+        // we should see results immediately, because
+        // the db connection returns to auto-commit mode
+        $count = $dbConnection2->fetchOne("SELECT COUNT(*) FROM $bugs");
+        $this->assertEquals(2, $count);
     }
 
     public function testAdapterTransactionRollback()
     {
-        $this->markTestIncomplete($this->getDriver() . ' has a problem with concurrent access to the same table');
+        $bugs = $this->_db->quoteIdentifier('zfbugs');
+        $bug_id = $this->_db->quoteIdentifier('bug_id');
+
+        // use our default connection as the Connection1
+        $dbConnection1 = $this->_db;
+
+        // create a second connection to the same database
+        $dbConnection2 = Zend_Db::factory($this->getDriver(), $this->_util->getParams());
+        $dbConnection2->getConnection();
+        $dbConnection2->query('SET ISOLATION LEVEL = UR');
+        
+        // notice the number of rows in connection 2
+        $count = $dbConnection2->fetchOne("SELECT COUNT(*) FROM $bugs");
+        $this->assertEquals(4, $count, 'Expecting to see 4 rows in bugs table (step 1)');
+
+        // start an explicit transaction in connection 1
+        $dbConnection1->beginTransaction();
+
+        // delete a row in connection 1
+        $rowsAffected = $dbConnection1->delete(
+            'zfbugs',
+            "$bug_id = 1"
+        );
+        $this->assertEquals(1, $rowsAffected);
+
+        // we should see one less row in connection 2
+        // because it is doing an uncommitted read
+        $count = $dbConnection2->fetchOne("SELECT COUNT(*) FROM $bugs");
+        $this->assertEquals(3, $count, 'Expecting to see 3 rows in bugs table (step 2) because conn2 is doing an uncommitted read');
+
+        // rollback the DELETE
+        $dbConnection1->rollback();
+
+        // now we should see the same number of rows
+        // because the DELETE was rolled back
+        $count = $dbConnection2->fetchOne("SELECT COUNT(*) FROM $bugs");
+        $this->assertEquals(4, $count, 'Expecting to still see 4 rows in bugs table after DELETE is rolled back (step 3)');
+
+        // delete another row in connection 1
+        $rowsAffected = $dbConnection1->delete(
+            'zfbugs',
+            "$bug_id = 2"
+        );
+        $this->assertEquals(1, $rowsAffected);
+
+        // we should see results immediately, because
+        // the db connection returns to auto-commit mode
+        $count = $dbConnection2->fetchOne("SELECT COUNT(*) FROM $bugs");
+        $this->assertEquals(3, $count, 'Expecting to see 3 rows in bugs table after DELETE (step 4)');
     }
 
     public function getDriver()
