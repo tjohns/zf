37f9de4c59f3ca62cadc2865e9042ba6757bb59c
matthew <matthew@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2007-09-11 20:13:37 +0000

    * Merging in [6191] -- test fixes and enhancements
    git-svn-id: http://framework.zend.com/svn/framework/branch/release-1.0@6305 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/MimeTest.php
+++ tests/Zend/MimeTest.php
@@ -1,24 +1,47 @@
 <?php
 /**
- * @package 	Zend_Mime
- * @subpackage  UnitTests
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Mime
+ * @subpackage UnitTests
+ * @copyright  Copyright (c) 2005-2007 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
-
 
 /**
- * Zend_Mime
+ * Test helper
  */
+require_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'TestHelper.php';
+
+/** Zend_Mail */
+require_once 'Zend/Mail.php';
+
+/** Zend_Mime */
 require_once 'Zend/Mime.php';
 
-/**
- * PHPUnit test case
- */
+/** PHPUnit test case */
 require_once 'PHPUnit/Framework/TestCase.php';
 
+/** Zend_MailTest */
+require_once dirname(__FILE__) . '/MailTest.php';
 
 /**
- * @package 	Zend_Mime
- * @subpackage  UnitTests
+ * @category   Zend
+ * @package    Zend_Mime
+ * @subpackage UnitTests
+ * @copyright  Copyright (c) 2005-2007 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 class Zend_MimeTest extends PHPUnit_Framework_TestCase
 {
