93cbd97d4c859d026f4dd98c4ea071a08aa45371
darby <darby@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2007-09-18 14:03:38 +0000

    * Replaced references to old license location with current license location * Relates to [ZF-507]
    git-svn-id: http://framework.zend.com/svn/framework/trunk@6400 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Mail/Storage/Maildir.php
+++ library/Zend/Mail/Storage/Maildir.php
@@ -7,7 +7,7 @@
  * This source file is subject to version 1.0 of the Zend Framework
  * license, that is bundled with this package in the file LICENSE, and
  * is available through the world-wide-web at the following URL:
- * http://www.zend.com/license/framework/1_0.txt. If you did not receive
+ * http://framework.zend.com/license/new-bsd. If you did not receive
  * a copy of the Zend Framework license and are unable to obtain it
  * through the world-wide-web, please send a note to license@zend.com
  * so we can mail you a copy immediately.
