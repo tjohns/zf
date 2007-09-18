efac8fd5644995caca7e8d1f9783bf3a9b68e6ae
darby <darby@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2007-09-18 15:13:04 +0000

    * Replaced references to LICENSE with current filename LICENSE.txt * Relates to [ZF-507]
    git-svn-id: http://framework.zend.com/svn/framework/trunk@6401 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- incubator/library/Zend/Db/Xml/XmlContentStore.php
+++ incubator/library/Zend/Db/Xml/XmlContentStore.php
@@ -6,7 +6,7 @@
  * LICENSE
  *
  * This source file is subject to version 1.0 of the Zend Framework
- * license, that is bundled with this package in the file LICENSE, and
+ * license, that is bundled with this package in the file LICENSE.txt, and
  * is available through the world-wide-web at the following URL:
  * http://framework.zend.com/license/new-bsd. If you did not receive
  * a copy of the Zend Framework license and are unable to obtain it
