6c1a448dbe032497604511335119df9a36360d1e
darby <darby@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2007-09-26 16:49:41 +0000

    * Added test helper
    git-svn-id: http://framework.zend.com/svn/framework/trunk@6530 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/Search/Lucene/AllTests.php
+++ tests/Zend/Search/Lucene/AllTests.php
@@ -3,6 +3,11 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
     define('PHPUnit_MAIN_METHOD', 'Zend_Search_Lucene_AllTests::main');
 }
 
+/**
+ * Test helper
+ */
+require_once dirname(dirname(dirname(dirname(__FILE__)))) . DIRECTORY_SEPARATOR . 'TestHelper.php';
+
 require_once 'PHPUnit/Framework/TestSuite.php';
 require_once 'PHPUnit/TextUI/TestRunner.php';
 
