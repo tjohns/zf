49a5daf3e921dfb722644a304cc77edf52fb1568
matthew <matthew@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2007-10-05 16:49:03 +0000

    * Move TestHelper require into main routine to allow using with phpunit
    git-svn-id: http://framework.zend.com/svn/framework/trunk@6594 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- incubator/tests/Zend/View/Helper/AllTests.php
+++ incubator/tests/Zend/View/Helper/AllTests.php
@@ -1,9 +1,8 @@
 <?php
 if (!defined('PHPUnit_MAIN_METHOD')) {
-    require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/TestHelper.php';
     define('PHPUnit_MAIN_METHOD', 'Zend_View_Helper_AllTests::main');
 }
-
+require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/TestHelper.php';
 require_once 'PHPUnit/Framework/TestSuite.php';
 require_once 'PHPUnit/TextUI/TestRunner.php';
 
