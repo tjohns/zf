d63dacad53a9095770527954304aff1d1b7b699b
matthew <matthew@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2007-10-30 19:16:34 +0000

    * Added page- and class-level docblocks to all classes; made sure all methods    have docblocks
    git-svn-id: http://framework.zend.com/svn/framework/trunk@6693 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- incubator/library/Zend/Filter/Inflector.php
+++ incubator/library/Zend/Filter/Inflector.php
@@ -1,11 +1,42 @@
 <?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Filter
+ * @copyright  Copyright (c) 2005-2007 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ * @version    $Id: $
+ */
 
+/**
+ * @see Zend_Filter_Interface
+ */
 require_once 'Zend/Filter/Interface.php';
+
+/** Zend_Loader_PluginLoader */
 require_once 'Zend/Loader/PluginLoader.php';
 
+/**
+ * Filter chain for string inflection
+ *
+ * @category   Zend
+ * @package    Zend_Filter
+ * @copyright  Copyright (c) 2005-2007 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
 class Zend_Filter_Inflector implements Zend_Filter_Interface 
 {
-    
     /**
      * @var Zend_Loader_PluginLoader_Interface
      */
@@ -223,5 +254,4 @@ class Zend_Filter_Inflector implements Zend_Filter_Interface
         require_once 'Zend/Filter/Exception.php';
         throw new Zend_Filter_Exception('No class named ' . $rule . ' implementing Zend_Filter_Interface could be found.');
     }
-        
 }
--- incubator/library/Zend/Filter/RegexReplace.php
+++ incubator/library/Zend/Filter/RegexReplace.php
@@ -1,17 +1,68 @@
 <?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Filter
+ * @copyright  Copyright (c) 2005-2007 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ * @version    $Id: $
+ */
 
+/**
+ * @see Zend_Filter_Interface
+ */
+require_once 'Zend/Filter/Interface.php';
+
+/**
+ * @category   Zend
+ * @package    Zend_Filter
+ * @copyright  Copyright (c) 2005-2007 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
 class Zend_Filter_RegexReplace implements Zend_Filter_Interface
 {
-    
+    /**
+     * Pattern to match
+     * @var string
+     */
     protected $_match = null;
+
+    /**
+     * Replacement pattern
+     * @var string
+     */
     protected $_replace = null;
     
+    /**
+     * Constructor
+     * 
+     * @param  string $match 
+     * @param  string $replace 
+     * @return void
+     */
     public function __construct($match, $replace)
     {
         $this->_match = $match;
         $this->_replace = $replace;
     }
     
+    /**
+     * Perform regexp replacement as filter
+     * 
+     * @param  string $value 
+     * @return string
+     */
     public function filter($value)
     {
         return preg_replace($this->_match, $this->_replace, $value);
