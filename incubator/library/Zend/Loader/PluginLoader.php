8d6b5f06e88a9d07261a2ebacadf63cad1d31345
matthew <matthew@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2007-10-30 19:21:27 +0000

    * Added file- and class-level docblocks  * Whitespace fixes
    git-svn-id: http://framework.zend.com/svn/framework/trunk@6694 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- incubator/library/Zend/Loader/PluginLoader.php
+++ incubator/library/Zend/Loader/PluginLoader.php
@@ -1,15 +1,39 @@
 <?php
-
 /**
- * @see Zend_Loader_PluginLoader_Interface
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Loader
+ * @subpackage PluginLoader
+ * @copyright  Copyright (c) 2005-2007 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
+
+/** Zend_Loader_PluginLoader_Interface */
 require_once 'Zend/Loader/PluginLoader/Interface.php';
 
-/**
- * @see Zend_Loader
- */
+/** Zend_Loader */
 require_once 'Zend/Loader.php';
 
+/**
+ * Generic plugin class loader
+ *
+ * @category   Zend
+ * @package    Zend_Loader
+ * @subpackage PluginLoader
+ * @copyright  Copyright (c) 2005-2007 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
 class Zend_Loader_PluginLoader implements Zend_Loader_PluginLoader_Interface 
 {
     /**
@@ -212,5 +236,4 @@ class Zend_Loader_PluginLoader implements Zend_Loader_PluginLoader_Interface
         require_once 'Zend/Loader/PluginLoader/Exception.php';
         throw new Zend_Loader_PluginLoader_Exception('Helper by ' . $name . ' was not found in the registry.');
     }
-
 }
--- incubator/library/Zend/Loader/PluginLoader/Interface.php
+++ incubator/library/Zend/Loader/PluginLoader/Interface.php
@@ -1,8 +1,35 @@
 <?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Loader
+ * @subpackage PluginLoader
+ * @copyright  Copyright (c) 2005-2007 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
 
+/**
+ * Plugin class loader interface
+ *
+ * @category   Zend
+ * @package    Zend_Loader
+ * @subpackage PluginLoader
+ * @copyright  Copyright (c) 2005-2007 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
 interface Zend_Loader_PluginLoader_Interface
 {
-    
     /**
      * Add prefixed paths to the registry of paths
      *
@@ -44,5 +71,4 @@ interface Zend_Loader_PluginLoader_Interface
      * @return string
      */
     public function load($name);
-        
 }
