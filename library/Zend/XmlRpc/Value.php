768ecddc19414ab4181976b21313f205bb5a3b52
matthew <matthew@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2007-11-26 16:16:39 +0000

    * Faster algorithm for determining if an array is associative (ZF-2090)
    git-svn-id: http://framework.zend.com/svn/framework/trunk@6937 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/XmlRpc/Value.php
+++ library/Zend/XmlRpc/Value.php
@@ -224,6 +224,18 @@ abstract class Zend_XmlRpc_Value
 
 
     /**
+     * Callback used when determining if an array is associative
+     * 
+     * @param  int|string $a 
+     * @param  int|string $b 
+     * @return int
+     */
+    protected static function _isAssocCallback($a, $b)
+    {
+        return $a === $b ? $a + 1 : 0;
+    }
+
+    /**
      * Transform a PHP native variable into a XML-RPC native value
      *
      * @param mixed $value The PHP variable for convertion
@@ -240,19 +252,16 @@ abstract class Zend_XmlRpc_Value
                 // Break intentionally omitted
             case 'array':
                 // Default native type for a PHP array (a simple numeric array) is 'array'
-                // If the PHP array is an assosiative array the native type will be 'struct'
                 $obj = 'Zend_XmlRpc_Value_Array';
 
-                // Go over the elements in the array, if the key is different than the index
-                //  it means this array has associative keys and it's a struct
+                // Determine if this is an associative array
                 if (is_array($value)) { // If the value is not array, it can't be an associated array
-                    $i = 0;
-                    foreach ($value as $key => $element) {
-                        if ($i !== $key) {
-                            $obj = 'Zend_XmlRpc_Value_Struct';
-                            break;
-                        }
-                        ++$i;
+                    if (count($value) 
+                        && (count($value) !== array_reduce(array_keys($value), array(__CLASS__, '_isAssocCallback'), 0))) 
+                    {
+                        // If the PHP array is an assosiative array the native 
+                        // type will be 'struct'
+                        $obj = 'Zend_XmlRpc_Value_Struct';
                     }
                 }
                 return new $obj($value);
