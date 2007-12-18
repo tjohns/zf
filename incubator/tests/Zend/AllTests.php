8d388dc2e16c29ca186abe8d12b3bdbd48664163
matthew <matthew@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2007-12-18 14:46:29 +0000

    * Merge Zend_View Enhanced placeholder implementations to core
    git-svn-id: http://framework.zend.com/svn/framework/trunk@7187 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- incubator/tests/Zend/AllTests.php
+++ incubator/tests/Zend/AllTests.php
@@ -30,14 +30,13 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
 require_once 'PHPUnit/Framework/TestSuite.php';
 require_once 'PHPUnit/TextUI/TestRunner.php';
 
-// require_once 'Zend/Crypt/AllTests.php';
-// require_once 'Zend/Db/AllTests.php';
-// require_once 'Zend/InfoCard/AllTests.php';
-// require_once 'Zend/Json/AllTests.php';
-// require_once 'Zend/Service/AllTests.php';
-// require_once 'Zend/TimeSyncTest.php';
-// require_once 'Zend/Validate/BarcodeTest.php';
-require_once 'Zend/View/AllTests.php';
+require_once 'Zend/Crypt/AllTests.php';
+require_once 'Zend/Db/AllTests.php';
+require_once 'Zend/InfoCard/AllTests.php';
+require_once 'Zend/Json/AllTests.php';
+require_once 'Zend/Service/AllTests.php';
+require_once 'Zend/TimeSyncTest.php';
+require_once 'Zend/Validate/BarcodeTest.php';
 
 /**
  * @category   Zend
@@ -67,38 +66,33 @@ class Zend_AllTests
         /*
          * Perform the tests for Zend_Db component now.
          */
-        // $suite->addTest(Zend_Db_AllTests::suite());
+        $suite->addTest(Zend_Db_AllTests::suite());
 
         /**
          * Perform the tests for Zend_InfoCard component now.
          */
-        // $suite->addTest(Zend_InfoCard_AllTests::suite());
+        $suite->addTest(Zend_InfoCard_AllTests::suite());
 
         /*
          * Perform the tests for Zend_Json component now.
          */
-        // $suite->addTest(Zend_Json_AllTests::suite());
+        $suite->addTest(Zend_Json_AllTests::suite());
 
         /*
          * Perform the tests for Zend_Service component now.
          */
-        // $suite->addTest(Zend_Service_AllTests::suite());
+        $suite->addTest(Zend_Service_AllTests::suite());
 
         /*
          * Perform the tests for Zend_TimeSync component now.
          */
-        // $suite->addTestSuite('Zend_TimeSyncTest');
+        $suite->addTestSuite('Zend_TimeSyncTest');
 
         /*
          * Perform the tests for Zend_Validate_Barcode component now.
          */
-        // $suite->addTestSuite('Zend_Validate_BarcodeTest');
+        $suite->addTestSuite('Zend_Validate_BarcodeTest');
 
-        /**
-         * Perform the tests for Zend_View component now.
-         */
-        $suite->addTest(Zend_View_AllTests::suite());
-	
         return $suite;
     }
 }
--- tests/Zend/View/Helper/AllTests.php
+++ tests/Zend/View/Helper/AllTests.php
@@ -8,17 +8,25 @@ require_once 'PHPUnit/TextUI/TestRunner.php';
 
 require_once 'Zend/View/Helper/ActionTest.php';
 require_once 'Zend/View/Helper/DeclareVarsTest.php';
+require_once 'Zend/View/Helper/DoctypeTest.php';
 require_once 'Zend/View/Helper/FormCheckboxTest.php';
 require_once 'Zend/View/Helper/FormLabelTest.php';
 require_once 'Zend/View/Helper/FormSelectTest.php';
 require_once 'Zend/View/Helper/FormTextTest.php';
+require_once 'Zend/View/Helper/HeadLinkTest.php';
+require_once 'Zend/View/Helper/HeadMetaTest.php';
+require_once 'Zend/View/Helper/HeadScriptTest.php';
+require_once 'Zend/View/Helper/HeadStyleTest.php';
+require_once 'Zend/View/Helper/HeadTitleTest.php';
 require_once 'Zend/View/Helper/HtmlListTest.php';
+require_once 'Zend/View/Helper/InlineScriptTest.php';
 require_once 'Zend/View/Helper/LayoutTest.php';
 require_once 'Zend/View/Helper/PartialTest.php';
 require_once 'Zend/View/Helper/PartialLoopTest.php';
 require_once 'Zend/View/Helper/PlaceholderTest.php';
 require_once 'Zend/View/Helper/Placeholder/ContainerTest.php';
 require_once 'Zend/View/Helper/Placeholder/RegistryTest.php';
+require_once 'Zend/View/Helper/Placeholder/StandaloneContainerTest.php';
 require_once 'Zend/View/Helper/UrlTest.php';
 
 
@@ -42,17 +50,25 @@ class Zend_View_Helper_AllTests
 
         $suite->addTestSuite('Zend_View_Helper_ActionTest');
         $suite->addTestSuite('Zend_View_Helper_DeclareVarsTest');
+        $suite->addTestSuite('Zend_View_Helper_DoctypeTest');
         $suite->addTestSuite('Zend_View_Helper_FormCheckboxTest');
         $suite->addTestSuite('Zend_View_Helper_FormLabelTest');
         $suite->addTestSuite('Zend_View_Helper_FormSelectTest');
         $suite->addTestSuite('Zend_View_Helper_FormTextTest');
+        $suite->addTestSuite('Zend_View_Helper_HeadLinkTest');
+        $suite->addTestSuite('Zend_View_Helper_HeadMetaTest');
+        $suite->addTestSuite('Zend_View_Helper_HeadScriptTest');
+        $suite->addTestSuite('Zend_View_Helper_HeadStyleTest');
+        $suite->addTestSuite('Zend_View_Helper_HeadTitleTest');
         $suite->addTestSuite('Zend_View_Helper_HtmlListTest');
+        $suite->addTestSuite('Zend_View_Helper_InlineScriptTest');
         $suite->addTestSuite('Zend_View_Helper_LayoutTest');
         $suite->addTestSuite('Zend_View_Helper_PartialTest');
         $suite->addTestSuite('Zend_View_Helper_PartialLoopTest');
         $suite->addTestSuite('Zend_View_Helper_PlaceholderTest');
         $suite->addTestSuite('Zend_View_Helper_Placeholder_ContainerTest');
         $suite->addTestSuite('Zend_View_Helper_Placeholder_RegistryTest');
+        $suite->addTestSuite('Zend_View_Helper_Placeholder_StandaloneContainerTest');
         $suite->addTestSuite('Zend_View_Helper_UrlTest');
         
         return $suite;
