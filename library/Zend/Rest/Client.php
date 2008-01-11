61fd2b458636d81cd995f9bc0aed40b696876e8b
davey <davey@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-01-11 03:19:59 +0000

    Apply fix from ZF-1853 - Reset HTTP client parameters for each request
    git-svn-id: http://framework.zend.com/svn/framework/branch/release-1.0@7380 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Rest/Client.php
+++ library/Zend/Rest/Client.php
@@ -94,7 +94,7 @@ class Zend_Rest_Client extends Zend_Service_Abstract
      * Call a remote REST web service URI and return the Zend_Http_Response object
      *
      * @param  string $path            The path to append to the URI
-     * @throws Zend_Rest_Client_Exception
+     * @throws Zend_Rest_Exception
      * @return void
      */
     final private function _prepareRest($path)
@@ -117,7 +117,7 @@ class Zend_Rest_Client extends Zend_Service_Abstract
          * Get the HTTP client and configure it for the endpoint URI.  Do this each time
          * because the Zend_Http_Client instance is shared among all Zend_Service_Abstract subclasses.
          */
-        self::getHttpClient()->setUri($this->_uri);
+        self::getHttpClient()->resetParameters()->setUri($this->_uri);
     }
 
     /**
