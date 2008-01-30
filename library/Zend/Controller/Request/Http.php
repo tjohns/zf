440fae98f65d5fc5cac54c1d946959a45b74ef36
weppos <weppos@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-01-30 22:33:32 +0000

    Zend_Controller: * fixed: typo in docblock (closes ZF-2284)
    git-svn-id: http://framework.zend.com/svn/framework/trunk@7713 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Controller/Request/Http.php
+++ library/Zend/Controller/Request/Http.php
@@ -277,7 +277,7 @@ class Zend_Controller_Request_Http extends Zend_Controller_Request_Abstract
     /**
      * Retrieve a member of the $_SERVER superglobal
      *
-     * If no $key is passed, returns the entire $_COOKIE array.
+     * If no $key is passed, returns the entire $_SERVER array.
      *
      * @param string $key
      * @param mixed $default Default value to use if key not found
@@ -295,7 +295,7 @@ class Zend_Controller_Request_Http extends Zend_Controller_Request_Abstract
     /**
      * Retrieve a member of the $_ENV superglobal
      *
-     * If no $key is passed, returns the entire $_COOKIE array.
+     * If no $key is passed, returns the entire $_ENV array.
      *
      * @param string $key
      * @param mixed $default Default value to use if key not found
