320f34bc5384be41a81ebfeeb50eaefbf8b3cb21
thomas <thomas@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-02-08 16:19:21 +0000

    Zend_View_Helper_Translate:  - WE ARE CORED :-)
    git-svn-id: http://framework.zend.com/svn/framework/trunk@7863 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/View/Helper/AllTests.php
+++ tests/Zend/View/Helper/AllTests.php
@@ -27,6 +27,7 @@ require_once 'Zend/View/Helper/PlaceholderTest.php';
 require_once 'Zend/View/Helper/Placeholder/ContainerTest.php';
 require_once 'Zend/View/Helper/Placeholder/RegistryTest.php';
 require_once 'Zend/View/Helper/Placeholder/StandaloneContainerTest.php';
+require_once 'Zend/View/Helper/TranslateTest.php';
 require_once 'Zend/View/Helper/UrlTest.php';
 
 
@@ -69,6 +70,7 @@ class Zend_View_Helper_AllTests
         $suite->addTestSuite('Zend_View_Helper_Placeholder_ContainerTest');
         $suite->addTestSuite('Zend_View_Helper_Placeholder_RegistryTest');
         $suite->addTestSuite('Zend_View_Helper_Placeholder_StandaloneContainerTest');
+        $suite->addTestSuite('Zend_View_Helper_TranslateTest');
         $suite->addTestSuite('Zend_View_Helper_UrlTest');
         
         return $suite;
