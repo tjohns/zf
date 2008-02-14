4d4b1e0642e0e04cd6d4db8788f6b3df0bc36504
matthew <matthew@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-02-14 17:11:56 +0000

    * ZF-2506: remove unused variable
    git-svn-id: http://framework.zend.com/svn/framework/branch/release-1.0@7998 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Controller/Plugin/Broker.php
+++ library/Zend/Controller/Plugin/Broker.php
@@ -78,6 +78,15 @@ class Zend_Controller_Plugin_Broker extends Zend_Controller_Plugin_Abstract
             $this->_plugins[$stackIndex] = $plugin;
         }
 
+        $request = $this->getRequest();
+        if ($request) {
+            $this->_plugins[$stackIndex]->setRequest($request);
+        }
+        $response = $this->getResponse();
+        if ($response) {
+            $this->_plugins[$stackIndex]->setResponse($response);
+        }
+
         ksort($this->_plugins);
 
         return $this;
@@ -118,7 +127,6 @@ class Zend_Controller_Plugin_Broker extends Zend_Controller_Plugin_Abstract
      */
     public function hasPlugin($class)
     {
-        $found = array();
         foreach ($this->_plugins as $plugin) {
             $type = get_class($plugin);
             if ($class == $type) {
