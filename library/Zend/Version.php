e5b5f2601e3b4d378be81eb631f2bea5bad7b3c5
alexander <alexander@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-02-23 00:28:53 +0000

    Merging version update into release maintenance branch.
    git-svn-id: http://framework.zend.com/svn/framework/branch/release-1.5@8318 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Version.php
+++ library/Zend/Version.php
@@ -32,7 +32,7 @@ final class Zend_Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    const VERSION = '1.5.0PR';
+    const VERSION = '1.5.0RC1';
 
     /**
      * Compare the specified Zend Framework version string $version
