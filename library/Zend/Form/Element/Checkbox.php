f7783799899e77e4a3fb11fb41081fdfd20fc5ae
matthew <matthew@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-03-07 17:36:42 +0000

    r16987@zlaptop:  matthew | 2008-03-07 12:35:53 -0500   * ZF-2750: Fix how checkboxes work:     * Allow setting arbitrary values for checked and unchecked states     * Enforce checked and unchecked values when setting element value     * Enforce checked property state when setting element value     * Enforce element value when setting checked property state     * Ensure that value is populated correctly when options are passed     * Ensure that checked value is always used when rendering element
    git-svn-id: http://framework.zend.com/svn/framework/trunk@8631 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Form/Element/Checkbox.php
+++ library/Zend/Form/Element/Checkbox.php
@@ -47,23 +47,146 @@ class Zend_Form_Element_Checkbox extends Zend_Form_Element_Xhtml
     public $helper = 'formCheckbox';
 
     /**
-     * Current value
-     * @var int 0 or 1
+     * Value when checked
+     * @var string
      */
-    protected $_value = 0;
+    protected $_checkedValue = '1';
+
+    /**
+     * Value when not checked
+     * @var string
+     */
+    protected $_uncheckedValue = '0';
+
+    /**
+     * Current value
+     * @var string 0 or 1
+     */
+    protected $_value = '0';
+
+    /**
+     * Set options
+     *
+     * Intercept checked and unchecked values and set them early; test stored 
+     * value against checked and unchecked values after configuration.
+     * 
+     * @param  array $options 
+     * @return Zend_Form_Element_Checkbox
+     */
+    public function setOptions(array $options)
+    {
+        if (array_key_exists('checkedValue', $options)) {
+            $this->setCheckedValue($options['checkedValue']);
+            unset($options['checkedValue']);
+        }
+        if (array_key_exists('uncheckedValue', $options)) {
+            $this->setUncheckedValue($options['uncheckedValue']);
+            unset($options['uncheckedValue']);
+        }
+        parent::setOptions($options);
+
+        $curValue = $this->getValue();
+        $test     = array($this->getCheckedValue(), $this->getUncheckedValue());
+        if (!in_array($curValue, $test)) {
+            $this->setValue($curValue);
+        }
+
+        return $this;
+    }
 
     /**
      * Set value
      *
-     * If non-null, sets checked flag to true
+     * If value matches checked value, sets to that value, and sets the checked
+     * flag to true.
+     *
+     * Any other value causes the unchecked value to be set as the current 
+     * value, and the checked flag to be set as false.
+     *
      * 
-     * @param mixed $value 
-     * @return void
+     * @param  mixed $value 
+     * @return Zend_Form_Element_Checkbox
      */
     public function setValue($value)
     {
-        $value = ($value === null) ? 0 : 1;
-        $this->checked = ($value === 0) ? false : true;
-        return parent::setValue($value);
+        if ($value == $this->getCheckedValue()) {
+            parent::setValue($value);
+            $this->checked = true;
+        } else {
+            parent::setValue($this->getUncheckedValue());
+            $this->checked = false;
+        }
+        return $this;
+    }
+
+    /**
+     * Set checked value
+     * 
+     * @param  string $value 
+     * @return Zend_Form_Element_Checkbox
+     */
+    public function setCheckedValue($value)
+    {
+        $this->_checkedValue = (string) $value;
+        return $this;
+    }
+
+    /**
+     * Get value when checked
+     * 
+     * @return string
+     */
+    public function getCheckedValue()
+    {
+        return $this->_checkedValue;
+    }
+
+    /**
+     * Set unchecked value
+     * 
+     * @param  string $value 
+     * @return Zend_Form_Element_Checkbox
+     */
+    public function setUncheckedValue($value)
+    {
+        $this->_uncheckedValue = (string) $value;
+        return $this;
+    }
+
+    /**
+     * Get value when not checked
+     * 
+     * @return string
+     */
+    public function getUncheckedValue()
+    {
+        return $this->_uncheckedValue;
+    }
+
+    /**
+     * Set checked flag
+     * 
+     * @param  bool $flag 
+     * @return Zend_Form_Element_Checkbox
+     */
+    public function setChecked($flag)
+    {
+        $this->checked = (bool) $flag;
+        if ($this->checked) {
+            $this->setValue($this->getCheckedValue());
+        } else {
+            $this->setValue($this->getUncheckedValue());
+        }
+        return $this;
+    }
+
+    /**
+     * Get checked flag
+     * 
+     * @return bool
+     */
+    public function isChecked($flag)
+    {
+        return $this->checked;
     }
 }
