d437562097617441924125f98c7522bb118aa017
thomas <thomas@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-03-22 23:00:29 +0000

    API doc:  - some fixes... more to come
    git-svn-id: http://framework.zend.com/svn/framework/trunk@9006 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- incubator/library/Zend/Build/Manifest/NameConflictException.php
+++ incubator/library/Zend/Build/Manifest/NameConflictException.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Zend Framework
  *
@@ -14,7 +13,8 @@
  * to license@zend.com so we can send you a copy immediately.
  *
  * @category   Zend
- * @package    Zend_Build_Manifest
+ * @package    Zend_Build
+ * @subpackage Zend_Build_Manifest
  * @copyright  Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
--- incubator/library/Zend/Build/Resource/Factory.php
+++ incubator/library/Zend/Build/Resource/Factory.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Zend Framework
  *
@@ -15,6 +14,7 @@
  *
  * @category   Zend
  * @package    Zend_Build
+ * @subpackage Zend_Build_Resource
  * @copyright  Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  * @version    $Id: Interface.php 3412 2007-02-14 22:22:35Z darby $
