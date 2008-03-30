92c68bff6df8b51bd127e91e044b27d675cfdab3
thomas <thomas@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-03-30 20:27:03 +0000

    API doc:  - some fixes
    git-svn-id: http://framework.zend.com/svn/framework/trunk@9102 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/XmlRpc/Server/Exception.php
+++ library/Zend/XmlRpc/Server/Exception.php
@@ -12,10 +12,12 @@
  * obtain it through the world-wide-web, please send an email
  * to license@zend.com so we can send you a copy immediately.
  *
+ * @category   Zend
  * @package    Zend_XmlRpc
  * @subpackage Server
  * @copyright  Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ * @version    $Id$
  */
 
 
