4148050c3a7332446abdaaa07ffc6865b3a2caa0
thomas <thomas@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-03-30 19:04:05 +0000

    API doc:  - some fixes
    git-svn-id: http://framework.zend.com/svn/framework/trunk@9096 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- incubator/library/Zend/Crypt/Math/BigInteger/Bcmath.php
+++ incubator/library/Zend/Crypt/Math/BigInteger/Bcmath.php
@@ -16,8 +16,8 @@
  * @package    Zend_Crypt
  * @subpackage Math
  * @copyright  Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
- * @version    $Id$
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ * @version    $Id: $
  */
 
 /** Zend_Crypt_Math_BigInteger_Interface */
