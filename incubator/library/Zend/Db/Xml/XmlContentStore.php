a9525f13cbbc4b5c54daf6ca1e60448222c5af79
thomas <thomas@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-04-08 22:01:04 +0000

    Incubator:  - coding standard fixes    (no tabs allowed)
    git-svn-id: http://framework.zend.com/svn/framework/trunk@9182 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- incubator/library/Zend/Db/Xml/XmlContentStore.php
+++ incubator/library/Zend/Db/Xml/XmlContentStore.php
@@ -41,106 +41,106 @@ require_once('Zend/Db/Xml/XmlContent.php');
 
 abstract class Zend_Db_Xml_XmlContentStore
 {
-	/**
-	 * Represents a connection handle to the persistence layer
-	 *
-	 * @var Zend_Db_Adapter_Abstract
-	 */
-	protected $_conn;
+    /**
+     * Represents a connection handle to the persistence layer
+     *
+     * @var Zend_Db_Adapter_Abstract
+     */
+    protected $_conn;
 
-	/**
-	 * Zend_Db_Xml_XmlContentStore constructor
-	 * 
-	 * @param Zend_Db_Adapter_Abstract
-	 */
-	public abstract function __construct($conn);
+    /**
+     * Zend_Db_Xml_XmlContentStore constructor
+     * 
+     * @param Zend_Db_Adapter_Abstract
+     */
+    public abstract function __construct($conn);
+
+    /**
+     * Saves the XML data represented by an Zend_Db_Xml_XmlContent object
+     * into the persistence layer.  An array of Zend_Db_Xml_XmlContent
+     * objects can also be passed in.
+     *
+     * @param mixed $doc
+     *
+     * @return integer
+     */
+    public abstract function insert($doc);
+
+    /**
+     * Replaces existing XML data represented by an Zend_Db_Xml_XmlContent object
+     * in the persistence layer. An array of Zend_Db_Xml_XmlContent
+     * objects can also be passed in.
+     *
+     * @param mixed $doc
+     *
+     * @return integer $numUpdated, the number of documents updated
+     */
+    public abstract function update($doc);
+
+
+    /**
+     * Removes the XML data represented by an Zend_Db_Xml_XmlContent object
+     * from the persistence layer. An array of Zend_Db_Xml_XmlContent
+     * objects can also be passed in.
+     *
+     * @param mixed $doc
+     *
+     * @return integer the number of rows deleted
+     */
+    public abstract function delete($doc);
+
+    /**
+     * Removes the XML data given the id of the XML document
+     *
+     * @param integer $id
+     *
+     * @return integer the number of rows deleted
+     */
+    public abstract function deleteById($id);
+
+    /**
+     * Returns all documents in the content store
+     *
+     * @return Zend_Db_Xml_XmlIterator
+     */
+    public abstract function selectAll();
+
+    /**
+     * Finds Zend_Db_Xml_XmlContent documents that match items in array
+     *
+     * @param array $searchParam is an associative array representing element name
+     * and element text pairs.  This is used to perform a simple xpath
+     * search in the persistence layer for docs that match the xpath
+     * expression
+     * 
+     * @param $where, search in DATA or ABOUT
+     * @param array $options, type of search
+     * 
+     * @return Zend_Db_Xml_XmlIterator
+     */
+    public abstract function find($searchParam, $where, $options);
+
+    /**
+     * Returns the Zend_Db_Xml_XmlContent object associated with given id
+     *
+     * @param integer $id
+     *
+     * @return Zend_Db_Xml_XmlIterator
+     */
+    public abstract function findById($id);
 	
-	/**
-	 * Saves the XML data represented by an Zend_Db_Xml_XmlContent object
-	 * into the persistence layer.  An array of Zend_Db_Xml_XmlContent
-	 * objects can also be passed in.
-	 *
-	 * @param mixed $doc
-	 *
-	 * @return integer
-	 */
-	public abstract function insert($doc);
-	
-	/**
-	 * Replaces existing XML data represented by an Zend_Db_Xml_XmlContent object
-	 * in the persistence layer. An array of Zend_Db_Xml_XmlContent
-	 * objects can also be passed in.
-	 *
-	 * @param mixed $doc
-	 *
-	 * @return integer $numUpdated, the number of documents updated
-	 */
-	public abstract function update($doc);
-	
-	
-	/**
-	 * Removes the XML data represented by an Zend_Db_Xml_XmlContent object
-	 * from the persistence layer. An array of Zend_Db_Xml_XmlContent
-	 * objects can also be passed in.
-	 *
-	 * @param mixed $doc
-	 *
-	 * @return integer the number of rows deleted
-	 */
-	public abstract function delete($doc);
-	
-	/**
-	 * Removes the XML data given the id of the XML document
-	 *
-	 * @param integer $id
-	 *
-	 * @return integer the number of rows deleted
-	 */
-	public abstract function deleteById($id);
-	
-	/**
-	 * Returns all documents in the content store
-	 *
-	 * @return Zend_Db_Xml_XmlIterator
-	 */
-	public abstract function selectAll();
-	
-	/**
-	 * Finds Zend_Db_Xml_XmlContent documents that match items in array
-	 *
-	 * @param array $searchParam is an associative array representing element name
-	 * and element text pairs.  This is used to perform a simple xpath
-	 * search in the persistence layer for docs that match the xpath
-	 * expression
-	 * 
-	 * @param $where, search in DATA or ABOUT
-	 * @param array $options, type of search
-	 * 
-	 * @return Zend_Db_Xml_XmlIterator
-	 */
-	public abstract function find($searchParam, $where, $options);
-	
-	/**
-	 * Returns the Zend_Db_Xml_XmlContent object associated with given id
-	 *
-	 * @param integer $id
-	 *
-	 * @return Zend_Db_Xml_XmlIterator
-	 */
-	public abstract function findById($id);
-		
-	/**
-	 * Convenience method to insert a DOM
-	 * directly into the persistence layer
-	 * 
-	 * @param DOMDocument $doc
-	 * 
-	 * @return Zend_Db_Xml_XmlContent
-	 */
-	public function insertDOM(DOMDocument $doc)
-	{
-		$xmlDoc = new Zend_Db_Xml_XmlContent($doc);
-		$this->insert($xmlDoc);
-		return $xmlDoc;
-	}
+    /**
+     * Convenience method to insert a DOM
+     * directly into the persistence layer
+     * 
+     * @param DOMDocument $doc
+     * 
+     * @return Zend_Db_Xml_XmlContent
+     */
+    public function insertDOM(DOMDocument $doc)
+    {
+        $xmlDoc = new Zend_Db_Xml_XmlContent($doc);
+        $this->insert($xmlDoc);
+        return $xmlDoc;
+    }
 }
--- incubator/library/Zend/Db/Xml/XmlIterator.php
+++ incubator/library/Zend/Db/Xml/XmlIterator.php
@@ -34,104 +34,104 @@ require_once('Zend/Db/Xml/XmlContent.php');
  */
 class Zend_Db_Xml_XmlIterator implements Iterator
 {
-	protected $_docIndex = 0;
-	
-	/**
-	 * @var array
-	 */
-	protected $_docs;
-	
-	/**
-	 * Zend_Db_Xml_XmlIterator constructor
-	 *
-	 */
-	public function __construct()
-	{
-		$this->_docs = array();
-	}
-	
-	/**
-	 * Add an Zend_Db_Xml_XmlContent to list
-	 *
-	 * @param Zend_Db_Xml_XmlContent $xmldoc
-	 */
-	public function add($xmldoc)
-	{
-		$this->_docs[] = $xmldoc;
-	}
+    protected $_docIndex = 0;
 
-	/**
-	 * Get the number of documents in the list
-	 * 
-	 * @return integer Zend_Db_Xml_XmlContent count
-	 */
-	public function count()
-	{
-		return count($this->_docs);
-	}
-	
-	/**
-	 * Required by the Iterator interface
-	 *
-	 * @internal
-	 */
-	public function rewind()
-	{
-		$this->_docIndex = 0;
-	}
+    /**
+     * @var array
+     */
+    protected $_docs;
 
-	/**
-	 * Required by the Iterator interface
-	 *
-	 * @internal
-	 *
-	 * @return Zend_Db_Xml_XmlContent the current document
-	 */
-	public function current()
-	{
-		if ($this->valid()) {
-			return $this->_docs[$this->_docIndex];
-		}
-	}
+    /**
+     * Zend_Db_Xml_XmlIterator constructor
+     *
+     */
+    public function __construct()
+    {
+        $this->_docs = array();
+    }
 
-	/**
-	 * Required by the Iterator interface
-	 *
-	 * @internal
-	 *
-	 * @return mixed The current key (starts at 0)
-	 */
-	public function key()
-	{
-		return $this->_docIndex;
-	}
+    /**
+     * Add an Zend_Db_Xml_XmlContent to list
+     *
+     * @param Zend_Db_Xml_XmlContent $xmldoc
+     */
+    public function add($xmldoc)
+    {
+        $this->_docs[] = $xmldoc;
+    }
 
-	/**
-	 * Required by the Iterator interface
-	 *
-	 * @internal
-	 *
-	 * @return mixed The next content document, or null if no more items
-	 */
-	public function next()
-	{
-		++$this->_docIndex;
-		if($this->valid()) {
-			return $this->_docs[$this->_docIndex];
-		} else {
-			return null;
-		}
-	}
+    /**
+     * Get the number of documents in the list
+     * 
+     * @return integer Zend_Db_Xml_XmlContent count
+     */
+    public function count()
+    {
+        return count($this->_docs);
+    }
 
-	/**
-	 * Required by the Iterator interface
-	 *
-	 * @internal
-	 *
-	 * @return boolean whether the iteration is valid
-	 */
-	public function valid()
-	{
-		return (0 <= $this->_docIndex && $this->_docIndex < $this->count());
-	}
+    /**
+     * Required by the Iterator interface
+     *
+     * @internal
+     */
+    public function rewind()
+    {
+        $this->_docIndex = 0;
+    }
+
+    /**
+     * Required by the Iterator interface
+     *
+     * @internal
+     *
+     * @return Zend_Db_Xml_XmlContent the current document
+     */
+    public function current()
+    {
+        if ($this->valid()) {
+            return $this->_docs[$this->_docIndex];
+        }
+    }
+
+    /**
+     * Required by the Iterator interface
+     *
+     * @internal
+     *
+     * @return mixed The current key (starts at 0)
+     */
+    public function key()
+    {
+        return $this->_docIndex;
+    }
+
+    /**
+     * Required by the Iterator interface
+     *
+     * @internal
+     *
+     * @return mixed The next content document, or null if no more items
+     */
+    public function next()
+    {
+        ++$this->_docIndex;
+        if($this->valid()) {
+            return $this->_docs[$this->_docIndex];
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Required by the Iterator interface
+     *
+     * @internal
+     *
+     * @return boolean whether the iteration is valid
+     */
+    public function valid()
+    {
+        return (0 <= $this->_docIndex && $this->_docIndex < $this->count());
+    }
 }
