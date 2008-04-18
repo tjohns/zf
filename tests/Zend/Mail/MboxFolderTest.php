871359ff96b762b7fbeede99999714a50131033d
darby <darby@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-04-18 19:54:38 +0000

    Zend_Mail_MboxFolderTest * add license header and use TestHelper
    git-svn-id: http://framework.zend.com/svn/framework/trunk@9245 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/Mail/MboxFolderTest.php
+++ tests/Zend/Mail/MboxFolderTest.php
@@ -1,32 +1,48 @@
 <?php
-
 /**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
  * @category   Zend
  * @package    Zend_Mail
  * @subpackage UnitTests
+ * @copyright  Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ * @version    $Id$
  */
 
 
 /**
- * Zend_Mail_Storage_Folder_Mbox
+ * Test helper
+ */
+require_once dirname(__FILE__) . '/../../TestHelper.php';
+
+/**
+ * @see Zend_Mail_Storage_Folder_Mbox
  */
 require_once 'Zend/Mail/Storage/Folder/Mbox.php';
 
 /**
- * Zend_Config
+ * @see Zend_Config
  */
 require_once 'Zend/Config.php';
-
-/**
- * PHPUnit test case
- */
-require_once 'PHPUnit/Framework/TestCase.php';
 
 
 /**
  * @category   Zend
  * @package    Zend_Mail
  * @subpackage UnitTests
+ * @copyright  Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 class Zend_Mail_MboxFolderTest extends PHPUnit_Framework_TestCase
 {
