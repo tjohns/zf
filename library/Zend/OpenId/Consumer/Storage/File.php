7e4912a57a149a102537d509372d4d6fef5a94c3
darby <darby@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-04-18 21:00:13 +0000

    Zend_OpenId_Consumer_Storage_File * require exception class file before throwing
    git-svn-id: http://framework.zend.com/svn/framework/trunk@9250 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/OpenId/Consumer/Storage/File.php
+++ library/Zend/OpenId/Consumer/Storage/File.php
@@ -70,24 +70,40 @@ class Zend_OpenId_Consumer_Storage_File extends Zend_OpenId_Consumer_Storage
         $this->_dir = $dir;
         if (!is_dir($this->_dir)) {
             if (!@mkdir($this->_dir, 0700, 1)) {
+                /**
+                 * @see Zend_OpenId_Exception
+                 */
+                require_once 'Zend/OpenId/Exception.php';
                 throw new Zend_OpenId_Exception(
                     'Cannot access storage directory ' . $dir,
                     Zend_OpenId_Exception::ERROR_STORAGE);
             }
         }
         if (($f = fopen($this->_dir.'/assoc.lock', 'w+')) === null) {
+            /**
+             * @see Zend_OpenId_Exception
+             */
+            require_once 'Zend/OpenId/Exception.php';
             throw new Zend_OpenId_Exception(
                 'Cannot create a lock file in the directory ' . $dir,
                 Zend_OpenId_Exception::ERROR_STORAGE);
         }
         fclose($f);
         if (($f = fopen($this->_dir.'/discovery.lock', 'w+')) === null) {
+            /**
+             * @see Zend_OpenId_Exception
+             */
+            require_once 'Zend/OpenId/Exception.php';
             throw new Zend_OpenId_Exception(
                 'Cannot create a lock file in the directory ' . $dir,
                 Zend_OpenId_Exception::ERROR_STORAGE);
         }
         fclose($f);
         if (($f = fopen($this->_dir.'/nonce.lock', 'w+')) === null) {
+            /**
+             * @see Zend_OpenId_Exception
+             */
+            require_once 'Zend/OpenId/Exception.php';
             throw new Zend_OpenId_Exception(
                 'Cannot create a lock file in the directory ' . $dir,
                 Zend_OpenId_Exception::ERROR_STORAGE);
