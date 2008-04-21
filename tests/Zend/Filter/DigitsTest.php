a9c31713b699c07cf4d4dbeb15f27fa577804547
yoshida@zend.co.jp <yoshida@zend.co.jp@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-04-21 06:59:08 +0000

    Fix ZF-2842
    git-svn-id: http://framework.zend.com/svn/framework/trunk@9267 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/Filter/DigitsTest.php
+++ tests/Zend/Filter/DigitsTest.php
@@ -45,10 +45,17 @@ class Zend_Filter_DigitsTest extends PHPUnit_Framework_TestCase
     /**
      * Zend_Filter_Digits object extended for checking whether Unicode PCRE is enabled
      *
-     * @var Zend_Filter_DigitsTest_Filter
+     * @var Zend_Filter_Digits
      */
     protected $_filter;
 
+    /**
+     * Is PCRE is compiled with UTF-8 and Unicode support
+     *
+     * @var mixed
+     **/
+    protected static $_unicodeEnabled;
+    
     /**
      * Creates a new Zend_Filter_Digits object for each test method
      *
@@ -56,7 +63,10 @@ class Zend_Filter_DigitsTest extends PHPUnit_Framework_TestCase
      */
     public function setUp()
     {
-        $this->_filter = new Zend_Filter_DigitsTest_Filter();
+        $this->_filter = new Zend_Filter_Digits();
+        if (null === self::$_unicodeEnabled) {
+            self::$_unicodeEnabled = (@preg_match('/\pL/u', 'a')) ? true : false;
+        }
     }
 
     /**
@@ -66,14 +76,35 @@ class Zend_Filter_DigitsTest extends PHPUnit_Framework_TestCase
      */
     public function testBasic()
     {
-        $valuesExpected = array(
-            'abc123'  => '123',
-            'abc 123' => '123',
-            'abcxyz'  => '',
-            'AZ@#4.3' => '43',
-            '1.23'    => '123',
-            '0x9f'    => '09'
-            );
+        if (self::$_unicodeEnabled && extension_loaded('mbstring')) {
+        	// Filter for the value with mbstring
+	        /**
+	         * The first element of $valuesExpected contains multibyte digit characters.
+	         *   But , Zend_Filter_Digits is expected to return only singlebyte digits.
+	         *
+	         * The second contains multibyte or singebyte space, and also alphabet.
+	         * The third  contains various multibyte characters.
+	         * The last contains only singlebyte digits.
+	         */
+	        $valuesExpected = array(
+	            '1９2八3四８'     => '123',
+	            'Ｃ 4.5B　6'      => '456',
+	            '9壱8＠7．6，5＃4' => '987654',
+	            '789'              => '789'
+	            );
+        } else {
+        	// POSIX named classes are not supported, use alternative 0-9 match
+        	// Or filter for the value without mbstring
+        	$valuesExpected = array(
+	            'abc123'  => '123',
+	            'abc 123' => '123',
+	            'abcxyz'  => '',
+	            'AZ@#4.3' => '43',
+	            '1.23'    => '123',
+	            '0x9f'    => '09'
+	            );
+	    }
+	            
         foreach ($valuesExpected as $input => $output) {
             $this->assertEquals(
                 $output,
@@ -81,48 +112,5 @@ class Zend_Filter_DigitsTest extends PHPUnit_Framework_TestCase
                 "Expected '$input' to filter to '$output', but received '$result' instead"
                 );
         }
-    }
-
-    /**
-     * Ensures that the filter follows expected behavior for multibyte characters
-     *
-     * @return void
-     */
-    public function testMultiByte()
-    {
-        $this->markTestIncomplete('Multibyte testing incomplete');
-        if (!$this->_filter->getUnicodeEnabled()) {
-            $this->markTestSkipped('Multibyte test not run; Unicode PCRE is not supported on this platform');
-        }
-        /**
-         * The first element of $valuesExpected contains multibyte digit characters.
-         *   But , Zend_Filter_Digits is expected to return only singlebyte digits.
-         *
-         * The second contains multibyte or singebyte space, and also alphabet.
-         * The third  contains various multibyte characters.
-         * The last contains only singlebyte digits.
-         */
-        $valuesExpected = array(
-            '1９2八3四８'     => '123',
-            'Ｃ 4.5B　6'      => '456',
-            '9壱8＠7．6，5＃4' => '987654',
-            '789'              => '789'
-            );
-        foreach ($valuesExpected as $input => $output) {
-            $this->assertEquals(
-                $output,
-                $result = $this->_filter->filter($input),
-                "Expected '$input' to filter to '$output', but received '$result' instead"
-                );
-        }
-    }
-}
-
-
-class Zend_Filter_DigitsTest_Filter extends Zend_Filter_Digits
-{
-    public function getUnicodeEnabled()
-    {
-        return self::$_unicodeEnabled;
     }
 }
