10200a06e1a905b754f5802e251178423e2c9792
thomas <thomas@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-05-04 21:26:53 +0000

    Coding Standard changes
    git-svn-id: http://framework.zend.com/svn/framework/trunk@9354 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/Filter/_files/TestNamespace/StringEquals.php
+++ tests/Zend/Filter/_files/TestNamespace/StringEquals.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Zend Framework
  *
@@ -13,11 +12,11 @@
  * obtain it through the world-wide-web, please send an email
  * to license@zend.com so we can send you a copy immediately.
  *
- * @category   Zend
- * @package    Zend_Validate
- * @copyright  Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
- * @license    http://framework.zend.com/license/new-bsd     New BSD License
- * @version    $Id: Digits.php 4135 2007-03-20 12:46:11Z darby $
+ * @category  Zend
+ * @package   Zend_Filter
+ * @copyright Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd     New BSD License
+ * @version   $Id: $
  */
 
 
@@ -29,10 +28,12 @@ require_once 'Zend/Validate/Abstract.php';
 PHPUnit_Util_Filter::addFileToFilter(__FILE__);
 
 /**
- * @category   Zend
- * @package    Zend_Validate
- * @copyright  Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
- * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ * Mock file for testbed
+ *
+ * @category  Zend
+ * @package   Zend_Filter
+ * @copyright Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd     New BSD License
  */
 class TestNamespace_StringEquals extends Zend_Validate_Abstract
 {
@@ -40,10 +41,12 @@ class TestNamespace_StringEquals extends Zend_Validate_Abstract
     const NOT_EQUALS = 'stringNotEquals';
 
     /**
+     * Array with message templates
+     *
      * @var array
      */
     protected $_messageTemplates = array(
-        self::NOT_EQUALS => "Not all strings in the argument are equal"
+        self::NOT_EQUALS => 'Not all strings in the argument are equal'
     );
 
     /**
@@ -52,20 +55,21 @@ class TestNamespace_StringEquals extends Zend_Validate_Abstract
      * Returns true if and only if all the elements of the array argument
      * are equal to one another with string comparison.
      *
-     * @param  array $value
+     * @param  array $value Value to validate
      * @return boolean
      */
-    public function isValid($value)
+    public function isValid(array $value)
     {
         $this->_setValue($value);
 
-        $initial = (string) current((array)$value);
+        $initial = (string) current((array) $value);
         foreach ((array) $value as $element) {
-            if ((string) $element != $initial) {
+            if ((string) $element !== $initial) {
                 $this->_error();
                 return false;
             }
         }
+
         return true;
     }
 
