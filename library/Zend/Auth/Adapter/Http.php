d14ab67bdb611d0c26dc0ca156c018df35a10376
doctorrock83 <doctorrock83@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-05-07 23:10:57 +0000

    Fixed ZF-3092 (Zend_Auth_Adapter_Http : The code allows "downgrade" of the authorization schemes.) Fixed bugs in tests : - array_shift() takes a reference, Zend_Debug used instead - Stub objects must be reseted before each use Added some more test cases
    git-svn-id: http://framework.zend.com/svn/framework/trunk@9403 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Auth/Adapter/Http.php
+++ library/Zend/Auth/Adapter/Http.php
@@ -387,30 +387,23 @@ class Zend_Auth_Adapter_Http implements Zend_Auth_Adapter_Interface
         list($clientScheme) = explode(' ', $authHeader);
         $clientScheme = strtolower($clientScheme);
 
-        if (!in_array($clientScheme, $this->_supportedSchemes)) {
+        // The server can issue multiple challenges, but the client should
+        // answer with only the selected auth scheme.
+        if (!in_array($clientScheme, $this->_supportedSchemes) || !in_array($clientScheme, $this->_acceptSchemes)) {
             $this->_response->setHttpResponseCode(400);
             return new Zend_Auth_Result(
                 Zend_Auth_Result::FAILURE_UNCATEGORIZED,
                 array(),
-                array('Client requested an unsupported authentication scheme')
+                array('Client requested an incorrect or unsupported authentication scheme')
             );
         }
-
-        // The server can issue multiple challenges, but the client should
-        // answer with only one selected auth scheme.
+        
         switch ($clientScheme) {
             case 'basic':
                 $result = $this->_basicAuth($authHeader);
                 break;
             case 'digest':
                 $result = $this->_digestAuth($authHeader);
-                break;
-            default:
-                /**
-                 * @see Zend_Auth_Adapter_Exception
-                 */
-                require_once 'Zend/Auth/Adapter/Exception.php';
-                throw new Zend_Auth_Adapter_Exception('Unsupported authentication scheme');
         }
 
         return $result;
