4aa6a9d828c75b21848f8eac2252ea5d57e1ba96
darby <darby@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-05-08 14:28:41 +0000

    Zend_Gdata_Media_Extension_MediaRestriction * fix phpdoc errors for __construct()
    git-svn-id: http://framework.zend.com/svn/framework/trunk@9410 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Gdata/Media/Extension/MediaRestriction.php
+++ library/Zend/Gdata/Media/Extension/MediaRestriction.php
@@ -35,9 +35,9 @@ require_once 'Zend/Gdata/App/Extension.php';
 class Zend_Gdata_Media_Extension_MediaRestriction extends Zend_Gdata_Extension
 {
 
-    protected $_rootElement = 'restriction'; 
+    protected $_rootElement = 'restriction';
     protected $_rootNamespace = 'media';
-    
+
     /**
      * @var string
      */
@@ -50,10 +50,10 @@ class Zend_Gdata_Media_Extension_MediaRestriction extends Zend_Gdata_Extension
 
     /**
      * Constructs a new MediaRestriction element
-     * 
-     * @var string $text
-     * @var string $relationship
-     * @var string $type
+     *
+     * @param string $text
+     * @param string $relationship
+     * @param string $type
      */
     public function __construct($text = null, $relationship = null,  $type = null)
     {
@@ -67,13 +67,13 @@ class Zend_Gdata_Media_Extension_MediaRestriction extends Zend_Gdata_Extension
     }
 
     /**
-     * Retrieves a DOMElement which corresponds to this element and all 
+     * Retrieves a DOMElement which corresponds to this element and all
      * child properties.  This is used to build an entry back into a DOM
      * and eventually XML text for sending to the server upon updates, or
-     * for application storage/persistence.  
+     * for application storage/persistence.
      *
      * @param DOMDocument $doc The DOMDocument used to construct DOMElements
-     * @return DOMElement The DOMElement representing this element and all 
+     * @return DOMElement The DOMElement representing this element and all
      * child properties.
      */
     public function getDOM($doc = null)
@@ -90,11 +90,11 @@ class Zend_Gdata_Media_Extension_MediaRestriction extends Zend_Gdata_Extension
 
     /**
      * Given a DOMNode representing an attribute, tries to map the data into
-     * instance members.  If no mapping is defined, the name and value are 
+     * instance members.  If no mapping is defined, the name and value are
      * stored in an array.
      *
      * @param DOMNode $attribute The DOMNode attribute needed to be handled
-     */ 
+     */
     protected function takeAttributeFromDOM($attribute)
     {
         switch ($attribute->localName) {
