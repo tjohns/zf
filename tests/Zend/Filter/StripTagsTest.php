64ae5cb68203050b3acf171f2491c401039fa8d6
darby <darby@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-05-19 13:00:45 +0000

    Zend_Filter_StripTagsTest * added test for ZF-3293
    git-svn-id: http://framework.zend.com/svn/framework/trunk@9473 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/Filter/StripTagsTest.php
+++ tests/Zend/Filter/StripTagsTest.php
@@ -469,4 +469,20 @@ class Zend_Filter_StripTagsTest extends PHPUnit_Framework_TestCase
         $expected = '<a href="Some &gt; Text">';
         $this->assertEquals($expected, $this->_filter->filter($input));
     }
+    
+    /**
+     * Ensures that an allowed attribute's value may end with an equals sign '='
+     * 
+     * @return void
+     * @link   http://framework.zend.com/issues/browse/ZF-3293
+     */
+    public function testSomething()
+    {
+        $tagsAllowed = array(
+            'element' => 'attribute'
+        );
+        $this->_filter->setTagsAllowed($tagsAllowed);
+        $input = '<element attribute="a=">contents</element>';
+        $this->assertEquals($input, $this->_filter->filter($input));
+    }
 }
