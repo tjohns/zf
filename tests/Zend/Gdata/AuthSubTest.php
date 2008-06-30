49acd33b18634aaaa1fb7451344f82cf22d1501c
jhartmann <jhartmann@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-06-30 21:32:14 +0000

    [ZF-760]      Unit-test to test secure Authsub.
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@9847 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/Gdata/AuthSubTest.php
+++ tests/Zend/Gdata/AuthSubTest.php
@@ -21,6 +21,7 @@
  */
 
 require_once 'Zend/Gdata/AuthSub.php';
+require_once 'Zend/Gdata/HttpClient.php';
 
 /**
  * @package Zend_Gdata
@@ -61,6 +62,47 @@ class Zend_Gdata_AuthSubTest extends PHPUnit_Framework_TestCase
         // This currently works (no reported bugs) as web browsers will
         // handle the encoding in most cases.
        $this->assertEquals('http://www.otherauthservice.com/accounts/AuthSubRequest?next=http%3A%2F%2Fwww.example.com%2Ffoo.php&scope=http://www.google.com/calendar/feeds&secure=0&session=1', $uri);
+    }
+
+    public function testSecureAuthSubSigning()
+    {
+        $c = new Zend_Gdata_HttpClient();
+        $c->setAuthSubPrivateKeyFile("Zend/Gdata/_files/RsaKey.pem", 
+                                     null, true);
+        $c->setAuthSubToken('abcdefg');
+        $requestData = $c->filterHttpRequest('POST', 
+                                             'http://www.example.com/feed', 
+                                              array(),
+                                              'foo bar', 
+                                              'text/plain');
+
+        $authHeaderCheckPassed = false;
+        $headers = $requestData['headers'];
+        foreach ($headers as $headerName => $headerValue) {
+            if (strtolower($headerName) == 'authorization') {
+                preg_match('/data="([^"]*)"/', $headerValue, $matches);
+                $dataToSign = $matches[1];
+                preg_match('/sig="([^"]*)"/', $headerValue, $matches);
+                $sig = $matches[1];
+                if (function_exists('openssl_verify')) {
+                    $fp = fopen('Zend/Gdata/_files/RsaCert.pem', 'r', true);
+                    $cert = '';
+                    while (!feof($fp)) {
+                        $cert .= fread($fp, 8192);
+                    }
+                    fclose($fp);
+                    $pubkeyid = openssl_get_publickey($cert);
+                    $verified = openssl_verify($dataToSign, 
+                                               base64_decode($sig), $pubkeyid);
+                    $this->assertEquals(1, $verified, 
+                                        'The generated signature was unable ' .
+                                        'to be verified.');
+                    $authHeaderCheckPassed = true;
+                }
+            }
+        }
+        $this->assertEquals(true, $authHeaderCheckPassed, 
+                            'Auth header not found for sig verification.');
     }
 
 }
