7ef3962ec24e84ae3af16ef20d2d32d78f3e12b6
alexander <alexander@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-07-08 18:16:49 +0000

    Zend_Soap_Wsdl: fix for wrong DOMDocoment::loadXML() call. Resolves [ZF-3601], [ZF-3602], [ZF-3603].
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@9996 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Soap/Wsdl.php
+++ library/Zend/Soap/Wsdl.php
@@ -24,7 +24,7 @@ require_once 'Zend/Server/Exception.php';
 
 /**
  * Zend_Soap_Wsdl
- * 
+ *
  * @category   Zend
  * @package    Zend_Soap
  */
@@ -38,7 +38,7 @@ class Zend_Soap_Wsdl {
      * @var object WSDL Root XML_Tree_Node
      */
     private $_wsdl;
-    
+
     /**
      * @var string URI where the WSDL will be available
      */
@@ -74,8 +74,8 @@ class Zend_Soap_Wsdl {
         if ($uri instanceof Zend_Uri_Http) {
             $uri = $this->_uri = $uri->getUri();
         }
-        
-        /** 
+
+        /**
          * @todo change DomDocument object creation from cparsing to construxting using API
          * It also should authomatically escape $name and $uri values if necessary
          */
@@ -86,7 +86,8 @@ class Zend_Soap_Wsdl {
                     xmlns:soap='http://schemas.xmlsoap.org/wsdl/soap/'
                     xmlns:xsd='http://www.w3.org/2001/XMLSchema'
                     xmlns:soap-enc='http://schemas.xmlsoap.org/soap/encoding/'></definitions>";
-        if (!$this->_dom = DomDocument::loadXML($wsdl)) {
+        $this->_dom = new DOMDocument();
+        if (!$this->_dom->loadXML($wsdl)) {
             throw new Zend_Server_Exception('Unable to create DomDocument');
         } else {
             $this->_wsdl = $this->_dom->documentElement;
@@ -106,9 +107,9 @@ class Zend_Soap_Wsdl {
     public function addMessage($name, $parts)
     {
         $message = $this->_dom->createElement('message');
-        
+
         $message->setAttribute('name', $name);
-        
+
         if (sizeof($parts) > 0) {
             foreach ($parts as $name => $type) {
                 $part = $this->_dom->createElement('part');
@@ -117,9 +118,9 @@ class Zend_Soap_Wsdl {
                 $message->appendChild($part);
             }
         }
-        
+
         $this->_wsdl->appendChild($message);
-        
+
         return $message;
     }
 
@@ -134,7 +135,7 @@ class Zend_Soap_Wsdl {
         $portType = $this->_dom->createElement('portType');
         $portType->setAttribute('name', $name);
         $this->_wsdl->appendChild($portType);
-        
+
         return $portType;
     }
 
@@ -152,7 +153,7 @@ class Zend_Soap_Wsdl {
     {
         $operation = $this->_dom->createElement('operation');
         $operation->setAttribute('name', $name);
-        
+
         if (is_string($input) && (strlen(trim($input)) >= 1)) {
             $node = $this->_dom->createElement('input');
             $node->setAttribute('message', $input);
@@ -168,9 +169,9 @@ class Zend_Soap_Wsdl {
             $node->setAttribute('message', $fault);
             $operation->appendChild($node);
         }
-        
+
         $portType->appendChild($operation);
-        
+
         return $operation;
     }
 
@@ -186,9 +187,9 @@ class Zend_Soap_Wsdl {
         $binding = $this->_dom->createElement('binding');
         $binding->setAttribute('name', $name);
         $binding->setAttribute('type', $portType);
-        
+
         $this->_wsdl->appendChild($binding);
-        
+
         return $binding;
     }
 
@@ -238,7 +239,7 @@ class Zend_Soap_Wsdl {
             $node->appendChild($soap_node);
             $operation->appendChild($node);
         }
-        
+
         $binding->appendChild($operation);
 
         return $operation;
@@ -257,9 +258,9 @@ class Zend_Soap_Wsdl {
         $soap_binding = $this->_dom->createElement('soap:binding');
         $soap_binding->setAttribute('style', $style);
         $soap_binding->setAttribute('transport', $transport);
-        
+
         $binding->appendChild($soap_binding);
-        
+
         return $soap_binding;
     }
 
@@ -277,9 +278,9 @@ class Zend_Soap_Wsdl {
         }
         $soap_operation = $this->_dom->createElement('soap:operation');
         $soap_operation->setAttribute('soapAction', $soap_action);
-        
+
         $binding->insertBefore($soap_operation, $binding->firstChild);
-        
+
         return $soap_operation;
     }
 
@@ -299,19 +300,19 @@ class Zend_Soap_Wsdl {
         }
         $service = $this->_dom->createElement('service');
         $service->setAttribute('name', $name);
-        
+
         $port = $this->_dom->createElement('port');
         $port->setAttribute('name', $port_name);
         $port->setAttribute('binding', $binding);
-        
+
         $soap_address = $this->_dom->createElement('soap:address');
         $soap_address->setAttribute('location', $location);
-        
+
         $port->appendChild($soap_address);
         $service->appendChild($port);
-        
+
         $this->_wsdl->appendChild($service);
-        
+
         return $service;
     }
 
@@ -329,7 +330,7 @@ class Zend_Soap_Wsdl {
         } else {
             $node = $input_node;
         }
-        
+
         /** @todo Check if 'documentation' is a correct name for the element (WSDL spec uses 'document') */
         $doc = $this->_dom->createElement('documentation');
         $doc_cdata = $this->_dom->createTextNode($documentation);
@@ -354,7 +355,7 @@ class Zend_Soap_Wsdl {
             $this->_wsdl->appendChild($dom);
         }
     }
-    
+
     /**
      * Return the WSDL as XML
      *
@@ -364,7 +365,7 @@ class Zend_Soap_Wsdl {
     {
            return $this->_dom->saveXML();
     }
-    
+
     /**
      * Return DOM Document
      *
@@ -463,7 +464,7 @@ class Zend_Soap_Wsdl {
 
         foreach ($class->getProperties() as $property) {
             if (preg_match_all('/@var\s+([^\s]+)/m', $property->getDocComment(), $matches)) {
-            	
+
             	/**
             	 * @todo check if 'xsd:element' must be used here (it may not be compatible with using 'complexType'
             	 * node for describing other classes used as attribute types for current class
