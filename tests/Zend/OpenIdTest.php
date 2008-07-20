22b47eb75c4b5514de646c9fc4f60c6f00d50b14
alexander <alexander@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-07-20 11:02:06 +0000

    More accurate  variable handling for OpenId unit tests. Fixes [ZF-3691]
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@10230 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/OpenIdTest.php
+++ tests/Zend/OpenIdTest.php
@@ -50,6 +50,19 @@ require_once 'Zend/OpenId/ResponseHelper.php';
  */
 class Zend_OpenIdTest extends PHPUnit_Framework_TestCase
 {
+    private $_serverVariables;
+
+    public function setUp()
+    {
+        $this->_serverVariables = $_SERVER;
+    }
+
+    public function tearDown()
+    {
+        $_SERVER = $this->_serverVariables;
+    }
+
+
     /**
      * testing testSelfUrl
      *
