8453a482d28552971aca009434aec8f1274ff4a3
matthew <matthew@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-07-24 20:14:17 +0000

    r21088@zlaptop:  matthew | 2008-07-24 16:12:32 -0400  Merge r10380 and r10383 to 1.5 release branch
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/release-1.5@10386 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Feed/Abstract.php
+++ library/Zend/Feed/Abstract.php
@@ -110,10 +110,11 @@ abstract class Zend_Feed_Abstract extends Zend_Feed_Element implements Iterator
     public function __wakeup()
     {
         @ini_set('track_errors', 1);
-        $doc = @DOMDocument::loadXML($this->_element);
+        $doc = new DOMDocument;
+        $status = @$doc->loadXML($this->_element);
         @ini_restore('track_errors');
 
-        if (!$doc) {
+        if (!$status) {
             // prevent the class to generate an undefined variable notice (ZF-2590)
             if (!isset($php_errormsg)) {
                 if (function_exists('xdebug_is_enabled')) {
--- library/Zend/Feed/Entry/Abstract.php
+++ library/Zend/Feed/Entry/Abstract.php
@@ -79,10 +79,11 @@ abstract class Zend_Feed_Entry_Abstract extends Zend_Feed_Element
             if ($element) {
                 // Load the feed as an XML DOMDocument object
                 @ini_set('track_errors', 1);
-                $doc = @DOMDocument::loadXML($element);
+                $doc = new DOMDocument();
+                $status = @$doc->loadXML($element);
                 @ini_restore('track_errors');
 
-                if (!$doc) {
+                if (!$status) {
                     // prevent the class to generate an undefined variable notice (ZF-2590)
                     if (!isset($php_errormsg)) {
                         if (function_exists('xdebug_is_enabled')) {
