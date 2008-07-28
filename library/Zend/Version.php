b48f229d9b97d80e03df0024a12fe295fb9a1e1c
alexander <alexander@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-07-28 12:26:23 +0000

    Update Version.php for the release 1.5.3
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/release-1.5@10506 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Version.php
+++ library/Zend/Version.php
@@ -32,7 +32,7 @@ final class Zend_Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    const VERSION = '1.5.2';
+    const VERSION = '1.5.3';
 
     /**
      * Compare the specified Zend Framework version string $version
