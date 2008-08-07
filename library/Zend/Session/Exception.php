681efc7b8bb1cb92c0f8b44b67e67a869b784923
ralph <ralph@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-08-07 05:17:59 +0000

    ZF-3226 - Merging r10735 to release 1.6 branch from trunk
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/release-1.6@10745 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Session/Exception.php
+++ library/Zend/Session/Exception.php
@@ -53,9 +53,9 @@ class Zend_Session_Exception extends Zend_Exception
      * @param  string $errstr
      * @return void
      */
-    static public function handleSessionStartError($errno, $errstr)
+    static public function handleSessionStartError($errno, $errstr, $errfile, $errline, $errcontext)
     {
-        self::$sessionStartError = $errstr;
+        self::$sessionStartError = $errfile . '(Line:' . $errline . '): Error #' . $errno . ' ' . $errstr . ' ' . $errcontext;
     }
 
     /**
@@ -66,7 +66,9 @@ class Zend_Session_Exception extends Zend_Exception
      * @param  string $errstr
      * @return void
      */
-    static public function handleSilentWriteClose($errno, $errstr)
+    static public function handleSilentWriteClose($errno, $errstr, $errfile, $errline, $errcontext)
     {
+        self::$sessionStartError .= PHP_EOL . $errfile . '(Line:' . $errline . '): Error #' . $errno . ' ' . $errstr . ' ' . $errcontext;
     }
 }
+
