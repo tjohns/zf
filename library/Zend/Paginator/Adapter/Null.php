d6e3314caf5c46c80a253b8636c6560b9de873e6
mratzloff <mratzloff@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-08-08 03:13:50 +0000

    ZF-3873: Merged changes from trunk.
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/release-1.6@10793 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Paginator/Adapter/Null.php
+++ library/Zend/Paginator/Adapter/Null.php
@@ -58,7 +58,7 @@ class Zend_Paginator_Adapter_Null implements Zend_Paginator_Adapter_Interface
      */
     public function getItems($offset, $itemCountPerPage)
     {
-        return array();
+        return array_fill(0, $itemCountPerPage, null);
     }
 
     /**
