032442063c86c85be2f4fc84e056dec4315b52c6
alexander <alexander@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-08-28 12:07:18 +0000

    Update ZF version. Merges cs-11103 from the trunk.
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/release-1.6@11104 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Version.php
+++ library/Zend/Version.php
@@ -32,7 +32,7 @@ final class Zend_Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    const VERSION = '1.6.0RC3';
+    const VERSION = '1.6.0';
 
     /**
      * Compare the specified Zend Framework version string $version
