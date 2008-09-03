e71e2cf9253defa9fb862e88c727bc62747d41e6
fab <fab@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-09-03 22:04:19 +0000

    implement the new extended backend interface for Memcached backend
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@11217 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Cache/Backend/Memcached.php
+++ library/Zend/Cache/Backend/Memcached.php
@@ -23,7 +23,7 @@
 /**
  * @see Zend_Cache_Backend_Interface
  */
-require_once 'Zend/Cache/Backend/Interface.php';
+require_once 'Zend/Cache/Backend/ExtendedInterface.php';
 
 /**
  * @see Zend_Cache_Backend
@@ -37,7 +37,7 @@ require_once 'Zend/Cache/Backend.php';
  * @copyright  Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
-class Zend_Cache_Backend_Memcached extends Zend_Cache_Backend implements Zend_Cache_Backend_Interface
+class Zend_Cache_Backend_Memcached extends Zend_Cache_Backend implements Zend_Cache_Backend_ExtendedInterface
 {
     /**
      * Default Host IP Address or DNS
@@ -126,10 +126,6 @@ class Zend_Cache_Backend_Memcached extends Zend_Cache_Backend implements Zend_Ca
      */
     public function load($id, $doNotTestCacheValidity = false)
     {
-        // WARNING : $doNotTestCacheValidity is not supported !!!
-        if ($doNotTestCacheValidity) {
-            $this->_log("Zend_Cache_Backend_Memcached::load() : \$doNotTestCacheValidity=true is unsupported by the Memcached backend");
-        }
         $tmp = $this->_memcache->get($id);
         if (is_array($tmp)) {
             return $tmp[0];
@@ -172,7 +168,12 @@ class Zend_Cache_Backend_Memcached extends Zend_Cache_Backend implements Zend_Ca
         } else {
             $flag = 0;
         }
-        $result = $this->_memcache->set($id, array($data, time()), $flag, $lifetime);
+        if ($this->test($id)) {
+            // because set and replace seems to have different behaviour
+            $result = $this->_memcache->replace($id, array($data, time(), $lifetime), $flag, $lifetime);
+        } else {
+            $result = $this->_memcache->set($id, array($data, time(), $lifetime), $flag, $lifetime);
+        }
         if (count($tags) > 0) {
             $this->_log("Zend_Cache_Backend_Memcached::save() : tags are unsupported by the Memcached backend");
         }
@@ -247,4 +248,171 @@ class Zend_Cache_Backend_Memcached extends Zend_Cache_Backend implements Zend_Ca
             $this->_log('memcached backend has a limit of 30 days (2592000 seconds) for the lifetime');
         }
     }
+    
+    /**
+     * Return an array of stored cache ids
+     * 
+     * @return array array of stored cache ids (string)
+     */
+    public function getIds()
+    {
+        $this->_log("Zend_Cache_Backend_Memcached::save() : getting the list of cache ids is unsupported by the Memcache backend");
+        return array();
+    }
+    
+    /**
+     * Return an array of stored tags
+	 *
+     * @return array array of stored tags (string)
+     */
+    public function getTags()
+    {
+        $this->_log("Zend_Cache_Backend_Memcached::save() : tags are unsupported by the Memcache backend");
+        return array();
+    }
+    
+    /**
+     * Return an array of stored cache ids which match given tags
+     * 
+     * In case of multiple tags, a logical AND is made between tags
+     *
+     * @param array $tags array of tags
+     * @return array array of matching cache ids (string)
+     */
+    public function getIdsMatchingTags($tags = array())
+    {
+        $this->_log("Zend_Cache_Backend_Memcached::save() : tags are unsupported by the Memcache backend");
+        return array();
+    }
+
+    /**
+     * Return an array of stored cache ids which don't match given tags
+     * 
+     * In case of multiple tags, a logical OR is made between tags
+     *
+     * @param array $tags array of tags
+     * @return array array of not matching cache ids (string)
+     */    
+    public function getIdsNotMatchingTags($tags = array())
+    {
+        $this->_log("Zend_Cache_Backend_Memcached::save() : tags are unsupported by the Memcache backend");
+        return array();
+    }
+    
+    /**
+     * Return the filling percentage of the backend storage
+     *
+     * @return int integer between 0 and 100
+     */
+    public function getFillingPercentage()
+    {
+        $mem = $this->_memcache->getStats();
+        $memSize = $mem['limit_maxbytes'];
+        $memUsed= $mem['bytes'];
+        if ($memSize == 0) {
+            Zend_Cache::throwException('can\'t get memcache memory size');
+        }
+        if ($memUsed > $memSize) {
+            return 100;
+        }
+        return ((int) (100. * ($memUsed / $memSize)));
+    }
+
+    /**
+     * Return an array of metadatas for the given cache id
+     *
+     * The array must include these keys :
+     * - expire : the expire timestamp
+     * - tags : a string array of tags
+     * - mtime : timestamp of last modification time
+     * 
+     * @param string $id cache id
+     * @return array array of metadatas (false if the cache id is not found)
+     */
+    public function getMetadatas($id)
+    {
+        $tmp = $this->_memcache->get($id);
+        if (is_array($tmp)) {
+            $data = $tmp[0];
+            $mtime = $tmp[1];
+            if (!isset($tmp[2])) {
+                // because this record is only with 1.7 release
+                // if old cache records are still there...
+                return false;
+            }
+            $lifetime = $tmp[2];
+            return array(
+                'expire' => $mtime + $lifetime,
+                'tags' => array(),
+                'mtime' => $mtime
+            );
+        }
+        return false;  
+    }
+    
+    /**
+     * Give (if possible) an extra lifetime to the given cache id
+     *
+     * @param string $id cache id
+     * @param int $extraLifetime
+     * @return boolean true if ok
+     */
+    public function touch($id, $extraLifetime)
+    {
+        if ($this->_options['compression']) {
+            $flag = MEMCACHE_COMPRESSED;
+        } else {
+            $flag = 0;
+        }
+        $tmp = $this->_memcache->get($id);
+        if (is_array($tmp)) {
+            $data = $tmp[0];
+            $mtime = $tmp[1];
+            if (!isset($tmp[2])) {
+                // because this record is only with 1.7 release
+                // if old cache records are still there...
+                return false;
+            }
+            $lifetime = $tmp[2];
+            $newLifetime = $lifetime - (time() - $mtime) + $extraLifetime;
+            if ($newLifetime <=0) {
+                return false; 
+            }
+            if ($this->test($id)) {
+                // because set and replace seems to have different behaviour
+                $result = $this->_memcache->replace($id, array($data, time(), $newLifetime), $flag, $newLifetime);
+            } else {
+                $result = $this->_memcache->set($id, array($data, time(), $newLifetime), $flag, $newLifetime);
+            }
+            return true;
+        }
+        return false;
+    }
+    
+    /**
+     * Return an associative array of capabilities (booleans) of the backend
+     * 
+     * The array must include these keys :
+     * - automatic_cleaning (is automating cleaning necessary)
+     * - tags (are tags supported)
+     * - expired_read (is it possible to read expired cache records
+     *                 (for doNotTestCacheValidity option for example))
+     * - priority does the backend deal with priority when saving
+     * - infinite_lifetime (is infinite lifetime can work with this backend)
+     * - get_list (is it possible to get the list of cache ids and the complete list of tags)
+     * 
+     * @return array associative of with capabilities
+     */
+    public function getCapabilities()
+    {
+        return array(
+            'automatic_cleaning' => false,
+            'tags' => false,
+            'expired_read' => false,
+            'priority' => false,
+            'infinite_lifetime' => false,
+            'get_list' => false
+        );
+    }
+    
 }
