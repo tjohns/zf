a673768ca9a292c0de23d674a8713ae4ca35aefb
thomas <thomas@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-09-09 19:22:49 +0000

    Zend_Translate:  - throw user-notice on empty sources
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/release-1.6@11317 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Translate/Adapter.php
+++ library/Zend/Translate/Adapter.php
@@ -273,7 +273,7 @@ abstract class Zend_Translate_Adapter {
             $temp = explode('_', $locale);
             if (isset($this->_translate[$temp[0]]) === false) {
                 // throwing a notice due to possible problems on locale setting
-                trigger_error("No translation for the language '{$locale}' available.", E_NOTICE);
+                trigger_error("No translation for the language '{$locale}' available.", E_USER_NOTICE);
             }
             $locale = $temp[0];
         }
