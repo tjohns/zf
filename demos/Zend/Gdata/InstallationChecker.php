7fabed74f644a6db8062302fd9da99481c2d3250
jhartmann <jhartmann@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-09-20 06:13:57 +0000

    Zend_Gdata ---------- [ZF-4334]   Installation checker script.
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@11451 44c647ce-9c0f-0410-b52a-842ac1e357ba

---
+++ demos/Zend/Gdata/InstallationChecker.php
@@ -0,0 +1,348 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Gdata
+ * @subpackage Demos
+ * @copyright  Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+/**
+ * Simple class to verify that the server that this is run on has a correct
+ * installation of the Zend Framework Gdata component.
+ */
+class InstallationChecker {
+
+    const PHP_EXTENSION_ERRORS = 'PHP Extension Errors';
+    const PHP_MANUAL_LINK_FRAGMENT = 'http://us.php.net/manual/en/book.';
+    const PHP_REQUIREMENT_CHECKER_ID = 'PHP Requirement checker v0.1';
+    const SSL_CAPABILITIES_ERRORS = 'SSL Capabilities Errors';
+    const YOUTUBE_API_CONNECTIVITY_ERRORS = 'YouTube API Connectivity Errors';
+    const ZEND_GDATA_INSTALL_ERRORS = 'Zend Framework Installation Errors';
+    const ZEND_SUBVERSION_URI = 'http://framework.zend.com/download/subversion';
+
+    private static $REQUIRED_EXTENSIONS = array(
+        'ctype', 'dom', 'libxml', 'spl', 'standard', 'openssl');
+
+    private $_allErrors = array(
+        self::PHP_EXTENSION_ERRORS => array(
+            'tested' => false, 'errors' => null),
+        self::ZEND_GDATA_INSTALL_ERRORS => array(
+            'tested' => false, 'errors' => null),
+        self::SSL_CAPABILITIES_ERRORS => array(
+            'tested' => false, 'errors' => null),
+        self::YOUTUBE_API_CONNECTIVITY_ERRORS => array(
+            'tested' => false, 'errors' => null)
+            );
+
+    private $_sapiModeCLI = null;
+
+    /**
+     * Create a new InstallationChecker object and run verifications.
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->determineIfInCLIMode();
+        $this->runAllVerifications();
+        $this->outputResults();
+    }
+
+    /**
+     * Set the sapiModeCLI variable to true if we are running CLI mode.
+     *
+     * @return void
+     */
+    private function determineIfInCLIMode()
+    {
+        if (php_sapi_name() == 'cli') {
+            $this->_sapiModeCLI = true;
+        }
+    }
+
+    /**
+     * Getter for sapiModeCLI variable.
+     *
+     * @return boolean True if we are running in CLI mode.
+     */
+    public function runningInCLIMode()
+    {
+        if ($this->_sapiModeCLI) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Run verifications, stopping at each step if there is a failure.
+     *
+     * @return void
+     */
+    public function runAllVerifications()
+    {
+        if (!$this->validatePHPExtensions()) {
+            return;
+        }
+        if (!$this->validateZendFrameworkInstallation()) {
+            return;
+        }
+        if (!$this->testSSLCapabilities()) {
+            return;
+        }
+        if (!$this->validateYouTubeAPIConnectivity()) {
+            return;
+        }
+    }
+
+    /**
+     * Validate that the required PHP Extensions are installed and available.
+     *
+     * @return boolean False if there were errors.
+     */
+    private function validatePHPExtensions()
+    {
+        $phpExtensionErrors = array();
+        foreach (self::$REQUIRED_EXTENSIONS as $requiredExtension) {
+            if (!extension_loaded($requiredExtension)) {
+                $requiredExtensionError = $requiredExtension .
+                    ' extension missing';
+                $referToLink = null;
+                if ($requiredExtension != 'standard') {
+                    $referToLink  = ' - refer to ' .
+                        self::PHP_MANUAL_LINK_FRAGMENT .
+                        $requiredExtension . '.php';
+                } else {
+                    $referToLink = ' - refer to ' .
+                        self::PHP_MANUAL_LINK_FRAGMENT . 'spl.php';
+                }
+                if ($referToLink) {
+                    $phpExtensionErrors[] = $requiredExtensionError .
+                        $referToLink;
+                }
+            }
+        }
+        $this->_allErrors[self::PHP_EXTENSION_ERRORS]['tested'] = true;
+        if (count($phpExtensionErrors) > 0) {
+            $this->_allErrors[self::PHP_EXTENSION_ERRORS]['errors'] =
+                $phpExtensionErrors;
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Validate that the Gdata component of the Zend Framework is installed
+     * properly. Also checks that the required YouTube API helper methods are
+     * found.
+     *
+     * @return boolean False if there were errors.
+     */
+    private function validateZendFrameworkInstallation()
+    {
+        $zendFrameworkInstallationErrors = array();
+        $zendLoaderPresent = false;
+        try {
+            $zendLoaderPresent = @fopen('Zend/Loader.php', 'r', true);
+        } catch (Exception $e) {
+            $zendFrameworkInstallationErrors[] = 'Exception thrown trying to ' .
+                'access Zend/Loader.php using \'use_include_path\' = true ' .
+                'Make sure you include the Zend Framework in your ' .
+                'include_path which currently contains: "' .
+                ini_get('include_path') . '"';
+        }
+
+        if ($zendLoaderPresent) {
+            @fclose($zendLoaderPresent);
+            require_once('Zend/Loader.php');
+            require_once('Zend/Version.php');
+            Zend_Loader::loadClass('Zend_Gdata_YouTube');
+            Zend_Loader::loadClass('Zend_Gdata_YouTube_VideoEntry');
+            $yt = new Zend_Gdata_YouTube();
+            $videoEntry = $yt->newVideoEntry();
+            if (!method_exists($videoEntry, 'setVideoTitle')) {
+                $zendFrameworkInstallationErrors[] = 'Your version of the ' .
+                    'Zend Framework ' . Zend_Version::VERSION . ' is too old' .
+                    ' to run the YouTube demo application and does not' .
+                    ' contain the new helper methods. Please check out a' .
+                    ' newer version from Zend\'s repository: ' .
+                    self::ZEND_SUBVERSION_URI;
+            }
+        } else {
+            if (count($zendFrameworkInstallationErrors) < 1) {
+                $zendFrameworkInstallationErrors[] = 'Exception thrown trying' .
+                    ' to access Zend/Loader.php using \'use_include_path\' =' .
+                    ' true. Make sure you include the Zend Framework in your' .
+                    ' include_path which currently contains: ' .
+                    ini_get('include_path');
+            }
+        }
+        $this->_allErrors[self::ZEND_GDATA_INSTALL_ERRORS]['tested'] = true;
+
+        if (count($zendFrameworkInstallationErrors) > 0) {
+            $this->_allErrors[self::ZEND_GDATA_INSTALL_ERRORS]['errors'] =
+                $zendFrameworkInstallationErrors;
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Validate that SSL Capabilities are available.
+     *
+     * @return boolean False if there were errors.
+     */
+    private function testSSLCapabilities()
+    {
+        $sslCapabilitiesErrors = array();
+        require_once 'Zend/Loader.php';
+        Zend_Loader::loadClass('Zend_Http_Client');
+
+        $httpClient = new Zend_Http_Client(
+            'https://www.google.com/accounts/AuthSubRequest');
+        $response = $httpClient->request();
+        $this->_allErrors[self::SSL_CAPABILITIES_ERRORS]['tested'] = true;
+
+        if ($response->isError()) {
+            $sslCapabilitiesErrors[] = 'Response from trying to access' .
+                ' \'https://www.google.com/accounts/AuthSubRequest\' ' .
+                $response->getStatus() . ' - ' . $response->getMessage();
+        }
+
+        if (count($sslCapabilitiesErrors) > 0) {
+            $this->_allErrors[self::SSL_CAPABILITIES_ERRORS]['errors'] =
+                $sslCapabilitiesErrors;
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Validate that we can connect to the YouTube API.
+     * 
+     * @return boolean False if there were errors.
+     */
+    private function validateYouTubeAPIConnectivity()
+    {
+        $connectivityErrors = array();
+        require_once 'Zend/Loader.php';
+        Zend_Loader::loadClass('Zend_Gdata_YouTube');
+        $yt = new Zend_Gdata_YouTube();
+        $topRatedFeed = $yt->getTopRatedVideoFeed();
+        if ($topRatedFeed instanceof Zend_Gdata_YouTube_VideoFeed) {
+            if ($topRatedFeed->getTotalResults()->getText() < 1) {
+                $connectivityErrors[] = 'There was less than 1 video entry' .
+                    ' in the \'Top Rated Video Feed\'';
+            }
+        } else {
+            $connectivityErrors[] = 'The call to \'getTopRatedVideoFeed()\' ' .
+                'did not result in a Zend_Gdata_YouTube_VideoFeed object';
+        }
+
+        $this->_allErrors[self::YOUTUBE_API_CONNECTIVITY_ERRORS]['tested'] =
+            true;
+        if (count($connectivityErrors) > 0) {
+            $this->_allErrors[self::YOUTUBE_API_CONNECTIVITY_ERRORS]['tested'] =
+                $connectivityErrors;
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Dispatch a call to outputResultsInHTML or outputResultsInText pending
+     * the current SAPI mode.
+     *
+     * @return void
+     */
+    public function outputResults()
+    {
+        if ($this->_sapiModeCLI) {
+          print $this->getResultsInText();
+        } else {
+          print $this->getResultsInHTML();
+        }
+    }
+
+
+    /**
+     * Return a string representing the results of the verifications.
+     *
+     * @return string A string representing the results.
+     */
+    private function getResultsInText()
+    {
+        $output = "== Ran PHP Verification using CLI ==\n";
+
+        $error_count = 0;
+        foreach($this->_allErrors as $key => $value) {
+            $output .= $key . ' -- ';
+            if (($value['tested'] == true) && (count($value['errors']) == 0)) {
+                $output .= "No errors found\n";
+            } elseif ($value['tested'] == true) {
+                $output .= "Tested\n";
+                $error_count = 0;
+                foreach ($value['errors'] as $error) {
+                    $output .= "Error number: " . $error_count . "\n--" .
+                        $error . "\n";
+                }
+            } else {
+                $output .= "Not tested\n";
+            }
+            $error_count++;
+        }
+        return $output;
+    }
+
+    /**
+     * Return an HTML table representing the results of the verifications.
+     *
+     * @return string An HTML string representing the results.
+     */
+    private function getResultsInHTML()
+    {
+        $html = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" " .
+            "\"http://www.w3.org/TR/html4/strict.dtd\">\n".
+            "<html><head>\n<title>PHP Verification checker</title></head>\n" .
+            "<body>\n<table class=\"verification_table\">" .
+            "<caption>Ran PHP Verification checker on " .
+            gmdate('c') . "</caption>\n";
+
+        $error_count = 0;
+        foreach($this->_allErrors as $key => $value) {
+            $html .= "<tr><td class=\"verification_type\">" . $key . "</td>";
+            if (($value['tested'] == true) && (count($value['errors']) == 0)) {
+                $html .= "<td class=\"tested_ok\">Tested</td></tr>\n" .
+                    "<tr><td colspan=\"2\">No errors found</td></tr>\n";
+            } elseif ($value['tested'] == true) {
+                $html .= "<td class=\"tested_errors\">Tested</td></tr>\n";
+                $error_count = 0;
+                foreach ($value['errors'] as $error) {
+                    $html .= "<tr><td>" . $error_count . "</td>" .
+                        "<td>" . $error . "</td></tr>\n";
+                }
+            } else {
+                $html .= "<td class=\"untested\">Not tested</td></tr>\n";
+
+            }
+            $error_count++;
+        }
+        $html .= "</body></html>";
+        return $html;
+    }
+}
+
+$installationChecker = new InstallationChecker();
+
