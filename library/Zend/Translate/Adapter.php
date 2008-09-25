a77b0bc7d68bfd2bdace5ec1a8b5978ce1babf8f
ralph <ralph@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-09-25 17:50:21 +0000

    ZF-4133 - Merging r11160 from trunk to release 1.6
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/release-1.6@11519 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Translate/Adapter.php
+++ library/Zend/Translate/Adapter.php
@@ -156,7 +156,6 @@ abstract class Zend_Translate_Adapter {
                         if (Zend_Locale::isLocale((string) $filename)) {
                             $locale = (string) $filename;
                         } else {
-                            $found = false;
                             $parts  = explode('.', $filename);
                             $parts2 = array();
                             foreach($parts as $token) {
@@ -196,7 +195,7 @@ abstract class Zend_Translate_Adapter {
                 $this->setLocale($locale);
             }
         }
-        if ((isset($translate[$originate]) === true) and (count($this->_translate[$originate]) > 0)) {
+        if ((isset($this->_translate[$originate]) === true) and (count($this->_translate[$originate]) > 0)) {
             $this->setLocale($originate);
         }
         return $this;
@@ -307,7 +306,7 @@ abstract class Zend_Translate_Adapter {
     {
         $list = array_keys($this->_translate);
         $result = null;
-        foreach($list as $key => $value) {
+        foreach($list as $value) {
             if (!empty($this->_translate[$value])) {
                 $result[$value] = $value;
             }
