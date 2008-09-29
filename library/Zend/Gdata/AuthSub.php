4519cc443f4239206a8036a75264c1029345c287
jhartmann <jhartmann@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-09-29 23:27:11 +0000

    [ZF-4418]     Fixing bug where HttpClient parameters and headers               were not preserved or reset during secure AuthSub.
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@11547 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Gdata/AuthSub.php
+++ library/Zend/Gdata/AuthSub.php
@@ -150,7 +150,18 @@ class Zend_Gdata_AuthSub
                                               $request_uri = self::AUTHSUB_REVOKE_TOKEN_URI)
     {
         $client = self::getHttpClient($token, $client);
-        $client->setUri($request_uri);
+ 
+        if ($client instanceof Zend_Gdata_HttpClient) {
+            $filterResult = $client->filterHttpRequest('GET', $request_uri);
+            $url = $filterResult['url'];
+            $headers = $filterResult['headers'];
+            $client->setHeaders($headers);
+            $client->setUri($url);
+            $client->resetParameters();
+        } else {
+            $client->setUri($request_uri);
+        }
+
         ob_start();
         try {
             $response = $client->request('GET');
@@ -181,7 +192,17 @@ class Zend_Gdata_AuthSub
             $token, $client = null, $request_uri = self::AUTHSUB_TOKEN_INFO_URI)
     {
         $client = self::getHttpClient($token, $client);
-        $client->setUri($request_uri);
+
+        if ($client instanceof Zend_Gdata_HttpClient) {
+            $filterResult = $client->filterHttpRequest('GET', $request_uri);
+            $url = $filterResult['url'];
+            $headers = $filterResult['headers'];
+            $client->setHeaders($headers);
+            $client->setUri($url);
+        } else {
+            $client->setUri($request_uri);
+        }
+
         ob_start();
         try {
             $response = $client->request('GET');
