14520fca1a5f0aec076c67111006bf72e90b7da9
mikaelkael <mikaelkael@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-09-30 19:55:36 +0000

    #ZF4275 : overwrite tests for Oracle specificities
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@11556 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/Db/Profiler/OracleTest.php
+++ tests/Zend/Db/Profiler/OracleTest.php
@@ -147,6 +147,38 @@ class Zend_Db_Profiler_OracleTest extends Zend_Db_Profiler_TestCommon
         $this->assertEquals(array(':bug_id' => 3), $params);
     }
 
+    /**
+     * Ensures that setFilterQueryType() actually filters
+     *
+     * @return void
+     */
+    protected function _testProfilerSetFilterQueryTypeCommon($queryType)
+    {
+        $bugs = $this->_db->quoteIdentifier('zfbugs', true);
+        $bug_id = $this->_db->quoteIdentifier('bug_id', true);
+        $bug_status = $this->_db->quoteIdentifier('bug_status', true);
+
+        $prof = $this->_db->getProfiler();
+        $prof->setEnabled(true);
+
+        $this->assertSame($prof->setFilterQueryType($queryType), $prof);
+        $this->assertEquals($queryType, $prof->getFilterQueryType());
+
+        $this->_db->query("SELECT * FROM $bugs");
+        $this->_db->query("INSERT INTO $bugs ($bug_id, $bug_status) VALUES (:id, :status)", array(':id' => 100,':status' => 'NEW'));
+        $this->_db->query("DELETE FROM $bugs");
+        $this->_db->query("UPDATE $bugs SET $bug_status = :status", array(':status'=>'FIXED'));
+
+        $qps = $prof->getQueryProfiles();
+        $this->assertType('array', $qps, 'Expecting some query profiles, got none');
+        foreach ($qps as $qp) {
+            $qtype = $qp->getQueryType();
+            $this->assertEquals($queryType, $qtype,
+                "Found query type $qtype, which should have been filtered out");
+        }
+
+        $prof->setEnabled(false);
+    }
 
     public function getDriver()
     {
