8c43b2b85fe709fc2340007306fc0ac7ca208a32
mikaelkael <mikaelkael@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-10-02 18:27:57 +0000

    Merge r11574 to branch fixing ZF-3587
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/release-1.6@11596 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Db/Table/Row/Abstract.php
+++ library/Zend/Db/Table/Row/Abstract.php
@@ -143,7 +143,7 @@ abstract class Zend_Db_Table_Row_Abstract
             $info = $table->info();
             $this->_primary = (array) $info['primary'];
         }
-        
+
         $this->init();
     }
 
@@ -672,7 +672,7 @@ abstract class Zend_Db_Table_Row_Abstract
         // retrieve recently updated row using primary keys
         $where = array();
         foreach ($primaryKey as $column => $value) {
-            $tableName = $db->quoteIdentifier($info[Zend_Db_Table_Abstract::NAME]);
+            $tableName = $db->quoteIdentifier($info[Zend_Db_Table_Abstract::NAME], true);
             $type = $metadata[$column]['DATA_TYPE'];
             $columnName = $db->quoteIdentifier($column, true);
             $where[] = $db->quoteInto("{$tableName}.{$columnName} = ?", $value, $type);
@@ -797,7 +797,7 @@ abstract class Zend_Db_Table_Row_Abstract
     public function findDependentRowset($dependentTable, $ruleKey = null, Zend_Db_Table_Select $select = null)
     {
         $db = $this->_getTable()->getAdapter();
-        
+
         if (is_string($dependentTable)) {
             try {
                 @Zend_Loader::loadClass($dependentTable);
@@ -946,7 +946,7 @@ abstract class Zend_Db_Table_Row_Abstract
         } else {
             $select->setTable($matchTable);
         }
-        
+
         // Use adapter from intersection table to ensure correct query construction
         $interInfo = $intersectionTable->info();
         $interDb   = $intersectionTable->getAdapter();
@@ -1013,13 +1013,13 @@ abstract class Zend_Db_Table_Row_Abstract
     public function __call($method, array $args)
     {
         $matches = array();
-        
+
         if (count($args) && $args[0] instanceof Zend_Db_Table_Select) {
             $select = $args[0];
         } else {
             $select = null;
         }
-        
+
         /**
          * Recognize methods for Has-Many cases:
          * findParent<Class>()
