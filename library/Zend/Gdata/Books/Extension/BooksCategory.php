a3b512256b32c3afdcaafd4f9e1db05582b8ffa3
rboyd <rboyd@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-10-04 22:34:48 +0000

    Zend_Gdata ---------- [ZF-4488]   Adds support for the Book Search Data API in Zend_Gdata.             Still needs test cases/documentation before moving over to             a release branch.  Code written by book search team, reviewed             by rboyd.
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@11678 44c647ce-9c0f-0410-b52a-842ac1e357ba

---
+++ library/Zend/Gdata/Books/Extension/BooksCategory.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Gdata
+ * @copyright  Copyright (c) 2005-2008 Zend Technologies USA Inc.
+ * (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+/**
+ * @see Zend_Gdata_Atom_Extension_Category
+ */
+require_once 'Zend/Gdata/Atom/Extension/Category.php';
+
+/**
+ * Describes a books category
+ *
+ * @category   Zend
+ * @package    Zend_Gdata
+ * @copyright  Copyright (c) 2005-2008 Zend Technologies USA Inc.
+ * (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+class Zend_Gdata_Books_Extension_BooksCategory extends
+    Zend_Gdata_Atom_Extension_Category
+{
+
+    /**
+     * Constructor for Zend_Gdata_Books_Extension_BooksCategory which
+     * Describes a books category
+     *
+     * @param DOMElement $element (optional) DOMElement from which this
+     *          object should be constructed.
+     */
+    public function __construct($label = null, $scheme = null, $term = null,
+        $value = null)
+    {
+        foreach (Zend_Gdata_Books::$namespaces as $nsPrefix => $nsUri) {
+            $this->registerNamespace($nsPrefix, $nsUri);
+        }
+        parent::__construct();
+        $this->_label = $label;
+        $this->_scheme = $scheme;
+        $this->_term = $term;
+        $this->_text = $value;
+    }
+
+
+}
+
---
+++ library/Zend/Gdata/Books/Extension/PreviewLink.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Gdata
+ * @copyright  Copyright (c) 2005-2008 Zend Technologies USA Inc.
+ * (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+/**
+ * @see Zend_Gdata_Books_Extension_BooksLink
+ */
+require_once 'Zend/Gdata/Books/Extension/BooksLink.php';
+
+/**
+ * Describes a preview link
+ *
+ * @category   Zend
+ * @package    Zend_Gdata
+ * @copyright  Copyright (c) 2005-2008 Zend Technologies USA Inc.
+ * (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+class Zend_Gdata_Books_Extension_PreviewLink extends
+    Zend_Gdata_Books_Extension_BooksLink
+{
+
+    /**
+     * Constructor for Zend_Gdata_Books_Extension_PreviewLink which
+     * Describes a preview link
+     *
+     * @param DOMElement $element (optional) DOMElement from which this
+     *          object should be constructed.
+     */
+    public function __construct($href = null, $rel = null, $title = null, 
+        $type = null, $value = null)
+    {
+        foreach (Zend_Gdata_Books::$namespaces as $nsPrefix => $nsUri) {
+            $this->registerNamespace($nsPrefix, $nsUri);
+        }
+        parent::__construct();
+        $this->_href = $href;
+        $this->_rel = $rel;
+        $this->_title = $title;
+        $this->_type = $type;
+        $this->_text = $value;
+    }
+
+
+}
+
