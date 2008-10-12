6b307c51651a6231a8e2f8f2d8151926bf6016b7
alexander <alexander@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-10-12 10:34:58 +0000

    Update release version.
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/release-1.6@11893 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Version.php
+++ library/Zend/Version.php
@@ -32,7 +32,7 @@ final class Zend_Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    const VERSION = '1.6.1';
+    const VERSION = '1.6.2';
 
     /**
      * Compare the specified Zend Framework version string $version
