f99beeabfeea717b80644cadca5c77961a1012d3
alexander <alexander@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-10-12 18:41:56 +0000

    Update libraru and tests folders from the release-1.6 branch.
    git-svn-id: http://framework.zend.com/svn/framework/standard/tags/release-1.6.2@11917 44c647ce-9c0f-0410-b52a-842ac1e357ba

---
+++ tests/Zend/Translate/_files/translation_de2.ts
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<!DOCTYPE TS>
+<TS>
+<context>
+    <name>Test</name>
+    <message>
+        <source>Message 1</source>
+        <translation>Nachricht 1</translation>
+    </message>
+    <message>
+        <source>Message 2</source>
+        <translation>Nachricht 2</translation>
+    </message>
+    <message>
+        <source>Küchen Möbel</source>
+        <translation>Cooking Furniture (en)</translation>
+    </message>
+    <message>
+        <source>Cooking Furniture</source>
+        <translation>Küchen Möbel (en)</translation>
+    </message>
+</context>
+</TS>
