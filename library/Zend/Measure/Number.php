099a240f49a6126f2e25e59b49c9896c5b7eed24
thomas <thomas@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-10-14 17:36:42 +0000

    Zend_Measure_Number:  - Coding Standard fixes
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@11956 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Measure/Number.php
+++ library/Zend/Measure/Number.php
@@ -275,7 +275,7 @@ class Zend_Measure_Number extends Zend_Measure_Abstract
      * @param  string  $type  Type from which to convert to decimal
      * @return string
      */
-    private function toDecimal($input, $type)
+    private function _toDecimal($input, $type)
     {
         $value = '';
         // Convert base xx values
@@ -397,7 +397,7 @@ class Zend_Measure_Number extends Zend_Measure_Abstract
             throw new Zend_Measure_Exception('Unknown type of number:' . $type);
         }
 
-        $value = $this->toDecimal($this->getValue(-1), $this->getType(-1));
+        $value = $this->_toDecimal($this->getValue(-1), $this->getType(-1));
         $value = $this->_fromDecimal($value, $type);
 
         $this->_value = $value;
