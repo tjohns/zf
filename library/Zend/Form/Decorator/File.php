5d3d8908dc36edb5c28b72c173e512813f6dae51
thomas <thomas@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-10-22 10:32:31 +0000

    Zend_File_Transfer:  - added content placement to the decorator (ZF-4604)
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@12073 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Form/Decorator/File.php
+++ library/Zend/Form/Decorator/File.php
@@ -40,7 +40,13 @@ class Zend_Form_Decorator_File extends Zend_Form_Decorator_Abstract
      * Attributes that should not be passed to helper
      * @var array
      */
-    protected $_attribBlacklist = array('helper', 'value');
+    protected $_attribBlacklist = array('helper', 'placement', 'separator', 'value');
+
+    /**
+     * Default placement: append
+     * @var string
+     */
+    protected $_placement = 'APPEND';
 
     /**
      * Get attributes to pass to file helper
@@ -49,7 +55,7 @@ class Zend_Form_Decorator_File extends Zend_Form_Decorator_Abstract
      */
     public function getAttribs()
     {
-        $attribs = $this->getOptions();
+        $attribs   = $this->getOptions();
 
         if (null !== ($element = $this->getElement())) {
             $attribs = array_merge($attribs, $element->getAttribs());
@@ -82,13 +88,14 @@ class Zend_Form_Decorator_File extends Zend_Form_Decorator_Abstract
             return $content;
         }
 
-        $name    = $element->getName();
-        $attribs = $this->getAttribs();
+        $name      = $element->getName();
+        $attribs   = $this->getAttribs();
         if (!array_key_exists('id', $attribs)) {
             $attribs['id'] = $name;
         }
 
         $separator = $this->getSeparator();
+        $placement = $this->getPlacement();
         $markup    = array();
         $size      = $element->getMaxFileSize();
         if ($size > 0) {
@@ -109,6 +116,13 @@ class Zend_Form_Decorator_File extends Zend_Form_Decorator_Abstract
         }
 
         $markup = implode($separator, $markup);
-        return $markup;
+
+        switch ($placement) {
+            case self::PREPEND:
+                return $markup . $separator . $content;
+            case self::APPEND:
+            default:
+                return $content . $separator . $markup;
+        }
     }
 }
