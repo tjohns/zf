530ea8c9b8eba0c0751195ca6d3d0afd68d0f6f4
thomas <thomas@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-10-23 20:22:01 +0000

    Zend_Form:  - fixed temporary file under Form_Element
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@12095 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/Form/Element/_files/TransferAdapter/Foo.php
+++ tests/Zend/Form/Element/_files/TransferAdapter/Foo.php
@@ -24,6 +24,11 @@ class Zend_Form_Element_FileTest_Adapter_Foo extends Zend_File_Transfer_Adapter_
         return $this->received;
     }
 
+    public function isUploaded($files = null)
+    {
+        return true;
+    }
+
     public function getProgress()
     {
         return;
