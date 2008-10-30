bb874ced4500bbaa797eacd8495db5c069d2cfab
mikaelkael <mikaelkael@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-10-30 21:36:19 +0000

    ZF-4613: fixed error with Oracle and Zend_Paginator (quote identifier)
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@12209 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Paginator/Adapter/DbSelect.php
+++ library/Zend/Paginator/Adapter/DbSelect.php
@@ -76,14 +76,14 @@ class Zend_Paginator_Adapter_DbSelect implements Zend_Paginator_Adapter_Interfac
     /**
      * Sets the total row count, either directly or through a supplied
      * query.  Without setting this, {@link getPages()} selects the count
-     * as a subquery (SELECT COUNT ... FROM (SELECT ...)).  While this 
-     * yields an accurate count even with queries containing clauses like 
-     * LIMIT, it can be slow in some circumstances.  For example, in MySQL, 
-     * subqueries are generally slow when using the InnoDB storage engine.  
-     * Users are therefore encouraged to profile their queries to find 
+     * as a subquery (SELECT COUNT ... FROM (SELECT ...)).  While this
+     * yields an accurate count even with queries containing clauses like
+     * LIMIT, it can be slow in some circumstances.  For example, in MySQL,
+     * subqueries are generally slow when using the InnoDB storage engine.
+     * Users are therefore encouraged to profile their queries to find
      * the solution that best meets their needs.
      *
-     * @param  Zend_Db_Select|integer $totalRowCount Total row count integer 
+     * @param  Zend_Db_Select|integer $totalRowCount Total row count integer
      *                                               or query
      * @return Zend_Paginator_Adapter_DbSelect $this
      * @throws Zend_Paginator_Exception
@@ -92,25 +92,25 @@ class Zend_Paginator_Adapter_DbSelect implements Zend_Paginator_Adapter_Interfac
     {
         if ($rowCount instanceof Zend_Db_Select) {
             $columns = $rowCount->getPart(Zend_Db_Select::COLUMNS);
-            
+
             $countColumnPart = $columns[0][1];
-            
+
             if ($countColumnPart instanceof Zend_Db_Expr) {
                 $countColumnPart = $countColumnPart->__toString();
             }
-            
+
             // The select query can contain only one column, which should be the row count column
             if (false === strpos($countColumnPart, self::ROW_COUNT_COLUMN)) {
                 /**
                  * @see Zend_Paginator_Exception
                  */
                 require_once 'Zend/Paginator/Exception.php';
-                
+
                 throw new Zend_Paginator_Exception('Row count column not found');
             }
-            
+
             $result = $rowCount->query(Zend_Db::FETCH_ASSOC)->fetch();
-            
+
             $this->_rowCount = count($result) > 0 ? $result[self::ROW_COUNT_COLUMN] : 0;
         } else if (is_integer($rowCount)) {
             $this->_rowCount = $rowCount;
@@ -119,7 +119,7 @@ class Zend_Paginator_Adapter_DbSelect implements Zend_Paginator_Adapter_Interfac
              * @see Zend_Paginator_Exception
              */
             require_once 'Zend/Paginator/Exception.php';
-            
+
             throw new Zend_Paginator_Exception('Invalid row count');
         }
 
@@ -136,7 +136,7 @@ class Zend_Paginator_Adapter_DbSelect implements Zend_Paginator_Adapter_Interfac
     public function getItems($offset, $itemCountPerPage)
     {
         $this->_select->limit($itemCountPerPage, $offset);
-        
+
         return $this->_select->query()->fetchAll();
     }
 
@@ -149,20 +149,20 @@ class Zend_Paginator_Adapter_DbSelect implements Zend_Paginator_Adapter_Interfac
     {
         if ($this->_rowCount === null) {
             $rowCount = clone $this->_select;
-            
+
             /**
              * The DISTINCT and GROUP BY queries only work when selecting one column.
              * The question is whether any RDBMS supports DISTINCT for multiple columns, without workarounds.
              */
             if (true === $rowCount->getPart(Zend_Db_Select::DISTINCT)) {
                 $columnParts = $rowCount->getPart(Zend_Db_Select::COLUMNS);
-                
+
                 $columns = array();
-                
+
                 foreach ($columnParts as $part) {
                 	$columns[] = $part[1];
                 }
-                
+
                 if (count($columns) == 1 && $columns[0] == '*') {
                     $groupPart = null;
                 } else {
@@ -171,10 +171,10 @@ class Zend_Paginator_Adapter_DbSelect implements Zend_Paginator_Adapter_Interfac
             } else {
                 $groupPart = implode(',', $rowCount->getPart(Zend_Db_Select::GROUP));
             }
-            
+
             $countPart  = empty($groupPart) ? 'COUNT(*)' : 'COUNT(DISTINCT ' . $groupPart . ')';
-            $expression = new Zend_Db_Expr($countPart . ' AS ' . self::ROW_COUNT_COLUMN);  
-            
+            $expression = new Zend_Db_Expr($countPart . ' AS ' . $rowCount->getAdapter()->quoteIdentifier(self::ROW_COUNT_COLUMN));
+
             $rowCount->__toString(); // Workaround for ZF-3719 and related
             $rowCount->reset(Zend_Db_Select::COLUMNS)
                      ->reset(Zend_Db_Select::ORDER)
@@ -182,7 +182,7 @@ class Zend_Paginator_Adapter_DbSelect implements Zend_Paginator_Adapter_Interfac
                      ->reset(Zend_Db_Select::GROUP)
                      ->reset(Zend_Db_Select::DISTINCT)
                      ->columns($expression);
-            
+
             $this->setRowCount($rowCount);
         }
 
