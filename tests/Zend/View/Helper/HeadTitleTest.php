58e303a3ed5166e83d46526569bdcefbc03a5de7
sidhighwind <sidhighwind@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-11-11 02:06:54 +0000

    Merging HeadTitle Helper improvement into release branch (ZF-3173)
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/release-1.6@12538 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/View/Helper/HeadTitleTest.php
+++ tests/Zend/View/Helper/HeadTitleTest.php
@@ -185,6 +185,17 @@ class Zend_View_Helper_HeadTitleTest extends PHPUnit_Framework_TestCase
 
         $this->assertEquals('<title>Prefix &amp; Some Title &amp; Postfix</title>', $this->helper->toString());
     }
+
+    public function testCanTranslateTitle()
+    {
+        require_once 'Zend/Translate/Adapter/Ini.php';
+        require_once 'Zend/Registry.php';
+        $adapter = new Zend_Translate_Adapter_Ini(dirname(__FILE__) . '/../../Translate/Adapter/_files/translation_en.ini', 'en');
+        Zend_Registry::set('Zend_Translate', $adapter);
+        $this->helper->enableTranslation();
+        $this->helper->headTitle('Message_1');
+        $this->assertEquals('<title>Message 1 (en)</title>', $this->helper->toString());
+    }
 }
 
 // Call Zend_View_Helper_HeadTitleTest::main() if this source file is executed directly.
