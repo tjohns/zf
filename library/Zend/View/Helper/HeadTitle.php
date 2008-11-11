a1f0de13f6cff67709cc53006447caaf9a5fbd2d
sidhighwind <sidhighwind@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-11-11 02:03:14 +0000

    ZF-3173: Add a way to translate within the HeadTitle View Helper.
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@12537 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/View/Helper/HeadTitle.php
+++ library/Zend/View/Helper/HeadTitle.php
@@ -40,6 +40,19 @@ class Zend_View_Helper_HeadTitle extends Zend_View_Helper_Placeholder_Container_
     protected $_regKey = 'Zend_View_Helper_HeadTitle';
 
     /**
+     * Whether or not auto-translation is enabled
+     * @var boolean
+     */
+    protected $_translate = false;
+
+    /**
+     * Translation object
+     *
+     * @var Zend_Translate_Adapter
+     */
+    protected $_translator;
+
+    /**
      * Retrieve placeholder for title element and optionally set state
      *
      * @param  string $title
@@ -63,20 +76,88 @@ class Zend_View_Helper_HeadTitle extends Zend_View_Helper_Placeholder_Container_
     }
 
     /**
+     * Sets a translation Adapter for translation
+     *
+     * @param  Zend_Translate|Zend_Translate_Adapter $translate
+     * @return Zend_View_Helper_HeadTitle
+     */
+    public function setTranslator($translate)
+    {
+        if ($translate instanceof Zend_Translate_Adapter) {
+            $this->_translator = $translate;
+        } elseif ($translate instanceof Zend_Translate) {
+            $this->_translator = $translate->getAdapter();
+        } else {
+            require_once 'Zend/View/Exception.php';
+            throw new Zend_View_Exception("You must set an instance of Zend_Translate or Zend_Translate_Adapter");
+        }
+        return $this;
+    }
+
+    /*
+     * Retrieve translation object
+     *
+     * If none is currently registered, attempts to pull it from the registry
+     * using the key 'Zend_Translate'.
+     *
+     * @return Zend_Translate_Adapter|null
+     */
+    public function getTranslator()
+    {
+        if (null === $this->_translator) {
+            require_once 'Zend/Registry.php';
+            if (Zend_Registry::isRegistered('Zend_Translate')) {
+                $this->setTranslator(Zend_Registry::get('Zend_Translate'));
+            }
+        }
+        return $this->_translator;
+    }
+
+    /**
+     * Enables translation
+     *
+     * @return Zend_View_Helper_HeadTitle
+     */
+    public function enableTranslation()
+    {
+        $this->_translate = true;
+        return $this;
+    }
+
+    /**
+     * Disables translation
+     *
+     * @return Zend_View_Helper_HeadTitle
+     */
+    public function disableTranslation()
+    {
+        $this->_translate = false;
+        return $this;
+    }
+
+    /**
      * Turn helper into string
      *
      * @param  string|null $indent
+     * @param  string|null $locale
      * @return string
      */
-    public function toString($indent = null)
+    public function toString($indent = null, $locale = null)
     {
         $indent = (null !== $indent)
                 ? $this->getWhitespace($indent)
                 : $this->getIndent();
 
         $items = array();
-        foreach ($this as $item) {
-            $items[] = $item;
+
+        if($this->_translate && $translator = $this->getTranslator()) {
+            foreach ($this as $item) {
+                $items[] = $translator->translate($item, $locale);
+            }
+        } else {
+            foreach ($this as $item) {
+                $items[] = $item;
+            }
         }
 
         $separator = $this->getSeparator();
