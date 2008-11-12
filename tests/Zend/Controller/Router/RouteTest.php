36d78f2c1be707299108de461a278438bf1f8d8a
wil <wil@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-11-12 00:11:03 +0000

    Commented out performance test in Zend_Controller unit tests. This test should be moved to a performance regression test suite when it is available.
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@12576 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/Controller/Router/RouteTest.php
+++ tests/Zend/Controller/Router/RouteTest.php
@@ -520,26 +520,31 @@ class Zend_Controller_Router_RouteTest extends PHPUnit_Framework_TestCase
      * Test guarding performance. Test may be failing on slow systems and shouldn't be failing on production.
      * This test is not critical in nature - it allows keeping changes performant.
      */
-    public function testRoutePerformance()
-    {
-        $count = 10000;
-        $expectedTime = 1;
-
-        $info = "This test may be failing on slow systems and shouldn't be failing on production. Tests if " . ($count / 10) . " complicated routes can be matched in a tenth of a second. Actual test matches " . $count . " times to make the test more reliable.";
-
-        $route = new Zend_Controller_Router_Route('archives/:year/:month/*', array('controller' => 'archive'));
-
-        $time_start = microtime(true);
-
-        for ($i = 1; $i <= $count; $i++) {
-            $values = $route->match('archives/2006/' . $i . '/controller/test/year/' . $i . '/sort/author');
-        }
-
-        $time_end = microtime(true);
-        $time = $time_end - $time_start;
-
-        $this->assertLessThan($expectedTime, $time, $info);
-    }
+    
+    /**
+	 * This test is commented out because performance testing should be done separately from unit
+	 * testing. It will be ported to a performance regression suite when such a suite is available.
+	 */
+//    public function testRoutePerformance()
+//    {
+//        $count = 10000;
+//        $expectedTime = 1;
+//
+//        $info = "This test may be failing on slow systems and shouldn't be failing on production. Tests if " . ($count / 10) . " complicated routes can be matched in a tenth of a second. Actual test matches " . $count . " times to make the test more reliable.";
+//
+//        $route = new Zend_Controller_Router_Route('archives/:year/:month/*', array('controller' => 'archive'));
+//
+//        $time_start = microtime(true);
+//
+//        for ($i = 1; $i <= $count; $i++) {
+//            $values = $route->match('archives/2006/' . $i . '/controller/test/year/' . $i . '/sort/author');
+//        }
+//
+//        $time_end = microtime(true);
+//        $time = $time_end - $time_start;
+//
+//        $this->assertLessThan($expectedTime, $time, $info);
+//    }
 
     public function testForZF2543()
     {
