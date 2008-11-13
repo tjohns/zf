aec0b20f7a5c03821e26a129de0aaa32969ed0ac
alexander <alexander@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-11-13 11:24:29 +0000

    Mark testAdvancedLogging(Zend_Log_Writer_FirebugTest) as skipped (it's actualy correct, but takes too much resources). Merges CS-12606. See [ZF-4934].
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/release-1.7@12607 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/Log/Writer/FirebugTest.php
+++ tests/Zend/Log/Writer/FirebugTest.php
@@ -51,8 +51,8 @@ require_once 'Zend/Controller/Response/Http.php';
  */
 class Zend_Log_Writer_FirebugTest extends PHPUnit_Framework_TestCase
 {
-  
-  
+
+
     protected $_controller = null;
     protected $_request = null;
     protected $_response = null;
@@ -72,12 +72,12 @@ class Zend_Log_Writer_FirebugTest extends PHPUnit_Framework_TestCase
         $suite  = new PHPUnit_Framework_TestSuite("Zend_Log_Writer_FirebugTest");
         $result = PHPUnit_TextUI_TestRunner::run($suite);
     }
-    
+
     public function setUp()
     {
         date_default_timezone_set('America/Los_Angeles');
-        
-        // Reset front controller to reset registered plugins and 
+
+        // Reset front controller to reset registered plugins and
         // registered request/response objects
         Zend_Controller_Front::getInstance()->resetInstance();
 
@@ -89,11 +89,11 @@ class Zend_Log_Writer_FirebugTest extends PHPUnit_Framework_TestCase
         $channel->setResponse($this->_response);
 
         $this->_writer = new Zend_Log_Writer_Firebug();
-        
+
         // Explicitly enable writer as it is disabled by default
         // when running from the command line.
         $this->_writer->setEnabled(true);
-        
+
         $this->_logger = new Zend_Log($this->_writer);
     }
 
@@ -102,11 +102,11 @@ class Zend_Log_Writer_FirebugTest extends PHPUnit_Framework_TestCase
         Zend_Wildfire_Channel_HttpHeaders::destroyInstance();
         Zend_Wildfire_Plugin_FirePhp::destroyInstance();
     }
-    
-    
+
+
     /**
      * Test for ZF-3960
-     * 
+     *
      * Zend_Log_Writer_Firebug should be automatically disabled when
      * run from the command line
      */
@@ -114,14 +114,14 @@ class Zend_Log_Writer_FirebugTest extends PHPUnit_Framework_TestCase
     {
         Zend_Wildfire_Channel_HttpHeaders::destroyInstance();
         Zend_Wildfire_Plugin_FirePhp::destroyInstance();
-      
+
         $log = new Zend_Log();
         $writerFirebug = new Zend_Log_Writer_Firebug();
         $log->addWriter($writerFirebug);
         $log->log('hi', 2);
     }
-    
-    
+
+
     public function testSetFormatter()
     {
         try {
@@ -135,14 +135,14 @@ class Zend_Log_Writer_FirebugTest extends PHPUnit_Framework_TestCase
     public function testLogStyling()
     {
         $this->assertEquals($this->_writer->getDefaultPriorityStyle(),
-                            Zend_Wildfire_Plugin_FirePhp::LOG);                
+                            Zend_Wildfire_Plugin_FirePhp::LOG);
         $this->assertEquals($this->_writer->setDefaultPriorityStyle(Zend_Wildfire_Plugin_FirePhp::WARN),
                             Zend_Wildfire_Plugin_FirePhp::LOG);
         $this->assertEquals($this->_writer->getDefaultPriorityStyle(),
-                            Zend_Wildfire_Plugin_FirePhp::WARN);                
-                            
+                            Zend_Wildfire_Plugin_FirePhp::WARN);
+
         $this->assertEquals($this->_writer->getPriorityStyle(9),
-                            false);                
+                            false);
         $this->assertEquals($this->_writer->setPriorityStyle(9,Zend_Wildfire_Plugin_FirePhp::WARN),
                             true);
         $this->assertEquals($this->_writer->getPriorityStyle(9),
@@ -150,26 +150,28 @@ class Zend_Log_Writer_FirebugTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($this->_writer->setPriorityStyle(9,Zend_Wildfire_Plugin_FirePhp::LOG),
                             Zend_Wildfire_Plugin_FirePhp::WARN);
     }
-    
+
     public function testBasicLogging()
     {
         $message = 'This is a log message!';
-           
+
         $this->_logger->log($message, Zend_Log::INFO);
 
         Zend_Wildfire_Channel_HttpHeaders::getInstance()->flush();
-        
+
         $headers = array();
         $headers['X-Wf-Protocol-1'] = 'http://meta.wildfirehq.org/Protocol/JsonStream/0.2';
         $headers['X-Wf-1-Structure-1'] = 'http://meta.firephp.org/Wildfire/Structure/FirePHP/FirebugConsole/0.1';
         $headers['X-Wf-1-Plugin-1'] = 'http://meta.firephp.org/Wildfire/Plugin/ZendFramework/FirePHP/1.6.2';
         $headers['X-Wf-1-1-1-1'] = '42|[{"Type":"INFO"},"This is a log message!"]|';
-        
+
         $this->assertTrue($this->_response->verifyHeaders($headers));
     }
-    
+
     public function testAdvancedLogging()
     {
+    	$this->markTestSkipped('takes too much resources if whole ZF suite is running.');
+
         $message = 'This is a log message!';
         $label = 'Test Label';
         $table = array('Summary line for the table',
@@ -180,15 +182,15 @@ class Zend_Log_Writer_FirebugTest extends PHPUnit_Framework_TestCase
                        )
                       );
 
-        
+
         $this->_logger->addPriority('TRACE', 8);
         $this->_logger->addPriority('TABLE', 9);
         $this->_writer->setPriorityStyle(8, 'TRACE');
         $this->_writer->setPriorityStyle(9, 'TABLE');
-        
+
         $this->_logger->trace($message);
         $this->_logger->table($table);
-        
+
         try {
           throw new Exception('Test Exception');
         } catch (Exception $e) {
@@ -201,22 +203,22 @@ class Zend_Log_Writer_FirebugTest extends PHPUnit_Framework_TestCase
         } catch (Exception $e) {
             // success
         }
-           
+
         $channel = Zend_Wildfire_Channel_HttpHeaders::getInstance();
         $protocol = $channel->getProtocol(Zend_Wildfire_Plugin_FirePhp::PROTOCOL_URI);
 
         $messages = array(Zend_Wildfire_Plugin_FirePhp::STRUCTURE_URI_FIREBUGCONSOLE=>
                           array(Zend_Wildfire_Plugin_FirePhp::PLUGIN_URI=>
                                 array(1=>'[{"Type":"TABLE"},["Summary line for the table",[["Column 1","Column 2"],["Row 1 c 1"," Row 1 c 2"],["Row 2 c 1"," Row 2 c 2"]]]]')));
-        
+
         $qued_messages = $protocol->getMessages();
-        
+
         unset($qued_messages[Zend_Wildfire_Plugin_FirePhp::STRUCTURE_URI_FIREBUGCONSOLE][Zend_Wildfire_Plugin_FirePhp::PLUGIN_URI][0]);
         unset($qued_messages[Zend_Wildfire_Plugin_FirePhp::STRUCTURE_URI_FIREBUGCONSOLE][Zend_Wildfire_Plugin_FirePhp::PLUGIN_URI][2]);
 
         $this->assertEquals(serialize($qued_messages),
                             serialize($messages));
-    }    
+    }
 }
 
 class Zend_Log_Writer_FirebugTest_Request extends Zend_Controller_Request_Http
