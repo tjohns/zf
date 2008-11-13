a0d438a5c4b22f43f145272b4d25820c66860603
alexander <alexander@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-11-13 15:23:05 +0000

    Zend_Server: docblocks fix (package name is changed from Zend_Controller to Zend_Server).
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@12618 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Server/Reflection.php
+++ library/Zend/Server/Reflection.php
@@ -13,7 +13,7 @@
  * to license@zend.com so we can send you a copy immediately.
  *
  * @category   Zend
- * @package    Zend_Controller
+ * @package    Zend_Server
  * @copyright  Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
