40409f569554fe98ba1ed2811062ec74137adc19
doctorrock83 <doctorrock83@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-11-13 16:24:01 +0000

    fix for ZF-4935 : pdo extension not checked properly
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@12625 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Db/Adapter/Pdo/Abstract.php
+++ library/Zend/Db/Adapter/Pdo/Abstract.php
@@ -279,6 +279,14 @@ abstract class Zend_Db_Adapter_Pdo_Abstract extends Zend_Db_Adapter_Abstract
      */
     public function setFetchMode($mode)
     {
+        //check for PDO extension
+        if (!extension_loaded('pdo')) {
+            /**
+             * @see Zend_Db_Adapter_Exception
+             */
+            require_once 'Zend/Db/Adapter/Exception.php';
+            throw new Zend_Db_Adapter_Exception('The PDO extension is required for this adapter but the extension is not loaded');
+        }
         switch ($mode) {
             case PDO::FETCH_LAZY:
             case PDO::FETCH_ASSOC:
