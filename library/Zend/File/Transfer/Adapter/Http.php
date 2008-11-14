ca4c9427ba3442955ef7078d6b99802374544d0e
thomas <thomas@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-11-14 21:38:24 +0000

    ZF-4922: Zend_File_Transfer - added check for 'file_uploads' ini setting throwing an exception for convinience
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@12659 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/File/Transfer/Adapter/Http.php
+++ library/Zend/File/Transfer/Adapter/Http.php
@@ -38,6 +38,11 @@ class Zend_File_Transfer_Adapter_Http extends Zend_File_Transfer_Adapter_Abstrac
      */
     public function __construct($options = array())
     {
+        if (ini_get('file_uploads') == false) {
+            require_once 'Zend/File/Transfer/Exception.php';
+            throw new Zend_File_Transfer_Exception('File uploads are not allowed in your php config!');
+        }
+
         $this->_files = $this->_prepareFiles($_FILES);
         $this->addValidator('Upload', null, $this->_files);
 
