d8702f0179490c9a7be87ef5325308acbaec04b5
alexander <alexander@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-11-14 16:04:53 +0000

    Create tag folder for ZF 1.7.0
    git-svn-id: http://framework.zend.com/svn/framework/standard/tags/release-1.7.0@12650 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- incubator/library/Zend/Tool/CodeGenerator/Exception.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once 'Zend/Exception.php';
-
-class Zend_Tool_CodeGenerator_Exception extends Zend_Exception
-{}
--- incubator/library/Zend/Tool/Project/Structure/Context/Exception.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once 'Zend/Tool/Project/Structure/Exception.php';
-
-class Zend_Tool_Project_Structure_Context_Exception extends Zend_Tool_Project_Structure_Exception
-{
-    
-}
