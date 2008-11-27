f9c15a1b67ec1a520c8cb6dea8ec0c61d63827b5
dasprid <dasprid@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-11-27 22:09:54 +0000

    Prototype for Zend_Application added to user branch
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/user@12919 44c647ce-9c0f-0410-b52a-842ac1e357ba

---
+++ dasprid/Zend_Application/library/Zend/Application.php
@@ -0,0 +1,177 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Application
+ * @copyright  Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ * @version    $Id$
+ */
+
+/**
+ * @category   Zend
+ * @package    Zend_Application
+ * @copyright  Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+class Zend_Application
+{
+    /**
+     * Plugin loader for decorators
+     *
+     * @var Zend_Loader_PluginLoader
+     */
+    protected $_pluginLoader = null;
+
+    /**
+     * Option keys to skip when calling setOptions()
+     *
+     * @var array
+     */
+    protected $_skipOptions = array(
+        'options',
+        'config',
+    );
+    
+    /**
+     * Registered plugins
+     * 
+     * @var array
+     */
+    protected $_plugins = array();
+
+    /**
+     * Create a instance with options
+     *
+     * @param mixed $options
+     */
+    public function __construct($options = null)
+    {
+        if (is_array($options)) {
+            $this->setOptions($options);
+        } else if ($options instanceof Zend_Config) {
+            $this->setConfig($options);
+        }
+    }
+
+    /**
+     * Set options from array
+     *
+     * @param  array $options Configuration for Zend_Application
+     * @return Zend_Application
+     */
+    public function setOptions(array $options)
+    {
+        foreach ($options as $key => $value) {
+            $method = 'set' . ucfirst($key);
+            if (method_exists($this, $method) && !in_array(strtolower($key), $this->_skipOptions)) {
+                $this->$method($value);
+            } else {
+                $this->registerPlugin($key, $value);
+            } 
+        }
+
+        return $this;
+    }
+
+    /**
+     * Set options from config object
+     *
+     * @param  Zend_Config $config Configuration for Zend_Application
+     * @return Zend_TagCloud
+     */
+    public function setConfig(Zend_Config $config)
+    {
+        return $this->setOptions($config->toArray());
+    }
+    
+    /**
+     * Register a new plugin
+     * 
+     * @param  string $plugin
+     * @param  mixed  $options
+     * @return Zend_Application
+     */
+    public function registerPlugin($plugin, $options = null)
+    {
+        $pluginLoader = $this->getPluginLoader();
+        
+        $className  = $this->getPluginLoader()->load($plugin);
+        $class      = new $className($options);
+        $pluginName = substr(strrchr($plugin, '_'), 1);
+        
+        
+        $this->_plugins[$pluginName] = $class;
+        
+        return $this;
+    }
+       
+    /**
+     * Get a registered plugin
+     *
+     * @param  string $pluginName
+     * @throws Zend_Application_Exception When plugin is not registered
+     * @return Zend_Application_Plugin
+     */
+    public function getPlugin($pluginName)
+    {
+        if (!isset($this->_plugins[$pluginName])) {
+            require_once 'Zend/Application/Exception.php';
+            throw new Zend_Application_Exception(sprintf('Plugin with name "%s" not registered', $pluginName));            
+        }
+
+        return $this->_plugins[$pluginName];
+    }
+    
+    /**
+     * Get the plugin loader for decorators
+     *
+     * @return Zend_Loader_PluginLoader
+     */
+    public function getPluginLoader()
+    {
+        if ($this->_pluginLoader === null) {
+            $prefix     = 'Zend_Application_Plugin_';
+            $pathPrefix = 'Zend/Application/Plugin/';
+
+            require_once 'Zend/Loader/PluginLoader.php';
+            $this->_pluginLoader = new Zend_Loader_PluginLoader(array($prefix => $pathPrefix));
+        }
+
+        return $this->_pluginLoader;
+    }
+    
+    /**
+     * Method overloading for 'init' calls
+     *
+     * @param  string $name
+     * @param  string $arguments
+     * @throws Zend_Application_Exception When the called method is not known
+     * @return Zend_Application
+     */
+    public function __call($name, array $arguments)
+    {
+        if (strpos('init', $name) !== 0) {
+            require_once 'Zend/Application/Exception.php';
+            throw new Zend_Application_Exception(sprintf('Unknown method "%s" called', $name));
+        }
+        
+        $pluginName = substr($name, 4);
+        $plugin     = $this->getPlugin($pluginName);
+        
+        $plugin->init();
+
+        return $this;
+    }
+}
---
+++ dasprid/Zend_Application/library/Zend/Application/Plugin.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category  Zend
+ * @package   Zend_Application
+ * @copyright Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd     New BSD License
+ * @version   $Id$
+ */
+
+/**
+ * Abstract class for application plugins
+ *
+ * @category  Zend
+ * @package   Zend_Application
+ * @copyright Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd     New BSD License
+ */
+abstract class Zend_Application_Plugin
+{
+    /**
+     * Option keys to skip when calling setOptions()
+     *
+     * @var array
+     */
+    protected $_skipOptions = array(
+        'options',
+        'config',
+    );
+
+    /**
+     * Create a instance with options
+     *
+     * @param mixed $options
+     */
+    public function __construct($options = null)
+    {
+        if (is_array($options)) {
+            $this->setOptions($options);
+        } else if ($options instanceof Zend_Config) {
+            $this->setConfig($options);
+        }
+    }
+
+    /**
+     * Set options from array
+     *
+     * @param  array $options Configuration for Zend_Application
+     * @return Zend_Application
+     */
+    public function setOptions(array $options)
+    {
+        foreach ($options as $key => $value) {
+            if (in_array(strtolower($key), $this->_skipOptions)) {
+                continue;
+            }
+
+            $method = 'set' . ucfirst($key);
+            if (method_exists($this, $method)) {
+                $this->$method($value);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Set options from config object
+     *
+     * @param  Zend_Config $config Configuration for Zend_Application
+     * @return Zend_TagCloud
+     */
+    public function setConfig(Zend_Config $config)
+    {
+        return $this->setOptions($config->toArray());
+    }
+    
+    /**
+     * Initiate the plugin
+     *
+     * @return void
+     */
+    abstract public function init();
+}
