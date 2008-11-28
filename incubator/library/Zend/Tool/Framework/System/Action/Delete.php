3202f61f65329b9b430fd3e8237b938421bb373b
alexander <alexander@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-11-28 11:42:30 +0000

    Create tag folder for ZF 1.7.1
    git-svn-id: http://framework.zend.com/svn/framework/standard/tags/release-1.7.1@12932 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- incubator/library/Zend/Tool/Framework/System/Action/Delete.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-class Zend_Tool_Framework_System_Action_Delete extends Zend_Tool_Framework_Provider_Action 
-{}
--- incubator/library/Zend/Tool/Project/Structure/Exception.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-class Zend_Tool_Project_Structure_Exception extends Zend_Exception
-{}
