dff8825777ddecb538125f77886701c64c8cf9ee
matthew <matthew@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-12-03 21:15:02 +0000

    ZF-3636: merge r12570 to trunk
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@13005 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Service/Yahoo/WebResult.php
+++ library/Zend/Service/Yahoo/WebResult.php
@@ -93,8 +93,17 @@ class Zend_Service_Yahoo_WebResult extends Zend_Service_Yahoo_Result
 
         $this->_xpath = new DOMXPath($result->ownerDocument);
         $this->_xpath->registerNamespace('yh', $this->_namespace);
-
-        $this->CacheUrl = $this->_xpath->query('./yh:Cache/yh:Url/text()', $result)->item(0)->data;
-        $this->CacheSize = (int) $this->_xpath->query('./yh:Cache/yh:Size/text()', $result)->item(0)->data;
+		
+        // check if the cache section exists
+        $cacheUrl = $this->_xpath->query('./yh:Cache/yh:Url/text()', $result)->item(0);
+        if ($cacheUrl instanceof DOMNode)
+        {
+        	$this->CacheUrl = $cacheUrl->data;
+        }
+        $cacheSize = $this->_xpath->query('./yh:Cache/yh:Size/text()', $result)->item(0);
+        if ($cacheSize instanceof DOMNode)
+        {
+        	$this->CacheSize = (int) $cacheSize->data;
+        }
     }
 }
