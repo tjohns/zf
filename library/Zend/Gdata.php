3e96635fca0ea7a726c0755a475f44aefe25ee5a
jhartmann <jhartmann@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-12-04 22:29:19 +0000

    [ZF-5161]     Fix @subpackage statement in Zend_Gdata classes
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@13022 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Gdata.php
+++ library/Zend/Gdata.php
@@ -15,6 +15,7 @@
  *
  * @category   Zend
  * @package    Zend_Gdata
+ * @subpackage Gdata
  * @copyright  Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
@@ -35,6 +36,7 @@ require_once 'Zend/Gdata/App.php';
  *
  * @category   Zend
  * @package    Zend_Gdata
+ * @subpackage Gdata
  * @copyright  Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
