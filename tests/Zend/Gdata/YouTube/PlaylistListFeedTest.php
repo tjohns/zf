8fe1a2cd3e7a681ca6314147221c582abf6074fd
jhartmann <jhartmann@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-12-16 08:12:42 +0000

    [ZF-5237]    Allow for support of new XML elements in              PlaylistListEntry in v2 of YouTube API.              Merging r13293 from trunk.
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/release-1.7@13311 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/Gdata/YouTube/PlaylistListFeedTest.php
+++ tests/Zend/Gdata/YouTube/PlaylistListFeedTest.php
@@ -39,59 +39,121 @@ class Zend_Gdata_YouTube_PlaylistListFeedTest extends PHPUnit_Framework_TestCase
         $this->feedText = file_get_contents(
                 'Zend/Gdata/YouTube/_files/PlaylistListFeedDataSample1.xml',
                 true);
+        $this->V2feedText = file_get_contents(
+                'Zend/Gdata/YouTube/_files/PlaylistListFeedDataSampleV2.xml',
+                true);
+
         $this->feed = new Zend_Gdata_YouTube_PlaylistListFeed();
     }
 
-    private function verifyAllSamplePropertiesAreCorrect ($playlistListEntry) {
+    private function verifyAllSamplePropertiesAreCorrect ($playlistListFeed) {
         $this->assertEquals('http://gdata.youtube.com/feeds/users/testuser/playlists',
-            $playlistListEntry->id->text);
-        $this->assertEquals('2007-09-20T20:59:47.530Z', $playlistListEntry->updated->text);
-        $this->assertEquals('http://schemas.google.com/g/2005#kind', $playlistListEntry->category[0]->scheme);
-        $this->assertEquals('http://gdata.youtube.com/schemas/2007#playlistLink', $playlistListEntry->category[0]->term);
-        $this->assertEquals('http://www.youtube.com/img/pic_youtubelogo_123x63.gif', $playlistListEntry->logo->text);
-        $this->assertEquals('text', $playlistListEntry->title->type);
-        $this->assertEquals('testuser\'s Playlists', $playlistListEntry->title->text);;
-        $this->assertEquals('self', $playlistListEntry->getLink('self')->rel);
-        $this->assertEquals('application/atom+xml', $playlistListEntry->getLink('self')->type);
-        $this->assertEquals('http://gdata.youtube.com/feeds/users/testuser/playlists?start-index=1&max-results=25', $playlistListEntry->getLink('self')->href);
-        $this->assertEquals('testuser', $playlistListEntry->author[0]->name->text);
-        $this->assertEquals('http://gdata.youtube.com/feeds/users/testuser', $playlistListEntry->author[0]->uri->text);
-        $this->assertEquals(2, $playlistListEntry->totalResults->text);
+            $playlistListFeed->id->text);
+        $this->assertEquals('2007-09-20T20:59:47.530Z', $playlistListFeed->updated->text);
+        $this->assertEquals('http://schemas.google.com/g/2005#kind', $playlistListFeed->category[0]->scheme);
+        $this->assertEquals('http://gdata.youtube.com/schemas/2007#playlistLink', $playlistListFeed->category[0]->term);
+        $this->assertEquals('http://www.youtube.com/img/pic_youtubelogo_123x63.gif', $playlistListFeed->logo->text);
+        $this->assertEquals('text', $playlistListFeed->title->type);
+        $this->assertEquals('testuser\'s Playlists', $playlistListFeed->title->text);;
+        $this->assertEquals('self', $playlistListFeed->getLink('self')->rel);
+        $this->assertEquals('application/atom+xml', $playlistListFeed->getLink('self')->type);
+        $this->assertEquals('http://gdata.youtube.com/feeds/users/testuser/playlists?start-index=1&max-results=25', $playlistListFeed->getLink('self')->href);
+        $this->assertEquals('testuser', $playlistListFeed->author[0]->name->text);
+        $this->assertEquals('http://gdata.youtube.com/feeds/users/testuser', $playlistListFeed->author[0]->uri->text);
+        $this->assertEquals(2, $playlistListFeed->totalResults->text);
+    }
+
+    private function verifyAllSamplePropertiesAreCorrectV2 ($playlistListFeed) {
+        $this->assertEquals('tag:youtube.com,2008:user:GoogleDevelopers:playlists',
+            $playlistListFeed->id->text);
+        $this->assertEquals('2008-12-10T09:56:03.000Z',
+            $playlistListFeed->updated->text);
+        $this->assertEquals('http://schemas.google.com/g/2005#kind',
+            $playlistListFeed->category[0]->scheme);
+        $this->assertEquals(
+            'http://gdata.youtube.com/schemas/2007#playlistLink',
+            $playlistListFeed->category[0]->term);
+        $this->assertEquals(
+            'http://www.youtube.com/img/pic_youtubelogo_123x63.gif',
+            $playlistListFeed->logo->text);
+        $this->assertEquals('Playlists of GoogleDevelopers',
+            $playlistListFeed->title->text);;
+        $this->assertEquals('self', $playlistListFeed->getLink('self')->rel);
+        $this->assertEquals('application/atom+xml',
+            $playlistListFeed->getLink('self')->type);
+        $this->assertEquals(
+            'http://gdata.youtube.com/feeds/api/users/GoogleDevelopers/' . 
+            'playlists?start-index=1&max-results=25&v=2',
+            $playlistListFeed->getLink('self')->href);
+        $this->assertEquals('GoogleDevelopers',
+            $playlistListFeed->author[0]->name->text);
+        $this->assertEquals('http://gdata.youtube.com/feeds/api/users/' .
+            'googledevelopers', $playlistListFeed->author[0]->uri->text);
+        $this->assertEquals(70, $playlistListFeed->totalResults->text);
     }
 
     public function testEmptyEntryShouldHaveNoExtensionElements() {
         $this->assertTrue(is_array($this->feed->extensionElements));
-        $this->assertTrue(count($this->feed->extensionElements) == 0);
+        $this->assertEquals(0, count($this->feed->extensionElements));
     }
 
     public function testEmptyEntryShouldHaveNoExtensionAttributes() {
         $this->assertTrue(is_array($this->feed->extensionAttributes));
-        $this->assertTrue(count($this->feed->extensionAttributes) == 0);
+        $this->assertEquals(0, count($this->feed->extensionAttributes));
     }
 
     public function testSampleEntryShouldHaveNoExtensionElements() {
         $this->feed->transferFromXML($this->feedText);
         $this->assertTrue(is_array($this->feed->extensionElements));
-        $this->assertTrue(count($this->feed->extensionElements) == 0);
+        $this->assertEquals(0, count($this->feed->extensionElements));
     }
 
     public function testSampleEntryShouldHaveNoExtensionAttributes() {
         $this->feed->transferFromXML($this->feedText);
         $this->assertTrue(is_array($this->feed->extensionAttributes));
-        $this->assertTrue(count($this->feed->extensionAttributes) == 0);
+        $this->assertEquals(0, count($this->feed->extensionAttributes));
+    }
+
+    public function testSampleEntryShouldHaveNoExtensionElementsV2() {
+        $this->feed->transferFromXML($this->V2feedText);
+        print_r($this->feed);
+        $this->assertTrue(is_array($this->feed->extensionElements));
+        $this->assertEquals(0, count($this->feed->extensionElements));
+    }
+
+    public function testSampleEntryShouldHaveNoExtensionAttributesV2() {
+        $this->feed->transferFromXML($this->V2feedText);
+        $this->assertTrue(is_array($this->feed->extensionAttributes));
+        $this->assertEquals(0, count($this->feed->extensionAttributes));
     }
 
     public function testEmptyPlaylistListFeedToAndFromStringShouldMatch() {
-        $entryXml = $this->feed->saveXML();
+        $feedXml = $this->feed->saveXML();
         $newPlaylistListFeed = new Zend_Gdata_YouTube_PlaylistListFeed();
-        $newPlaylistListFeed->transferFromXML($entryXml);
+        $newPlaylistListFeed->transferFromXML($feedXml);
         $newPlaylistListFeedXml = $newPlaylistListFeed->saveXML();
-        $this->assertTrue($entryXml == $newPlaylistListFeedXml);
+        $this->assertTrue($feedXml == $newPlaylistListFeedXml);
+    }
+
+    public function testEmptyPlaylistListFeedToAndFromStringShouldMatchV2() {
+        $this->feed->transferFromXML($this->V2feedText);
+        $feedXml = $this->feed->saveXML();
+        $newPlaylistListFeed = new Zend_Gdata_YouTube_PlaylistListFeed();
+        $newPlaylistListFeed->transferFromXML($feedXml);
+        $newPlaylistListFeed->setMajorProtocolVersion(2);
+        $newPlaylistListFeedXml = $newPlaylistListFeed->saveXML();
+        $this->assertTrue($feedXml == $newPlaylistListFeedXml);
     }
 
     public function testSamplePropertiesAreCorrect () {
         $this->feed->transferFromXML($this->feedText);
         $this->verifyAllSamplePropertiesAreCorrect($this->feed);
+    }
+
+    public function testSamplePropertiesAreCorrectV2 () {
+        $this->feed->transferFromXML($this->V2feedText);
+        $this->feed->setMajorProtocolVersion(2);
+        $this->verifyAllSamplePropertiesAreCorrectV2($this->feed);
     }
 
     public function testConvertPlaylistListFeedToAndFromString() {
