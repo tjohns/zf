c39b15864d2704f11f66dc4030f8ad317eca28fc
beberlei <beberlei@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-12-17 09:47:56 +0000

    ZF-5287 - Added Online-Test for AutoDiscover WSDL generation tests in conjunction with Soap Client and Server
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@13336 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/Soap/AllTests.php
+++ tests/Zend/Soap/AllTests.php
@@ -8,12 +8,13 @@ require_once dirname(__FILE__)."/../../TestHelper.php";
 require_once 'PHPUnit/Framework/TestSuite.php';
 require_once 'PHPUnit/TextUI/TestRunner.php';
 
-require_once 'Zend/Soap/AutoDiscoverTest.php';
 require_once 'Zend/Soap/ClientTest.php';
 require_once 'Zend/Soap/ServerTest.php';
 require_once 'Zend/Soap/WsdlTest.php';
 require_once "Zend/Soap/Wsdl/ArrayOfTypeComplexStrategyTest.php";
 require_once "Zend/Soap/Wsdl/ArrayOfTypeSequenceStrategyTest.php";
+require_once 'Zend/Soap/AutoDiscoverTest.php';
+require_once 'Zend/Soap/AutoDiscover/OnlineTest.php';
 
 class Zend_Soap_AllTests
 {
@@ -26,12 +27,13 @@ class Zend_Soap_AllTests
     {
         $suite = new PHPUnit_Framework_TestSuite('Zend Framework - Zend_Soap');
 
-        $suite->addTestSuite('Zend_Soap_AutoDiscoverTest');
         $suite->addTestSuite('Zend_Soap_ClientTest');
         $suite->addTestSuite('Zend_Soap_ServerTest');
         $suite->addTestSuite('Zend_Soap_WsdlTest');
         $suite->addTestSuite('Zend_Soap_Wsdl_ArrayOfTypeComplexStrategyTest');
         $suite->addTestSuite('Zend_Soap_Wsdl_ArrayOfTypeSequenceStrategyTest');
+        $suite->addTestSuite('Zend_Soap_AutoDiscoverTest');
+        $suite->addTestSuite('Zend_Soap_AutoDiscover_OnlineTest');
 
         return $suite;
     }
