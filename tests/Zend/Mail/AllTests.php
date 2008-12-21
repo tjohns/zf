c0c41e8adee3bc4e482e3d86a80a62f122a041fb
alexander <alexander@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-12-21 10:01:18 +0000

    Fix for missing TestHelper in Zend_Mail_AllTests.php. Closes [ZF-5061].
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@13406 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/Mail/AllTests.php
+++ tests/Zend/Mail/AllTests.php
@@ -3,6 +3,12 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
     define('PHPUnit_MAIN_METHOD', 'Zend_Mail_AllTests::main');
 }
 
+/**
+ * Test helper
+ */
+require_once dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'TestHelper.php';
+
+
 if (!defined('TESTS_ZEND_MAIL_POP3_ENABLED')) {
     if (is_readable('TestConfiguration.php')) {
         require_once 'TestConfiguration.php';
