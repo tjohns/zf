e8de94ca1e8f3de8878d0119548f3272a184eee9
ralph <ralph@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-01-06 17:05:22 +0000

    GENERAL - Updating Zend_Version in trunk to reflect 1.8.0devel
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@13524 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Version.php
+++ library/Zend/Version.php
@@ -32,7 +32,7 @@ final class Zend_Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    const VERSION = '1.7.2';
+    const VERSION = '1.8.0devel';
 
     /**
      * Compare the specified Zend Framework version string $version
