e890c7ea2149b26a2a17e3e9c28a493c8fe9c0d9
matthew <matthew@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-01-15 15:02:55 +0000

    ZF-5537: Add missing class level docblock to Zend_Dom_Exception
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@13646 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Dom/Exception.php
+++ library/Zend/Dom/Exception.php
@@ -21,6 +21,13 @@
 /** Zend_Exception */
 require_once 'Zend/Exception.php';
 
+/**
+ * Zend_Dom Exceptions
+ * 
+ * @package    Zend_Dom
+ * @copyright  Copyright (C) 2008 - Present, Zend Technologies, Inc.
+ * @license    New BSD {@link http://framework.zend.com/license/new-bsd}
+ */
 class Zend_Dom_Exception extends Zend_Exception
 {
 }
