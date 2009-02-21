66f7ca6cd3ee216af1637ef5aa464c08dcb577cf
yoshida@zend.co.jp <yoshida@zend.co.jp@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-02-21 11:24:22 +0000

    [ZF-5580]Switch place of domain and expires in phpDoc
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@14130 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Http/Cookie.php
+++ library/Zend/Http/Cookie.php
@@ -90,8 +90,8 @@ class Zend_Http_Cookie
      *
      * @param string $name
      * @param string $value
-     * @param int $expires
      * @param string $domain
+     * @param int $expires
      * @param string $path
      * @param bool $secure
      */
@@ -222,7 +222,7 @@ class Zend_Http_Cookie
 
         // Make sure we have a valid Zend_Uri_Http object
         if (! ($uri->valid() && ($uri->getScheme() == 'http' || $uri->getScheme() =='https'))) {
-            require_once 'Zend/Http/Exception.php';    
+            require_once 'Zend/Http/Exception.php';
             throw new Zend_Http_Exception('Passed URI is not a valid HTTP or HTTPS URI');
         }
 
