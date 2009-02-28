ee2af0ca31a4d086a1799c2afad8191cb3cfea22
matthew <matthew@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-02-28 21:53:47 +0000

    ZF-5912: Fix @subpackage annotation
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@14191 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/View/Helper/Placeholder/Container/Abstract.php
+++ library/Zend/View/Helper/Placeholder/Container/Abstract.php
@@ -23,7 +23,7 @@
  * Abstract class representing container for placeholder values
  *
  * @package    Zend_View
- * @subpackage Helpers
+ * @subpackage Helper
  * @copyright  Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
