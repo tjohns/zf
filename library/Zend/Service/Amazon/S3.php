9d777d31505685f3ab3465366d2e6c4ea4c2c4a2
stas <stas@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-03-18 23:38:49 +0000

    bugfix
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@14371 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Service/Amazon/S3.php
+++ library/Zend/Service/Amazon/S3.php
@@ -410,7 +410,7 @@ class Zend_Service_Amazon_S3 extends Zend_Service_Abstract
         $client->resetParameters();
         // Work around buglet in HTTP client - it doesn't clean headers
         // Remove when ZHC is fixed
-        $client->setHeaders(array('Content-MD5' => null, 'Expect' => null, 'Range' => null));
+        $client->setHeaders(array('Content-MD5' => null, 'Expect' => null, 'Range' => null, "x-amz-acl" => null));
         $client->setUri(self::S3_ENDPOINT.'/'.$path);
         $client->setHeaders($headers);
 
