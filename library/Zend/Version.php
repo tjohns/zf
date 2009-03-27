6f3eb8224acb976d507eb15f891a734cbaa18249
alexander <alexander@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-03-27 13:20:46 +0000

    ZF version update.
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/release-1.7@14509 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Version.php
+++ library/Zend/Version.php
@@ -32,7 +32,7 @@ final class Zend_Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    const VERSION = '1.7.7';
+    const VERSION = '1.7.8';
 
     /**
      * Compare the specified Zend Framework version string $version
