6afb37af974d6a61f0b682726d6ed3088ae9db7a
alexander <alexander@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-04-07 14:39:00 +0000

    Update ZF version
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@14737 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Version.php
+++ library/Zend/Version.php
@@ -32,7 +32,7 @@ final class Zend_Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    const VERSION = '1.8.0PR';
+    const VERSION = '1.8.0alpha';
 
     /**
      * Compare the specified Zend Framework version string $version
