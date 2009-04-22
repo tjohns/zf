43f7721b26f4fdfd856a445a262e9eff4999579a
ralph <ralph@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-04-22 13:18:01 +0000

    ZF-6372 - Fix for missing require_once for exception class in Zend_Reflection_Docblock
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@15067 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Reflection/Docblock.php
+++ library/Zend/Reflection/Docblock.php
@@ -98,6 +98,7 @@ class Zend_Reflection_Docblock implements Reflector
         if ($commentOrReflector instanceof Reflector) {
             $this->_reflector = $commentOrReflector;
             if (!method_exists($commentOrReflector, 'getDocComment')) {
+                require_once 'Zend/Reflection/Exception.php';
                 throw new Zend_Reflection_Exception('Reflector must contain method "getDocComment"');
             }
             $docComment = $commentOrReflector->getDocComment();
@@ -110,10 +111,12 @@ class Zend_Reflection_Docblock implements Reflector
         } elseif (is_string($commentOrReflector)) {
             $docComment = $commentOrReflector;
         } else {
+            require_once 'Zend/Reflection/Exception.php';
             throw new Zend_Reflection_Exception(get_class($this) . ' must have a (string) DocComment or a Reflector in the constructor.');
         }
         
         if ($docComment == '') {
+            require_once 'Zend/Reflection/Exception.php';
             throw new Zend_Reflection_Exception('DocComment cannot be empty.');
         }
         
