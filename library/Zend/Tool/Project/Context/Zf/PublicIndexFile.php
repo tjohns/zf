4076c0af1d3dd74bd445a4b31c718049f8095f97
matthew <matthew@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-04-27 01:25:48 +0000

    ZF-6341: add library/ to include_path by default
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@15184 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Tool/Project/Context/Zf/PublicIndexFile.php
+++ library/Zend/Tool/Project/Context/Zf/PublicIndexFile.php
@@ -71,6 +71,12 @@ defined('APPLICATION_PATH')
 defined('APPLICATION_ENV')
     || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
 
+// Ensure library/ is on include_path
+set_include_path(implode(PATH_SEPARATOR, array(
+    realpath(APPLICATION_PATH . '/../library'),
+    get_include_path(),
+)));
+
 /** Zend_Application */
 require_once 'Zend/Application.php';  
 
