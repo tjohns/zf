344adf4f6d7c4d801d9d7d7ac5adf03340adb90b
ralph <ralph@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-05-18 13:40:23 +0000

    ZF-6072 Proposed solution for ZF-6072 commited to ralph's user branch
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/user@15624 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- ralph/ZendSession/Zend/Session.php
+++ ralph/ZendSession/Zend/Session.php
@@ -785,6 +785,7 @@ class Zend_Session extends Zend_Session_Abstract
     public static function namespaceUnset($namespace)
     {
         parent::_namespaceUnset($namespace);
+        Zend_Session_Namespace::resetSingleInstance($namespace);
     }
 
 
