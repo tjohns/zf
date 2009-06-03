6c5839cc68837a5ddd77a5cbae798ecf2d856ab9
stas <stas@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-06-03 07:48:21 +0000

    fix ZF-6675
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/release-1.8@15879 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Amf/Parse/Amf3/Deserializer.php
+++ library/Zend/Amf/Parse/Amf3/Deserializer.php
@@ -250,7 +250,7 @@ class Zend_Amf_Parse_Amf3_Deserializer extends Zend_Amf_Parse_Deserializer
 
         // Create a holder for the array in the reference list
         $data = array();
-        $this->_referenceObjects[] &= $data;
+        $this->_referenceObjects[] =& $data;
         $key = $this->readString();
 
         // Iterating for string based keys.
