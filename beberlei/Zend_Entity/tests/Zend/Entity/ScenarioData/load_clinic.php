f32e388d59344cc2135743b787c10d4cba4d41e2
beberlei <beberlei@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-06-14 09:39:25 +0000

    User branch commit
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/user@16063 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- beberlei/Zend_Entity/tests/Zend/Entity/ScenarioData/load_clinic.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once "Clinic/Entities/Clinic_Bed.php";
-require_once "Clinic/Entities/Clinic_Occupancy.php";
-require_once "Clinic/Entities/Clinic_Patient.php";
-require_once "Clinic/Entities/Clinic_Station.php";
--- beberlei/Zend_Entity/tests/Zend/Entity/ScenarioData/load_invoice.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once "Invoice/Entities/Account.php";
-require_once "Invoice/Entities/Address.php";
-require_once "Invoice/Entities/Customer.php";
-require_once "Invoice/Entities/Invoice.php";
-require_once "Invoice/Entities/InvoiceItem.php";
-require_once "Invoice/Entities/Item.php";
