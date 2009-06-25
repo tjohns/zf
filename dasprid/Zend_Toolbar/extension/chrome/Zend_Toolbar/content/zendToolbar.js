add7bee18bd976754e6c7086e566f122f66e63e7
dasprid <dasprid@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-06-25 21:46:41 +0000

    Created proper javascript structure and Makefile
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/user@16295 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- dasprid/Zend_Toolbar/extension/chrome/Zend_Toolbar/content/zendToolbar.js
+++ dasprid/Zend_Toolbar/extension/chrome/Zend_Toolbar/content/zendToolbar.js
@@ -1,133 +1,140 @@
-var Zend_Toolbar = {};
-
-(function()
-{
-    // Registration
-    var namespaces = [];
-    this.ns = function(fn)
-    {
-        var ns = {};
-        namespaces.push(fn, ns);
-        return ns;
-    };
-    
-    // Initialization
-    this._initialize = function()
-    {
-        for (var i = 0; i < namespaces.length; i += 2) {
-            var fn = namespaces[i];
-            var ns = namespaces[i+1];
-            fn.apply(ns);
-        }
-        
-        Zend_Toolbar.initialize();
-    };
-    
-    // Clean up
-    this._shutdown = function()
-    {
-        Zend_Toolbar.shutdown();
-        
-        window.removeEventListener('load', Zend_Toolbar._initialize, false);
-        window.removeEventListener('unload', Zend_Toolbar._shutdown, false);
-    };
-    
-    // Register handlers to maintain extension life cycle.
-    window.addEventListener('load', Zend_Toolbar._initialize, false);
-    window.addEventListener('unload', Zend_Toolbar._shutdown, false);
-}).apply(Zend_Toolbar);
-
-Zend_Toolbar.LIB = {
-    // Extension singleton shortcut
-    app: Zend_Toolbar,
-
-    // XPCOM shortcuts
-    Cc: Components.classes,
-    Ci: Components.interfaces,
-    Cr: Components.results,
-
-    // In case firebug (tracing console) is not installed (see README)
-    console: {
-        enabled: false,
-        dump: function() {}
-    },
-};
-
-
-
-    
-
 /**
- * Zend_Toolbar Bootstrap
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Toolbar
+ * @copyright  Copyright (c) 2005-2009 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 
-Zend_Toolbar.ns(function() { with (Zend_Toolbar.LIB) {
 
-  
-  var pageListener = {
+/**
+ * Toolbar
+ *
+ * @category   Zend
+ * @package    Zend_Toolbar
+ * @copyright  Copyright (c) 2005-2009 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+var Zend_Toolbar = function()
+{   
+    /**
+     * Public methods and properties
+     */
+    var public = {
+        /**
+         * Initialize the toolbar
+         */
+        construct: function()
+        {
+            app.wildfireToolbarPlugin = new Wildfire.Plugin.FrameworkToolbar();
+            app.wildfireToolbarPlugin.init();
+    
+            gBrowser.addProgressListener(private.pageListener, private.Ci.nsIWebProgress.NOTIFY_STATE_DOCUMENT);
+        },
 
-    onStateChange: function(aWebProgress, aRequest, aFlag, aStatus)
-    {
-      // TODO: Only respond to top-level page loads 
-      if(aFlag & Ci.nsIWebProgressListener.STATE_STOP)
-      {
-        console.dump('Received Response from: ' + aRequest.name, aRequest, 'Wildfire');
-        
-        aRequest.visitResponseHeaders({
-          visitHeader: function(name, value)
-          {
-            app.wildfireToolbarPlugin.channel.messageReceived(name, value);
-          }
-        });
-        app.wildfireToolbarPlugin.channel.allMessagesReceived();
-
-        if(app.wildfireToolbarPlugin.hasData()) {
-
-          var data = app.wildfireToolbarPlugin.getData();
-
-          console.dump('Data received', data, 'Wildfire');
-
-
-          // TODO: Deal with data
-
-          
-        } else {
-          console.dump('No data received', null, 'Wildfire');
+        /**
+         * Shutdown the toolbar
+         */
+        destruct: function()
+        {
+            gBrowser.removeProgressListener(private.pageListener, private.Ci.nsIWebProgress.NOTIFY_STATE_DOCUMENT);
         }
-      }
-      return 0;
-    },
-  
-    onLocationChange: function(aProgress, aRequest, aURI) {return 0;},
-    onProgressChange: function() {return 0;},
-    onStatusChange: function() {return 0;},
-    onSecurityChange: function() {return 0;},
-    onLinkIconAvailable: function() {return 0;},
-    
-    QueryInterface: function(aIID)
-    {
-      if (aIID.equals(Ci.nsIWebProgressListener) ||
-          aIID.equals(Ci.nsISupportsWeakReference) ||
-          aIID.equals(Ci.nsISupports)) {
-        return this;
-      }
-      throw Cr.NS_NOINTERFACE;
     }
-  }
-  
-  Zend_Toolbar.initialize = function() {
-
-    app.wildfireToolbarPlugin = new Wildfire.Plugin.FrameworkToolbar();
-    app.wildfireToolbarPlugin.init();
-
-    gBrowser.addProgressListener(pageListener, Ci.nsIWebProgress.NOTIFY_STATE_DOCUMENT);
     
-  }
+    /**
+     * Private methods and properties
+     */
+    var private = {
+        /**
+         * XPCOM shotcuts
+         */
+        Cc: Components.classes,
+        Ci: Components.interfaces,
+        Cr: Components.results,
+            
+        /**
+         * Page load listener
+         * 
+         * @todo Only respond to top-level page loads
+         */
+        pageListener: {
+            onStateChange: function(aWebProgress, aRequest, aFlag, aStatus)
+            {
+                if (aFlag & this.Ci.nsIWebProgressListener.STATE_STOP) {
+                    aRequest.visitResponseHeaders({
+                        visitHeader: function(name, value) 
+                        {
+                            app.wildfireToolbarPlugin.channel.messageReceived(name, value);
+                        }
+                    });
+                    
+                    app.wildfireToolbarPlugin.channel.allMessagesReceived();
 
-  Zend_Toolbar.shutdown = function() {
-
-    gBrowser.removeProgressListener(pageListener, Ci.nsIWebProgress.NOTIFY_STATE_DOCUMENT);
+                    if (app.wildfireToolbarPlugin.hasData()) {
+                        var data = app.wildfireToolbarPlugin.getData();
+                    } else {
+                        // Hide toolbar
+                    }
+                }
+                
+                return 0;
+            },
+        
+            onLocationChange: function(aProgress, aRequest, aURI)
+            {
+                return 0;
+            },
+            
+            onProgressChange: function()
+            {
+                return 0;
+            },
+            
+            onStatusChange: function()
+            {
+                return 0;
+            },
+            
+            onSecurityChange: function()
+            {
+                return 0;
+            },
+            
+            onLinkIconAvailable: function()
+            {
+                return 0;
+            },
+          
+            QueryInterface: function(aIID)
+            {
+                if (aIID.equals(this.Ci.nsIWebProgressListener) ||
+                    aIID.equals(this.Ci.nsISupportsWeakReference) ||
+                    aIID.equals(this.Ci.nsISupports))
+                {
+                    return this;
+                }
+                
+                throw this.Cr.NS_NOINTERFACE;
+            }
+        }
+    };
     
-  }
+    return public;
+};   
 
-}});
+/**
+ * Register constructor and destructor
+ */
+window.addEventListener('load',   Zend_Toolbar.construct, false);
+window.addEventListener('unload', Zend_Toolbar.destruct,  false);
