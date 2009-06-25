79969073231b499d811a93437559212883a20ccb
dasprid <dasprid@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-06-25 20:23:05 +0000

    Some minor cleanup
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/user@16292 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- dasprid/Zend_Toolbar/extension/chrome/Zend_Toolbar/content/zendToolbar.js
+++ dasprid/Zend_Toolbar/extension/chrome/Zend_Toolbar/content/zendToolbar.js
@@ -1,3 +1,61 @@
+var Zend_Toolbar = {};
+
+(function()
+{
+    // Registration
+    var namespaces = [];
+    this.ns = function(fn)
+    {
+        var ns = {};
+        namespaces.push(fn, ns);
+        return ns;
+    };
+    
+    // Initialization
+    this._initialize = function()
+    {
+        for (var i = 0; i < namespaces.length; i += 2) {
+            var fn = namespaces[i];
+            var ns = namespaces[i+1];
+            fn.apply(ns);
+        }
+        
+        Zend_Toolbar.initialize();
+    };
+    
+    // Clean up
+    this._shutdown = function()
+    {
+        Zend_Toolbar.shutdown();
+        
+        window.removeEventListener('load', Zend_Toolbar._initialize, false);
+        window.removeEventListener('unload', Zend_Toolbar._shutdown, false);
+    };
+    
+    // Register handlers to maintain extension life cycle.
+    window.addEventListener('load', Zend_Toolbar._initialize, false);
+    window.addEventListener('unload', Zend_Toolbar._shutdown, false);
+}).apply(Zend_Toolbar);
+
+Zend_Toolbar.LIB = {
+    // Extension singleton shortcut
+    app: Zend_Toolbar,
+
+    // XPCOM shortcuts
+    Cc: Components.classes,
+    Ci: Components.interfaces,
+    Cr: Components.results,
+
+    // In case firebug (tracing console) is not installed (see README)
+    console: {
+        enabled: false,
+        dump: function() {}
+    },
+};
+
+
+
+    
 
 /**
  * Zend_Toolbar Bootstrap
--- dasprid/Zend_Toolbar/extension/defaults/preferences/prefs.js
+++ dasprid/Zend_Toolbar/extension/defaults/preferences/prefs.js
@@ -1,15 +1,3 @@
-/* Preferences sample:
-pref("Zend_Toolbar.prefkey4String", "A string");
-pref("Zend_Toolbar.prefkey4boolean", false);
-pref("Zend_Toolbar.prefkey4number", 0);
-*/
-
-
-
-// Firebug Trace Console
-
-pref("extensions.Zend_Toolbar.alwaysOpenTraceConsole", false);
-
-pref("extensions.Zend_Toolbar.DBG_Temporary", false);
-
-pref("extensions.Zend_Toolbar.DBG_Wildfire", false);
+pref('extensions.Zend_Toolbar.alwaysOpenTraceConsole', false);
+pref('extensions.Zend_Toolbar.DBG_Temporary', false);
+pref('extensions.Zend_Toolbar.DBG_Wildfire', false);
