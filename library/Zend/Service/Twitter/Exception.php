cb89af7ef726b98aa2d955dc0e947d22a2f5f048
yoshida@zend.co.jp <yoshida@zend.co.jp@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-07-16 01:46:45 +0000

    [ZF-7263]Zend_Service_Twitter_Exception extends Exception instead of Zend_Service_Exception
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/release-1.8@16756 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Service/Twitter/Exception.php
+++ library/Zend/Service/Twitter/Exception.php
@@ -15,16 +15,21 @@
  * @category   Zend
  * @package    Zend_Service
  * @subpackage Twitter
- * @copyright  Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright  Copyright (c) 2005-2009 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  * @version    $Id: $
  */
 
 /**
+ * @see Zend_Service_Exception
+ */
+require_once 'Zend/Service/Exception.php';
+
+/**
  * @category   Zend
  * @package    Zend_Service
  * @subpackage Twitter
- * @copyright  Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright  Copyright (c) 2005-2009 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 class Zend_Service_Twitter_Exception extends Zend_Service_Exception
