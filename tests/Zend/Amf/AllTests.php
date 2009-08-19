e217c173bca8ea52a4b69af4220d224ff8cc025b
alexander <alexander@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-08-19 20:06:40 +0000

    Re-apply changes, made by cs-17363 for subset of files with conflicts. Merges cs-17673 into the release branch. [ZF-7581]
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/release-1.9@17681 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/Amf/AllTests.php
+++ tests/Zend/Amf/AllTests.php
@@ -1,5 +1,26 @@
 <?php
 /**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Amf
+ * @subpackage UnitTests
+ * @copyright  Copyright (c) 2005-2009 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ * @version    $Id$
+ */
+
+/**
  * Test helper
  */
 require_once dirname(__FILE__) . '/../../TestHelper.php';
@@ -19,6 +40,14 @@ require_once 'Zend/Amf/AuthTest.php';
 require_once 'Zend/Amf/ResourceTest.php';
 
 
+/**
+ * @category   Zend
+ * @package    Zend_Amf
+ * @subpackage UnitTests
+ * @copyright  Copyright (c) 2005-2009 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ * @group      Zend_Amf
+ */
 class Zend_Amf_AllTests
 {
     public static function main()
@@ -49,3 +78,4 @@ class Zend_Amf_AllTests
 if (PHPUnit_MAIN_METHOD == 'Zend_Amf_AllTests::main') {
     Zend_Amf_AllTests::main();
 }
+
