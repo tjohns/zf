be50b506f43615b8cd125726698c1c21c5b281ef
alexander <alexander@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-08-20 13:33:05 +0000

    Re-apply changes, made by cs-17363 for the rest of files with conflicts. Merges cs-17682 into the release branch. [ZF-7581]
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/release-1.9@17689 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/Tool/AllTests.php
+++ tests/Zend/Tool/AllTests.php
@@ -17,6 +17,7 @@
  * @subpackage UnitTests
  * @copyright  Copyright (c) 2005-2009 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ * @version    $Id $
  */
 
 /**
@@ -37,6 +38,7 @@ require_once 'Zend/Tool/Project/AllTests.php';
  * @subpackage UnitTests
  * @copyright  Copyright (c) 2005-2009 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ * @group      Zend_Tool
  */
 class Zend_Tool_AllTests
 {
