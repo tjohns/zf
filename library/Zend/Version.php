e885e4f5b4a981d8e77f797dfbf29fd0cf6a8b35
alexander <alexander@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-09-21 16:42:22 +0000

    Update ZF version.
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/release-1.9@18344 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Version.php
+++ library/Zend/Version.php
@@ -32,7 +32,7 @@ final class Zend_Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    const VERSION = '1.9.2';
+    const VERSION = '1.9.3';
 
     /**
      * Compare the specified Zend Framework version string $version
