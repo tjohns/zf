86e3efeb3874ee29bd330d5863a4297b79da2387
matthew <matthew@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2010-01-08 15:57:28 +0000

    [RELEASE] Preparing for 1.9.7 release
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/release-1.9@20147 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Version.php
+++ library/Zend/Version.php
@@ -32,7 +32,7 @@ final class Zend_Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    const VERSION = '1.9.6';
+    const VERSION = '1.9.7';
 
     /**
      * Compare the specified Zend Framework version string $version
