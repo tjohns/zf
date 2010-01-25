31b6318adc9fe220a9db16bd5cbc662d1d33ad08
matthew <matthew@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2010-01-25 22:37:59 +0000

    [RELEASE] Updating README.txt and Zend_Version in prep for 1.10.0 release
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/release-1.10@20638 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Version.php
+++ library/Zend/Version.php
@@ -32,7 +32,7 @@ final class Zend_Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    const VERSION = '1.10.0rc1';
+    const VERSION = '1.10.0';
 
     /**
      * Compare the specified Zend Framework version string $version
