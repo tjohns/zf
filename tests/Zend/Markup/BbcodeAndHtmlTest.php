088d9d5f8178f1ec95a1351d1e6367661173d60c
kokx <kokx@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2010-02-06 16:42:04 +0000

    ZF-8916: Merged r20578, r20579 and r20942 to the 1.10 release branch.
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/release-1.10@20945 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/Markup/BbcodeAndHtmlTest.php
+++ tests/Zend/Markup/BbcodeAndHtmlTest.php
@@ -256,6 +256,27 @@ class Zend_Markup_BbcodeAndHtmlTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('<ul><li>blaat</li></ul>', $this->_markup->render('[list][*]blaat[/*][/list]'));
     }
 
+    public function testListDisallowingPlaintext()
+    {
+        $input = "[list]\ntest[*]Foo[/*]\n[/list]";
+        $expected = "<ul><li>Foo</li></ul>";
+        $this->assertEquals($expected, $this->_markup->render($input));
+    }
+
+    public function testFailureAfterCodeTag()
+    {
+        $input = "[code][b][/code][list][*]Foo[/*][/list]";
+        $expected = "<code><span style=\"color: #000000\">\n[b]</span>\n</code><ul><li>Foo</li></ul>";
+        $this->assertEquals($expected, $this->_markup->render($input));
+    }
+
+    public function testInvalidationAfterInvalidTag()
+    {
+        $input = "[b][list][*]Foo[/*][/list][/b]";
+        $expected = "<strong>[list][*]Foo[/*][/list]</strong>";
+        $this->assertEquals($expected, $this->_markup->render($input));
+    }
+
     public function testListTypes()
     {
         $types = array(
