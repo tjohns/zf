82d1399ba872eb8e4c0202e275c880b44bb6ec22
freak <freak@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2010-02-12 01:00:31 +0000

    ZF-9136 merge r21024 to 1.10 release branch
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/release-1.10@21025 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/Application/Resource/MailTest.php
+++ tests/Zend/Application/Resource/MailTest.php
@@ -35,12 +35,6 @@ require_once dirname(__FILE__) . '/../../../TestHelper.php';
 require_once 'Zend/Loader/Autoloader.php';
 
 /**
- * @see Zend_Application_Resource_Mail
- */
-require_once 'Zend/Application/Resource/Mail.php';
-
-
-/**
  * @category   Zend
  * @package    Zend_Application
  * @subpackage UnitTests
@@ -196,10 +190,36 @@ class Zend_Application_Resource_MailTest extends PHPUnit_Framework_TestCase
         $resource->setOptions($options);
 
         $resource->init();
-        $this->assertTrue(Zend_Mail::getDefaultTransport() instanceof Zend_Mail_Transport_Sendmail);    	
+        $this->assertTrue(Zend_Mail::getDefaultTransport() instanceof Zend_Mail_Transport_Sendmail);       
     }
+
+    /**
+     * @group ZF-9136
+     */
+    public function testCustomMailTransportWithFQName() {
+        $options = array('transport' => array('type' => 'Zend_Mail_Transport_Sendmail'));
+        $resource = new Zend_Application_Resource_Mail(array());
+        $resource->setBootstrap($this->bootstrap);
+        $resource->setOptions($options);
+
+        $this->assertTrue($resource->init() instanceof Zend_Mail_Transport_Sendmail);
+    }
+
+    /**
+     * @group ZF-9136
+     */
+    public function testCustomMailTransportWithWrontCasesAsShouldBe() {
+        $options = array('transport' => array('type' => 'Zend_Application_Resource_mailTestCAsE'));
+        $resource = new Zend_Application_Resource_Mail(array());
+        $resource->setBootstrap($this->bootstrap);
+        $resource->setOptions($options);
+
+        $this->assertTrue($resource->init() instanceof Zend_Application_Resource_mailTestCAsE);
+    }
+    
 }
 
-if (PHPUnit_MAIN_METHOD == 'Zend_Application_Resource_LogTest::main') {
-    Zend_Application_Resource_LogTest::main();
+if (PHPUnit_MAIN_METHOD == 'Zend_Application_Resource_MailTest::main') {
+    Zend_Application_Resource_MailTest::main();
 }
+
