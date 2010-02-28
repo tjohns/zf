421aa7a154a068357e11fdaedfcdc1d15d4292cc
mabe <mabe@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2010-02-28 23:00:54 +0000

    mabe user branch: zend cache 2.0: check ttl on get/exists and use ttl option instead of store a copy
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/user@21270 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- mabe/zend_cache-2.0/library/zend/cache/adapter/Variable.php
+++ mabe/zend_cache-2.0/library/zend/cache/adapter/Variable.php
@@ -17,8 +17,7 @@ class Variable extends AdapterAbstract
     {
         $key = $this->_key($key);
         $ns  = isset($options['namespace']) ? $options['namespace'] : '';
-        $ttl = isset($options['ttl']) ? $this->_ttl($options['ttl']) : $this->_ttl;
-        $this->_data[$ns][$key] = array($value, time(), $ttl);
+        $this->_data[$ns][$key] = array($value, time());
         return true;
     }
 
@@ -26,11 +25,10 @@ class Variable extends AdapterAbstract
     {
         $key = $this->_key($key);
         $ns  = isset($options['namespace']) ? $options['namespace'] : '';
-        $ttl = isset($options['ttl']) ? $this->_ttl($options['ttl']) : $this->_ttl;
         if (isset($this->_data[$ns][$key])) {
             throw new \Exception("Key '$key' already exists within namespace '$ns'");
         }
-        $this->_data[$ns][$key] = array($value, time(), $ttl);
+        $this->_data[$ns][$key] = array($value, time());
         return true;
     }
 
@@ -38,50 +36,75 @@ class Variable extends AdapterAbstract
     {
         $key = $this->_key($key);
         $ns  = isset($options['namespace']) ? $options['namespace'] : '';
-        $ttl = isset($options['ttl']) ? $this->_ttl($options['ttl']) : $this->_ttl;
         if (!isset($this->_data[$ns][$key])) {
             throw new \Exception("Key '$key' doen't exists within namespace '$ns'");
         }
-        $this->_data['ns'][$key] = array($value, time(), $ttl);
+        $this->_data['ns'][$key] = array($value, time());
         return true;
     }
-
 
     public function get($key = null, array $options = array())
     {
         $key = $this->_key($key);
         $ns  = isset($options['namespace']) ? $options['namespace'] : '';
-        if (isset($this->_data[$ns][$key])) {
-            // @todo: check if expired
-            return $this->_data[$ns][$key][0];
+
+        if (!isset($this->_data[$ns][$key])) {
+            return false;
         }
-        return false;
+
+        // check if expired
+        if (!isset($options['validate']) || $options['validate']) {
+            $ttl = isset($options['ttl']) ? $this->_ttl($options['ttl']) : $this->getTtl();
+            if ( $ttl && time() > ($this->_data[$ns][$key][1] + $ttl) ) {
+                return false;
+            }
+        }
+            
+        return $this->_data[$ns][$key][0];
     }
 
     public function exists($key = null, array $options = array())
     {
         $key = $this->_key($key);
         $ns  = isset($options['namespace']) ? $options['namespace'] : '';
-        if (isset($this->_data[$ns][$key])) {
-            // @todo: check if expired
-            return true;
+
+        if (!isset($this->_data[$ns][$key])) {
+            return false;
         }
-        return false;
+
+        // check if expired
+        if (!isset($options['validate']) || $options['validate']) {
+            $ttl = isset($options['ttl']) ? $this->_ttl($options['ttl']) : $this->getTtl();
+            if ( $ttl && time() > ($this->_data[$ns][$key][1] + $ttl) ) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public function info($key = null, array $options = array())
     {
         $key = $this->_key($key);
         $ns  = isset($options['namespace']) ? $options['namespace'] : '';
-        if (isset($this->_data[$ns][$key])) {
-            return array(
-                'mtime' => $this->_data[$ns][$key][1],
-                'ttl'   => $this->_data[$ns][$key][2]
-            );
-        }
-        return false;
-    }
 
+        if (!isset($this->_data[$ns][$key])) {
+            return false;
+        }
+
+        // check if expired
+        if (!isset($options['validate']) || $options['validate']) {
+            $ttl = isset($options['ttl']) ? $this->_ttl($options['ttl']) : $this->getTtl();
+            if ( $ttl && time() > ($this->_data[$ns][$key][1] + $ttl) ) {
+                return false;
+            }
+        }
+
+        return array(
+            'mtime' => $this->_data[$ns][$key][1],
+            'ttl'   => isset($options['ttl']) ? $this->_ttl($options['ttl']) : $this->getTtl()
+        );
+    }
 
     public function remove($key = null, array $options = array())
     {
@@ -99,19 +122,16 @@ class Variable extends AdapterAbstract
         return true;
     }
 
-
     public function increment($value, $key = null, array $options = array())
     {
         $key   = $this->_key($key);
         $ns    = isset($options['namespace']) ? $options['namespace'] : '';
-        $ttl   = isset($options['ttl']) ? $this->_ttl($options['ttl']) : $this->_ttl;
         $value = (int)$value;
         if (isset($this->_data[$ns][$key])) {
             $this->_data[$ns][$key][0]+= $value;
             $this->_data[$ns][$key][1] = time();
-            $this->_data[$ns][$key][2] = $ttl;
         } else {
-            $this->_data[$ns][$key] = array($value, time(), $ttl);
+            $this->_data[$ns][$key] = array($value, time());
         }
     }
 
@@ -119,17 +139,14 @@ class Variable extends AdapterAbstract
     {
         $key   = $this->_key($key);
         $ns    = isset($options['namespace']) ? $options['namespace'] : '';
-        $ttl   = isset($options['ttl']) ? $this->_ttl($options['ttl']) : $this->_ttl;
         $value = (int)$value;
         if (isset($this->_data[$ns][$key])) {
             $this->_data[$ns][$key][0]-= $value;
             $this->_data[$ns][$key][1] = time();
-            $this->_data[$ns][$key][2] = $ttl;
         } else {
-            $this->_data[$ns][$key] = array($value, time(), $ttl);
+            $this->_data[$ns][$key] = array($value, time());
         }
     }
-
 
     public function clear($mode, array $options = array())
     {
@@ -153,15 +170,17 @@ class Variable extends AdapterAbstract
             if ( ($mode & \zend\Cache::MATCHING_ALL) != \zend\Cache::MATCHING_ALL
               && ($mode & \zend\Cache::MATCHING_ALL) != 0 ) {
 
+                $ttl = isset($options['ttl']) ? $this->_ttl($options['ttl']) : $this->getTtl();
+
                 // if Zend_Cache::MATCHING_EXPIRED mode selected do not remove active data
                 if (($mode & \zend\Cache::MATCHING_EXPIRED) == \zend\Cache::MATCHING_EXPIRED) {
-                    if ( time() <= ($info[1]+$info[2]) ) {
+                    if ( time() <= ($info[1]+$ttl) ) {
                         continue;
                     }
 
                 // if Zend_Cache::MATCHING_ACTIVE mode selected do not remove expired data
                 } else {
-                    if ( time() > ($info[1]+$info[2]) ) {
+                    if ( time() > ($info[1]+$ttl) ) {
                         continue;
                     }
                 }
@@ -176,19 +195,19 @@ class Variable extends AdapterAbstract
 
                 // if MATCHING_TAGS mode -> check if all given tags available in current cache
                 if (($mode & \zend\Cache::MATCHING_TAGS) == \zend\Cache::MATCHING_TAGS ) {
-                    if (count(array_diff($opts['tags'], $info[3])) > 0) {
+                    if (count(array_diff($opts['tags'], $info[2])) > 0) {
                         continue;
                     }
 
                 // if MATCHING_NO_TAGS mode -> check if no given tag available in current cache
                 } elseif( ($mode & \zend\Cache::MATCHING_NO_TAGS) == \zend\Cache::MATCHING_NO_TAGS ) {
-                    if (count(array_diff($opts['tags'], $info[3])) != count($opts['tags'])) {
+                    if (count(array_diff($opts['tags'], $info[2])) != count($opts['tags'])) {
                         continue;
                     }
 
                 // if MATCHING_ANY_TAGS mode -> check if any given tag available in current cache
                 } elseif ( ($mode & \zend\Cache::MATCHING_ANY_TAGS) == \zend\Cache::MATCHING_ANY_TAGS ) {
-                    if (count(array_diff($opts['tags'], $info[3])) == count($opts['tags'])) {
+                    if (count(array_diff($opts['tags'], $info[2])) == count($opts['tags'])) {
                         continue;
                     }
 
@@ -199,7 +218,7 @@ class Variable extends AdapterAbstract
             // on this time all tag tests match
             ////////////////////////////////////////
 
-            $keys[] = $id;
+            $keys[] = $key;
         }
 
         return  $keys;
