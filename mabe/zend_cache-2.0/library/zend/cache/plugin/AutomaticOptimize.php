1c9d7cb1c5571050448e61c7fdf5d99eb66fd1cb
mabe <mabe@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2010-03-02 19:53:32 +0000

    mabe user branch: zend cache 2.0: automatic optimize
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/user@21297 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- mabe/zend_cache-2.0/library/zend/cache/plugin/AutomaticOptimize.php
+++ mabe/zend_cache-2.0/library/zend/cache/plugin/AutomaticOptimize.php
@@ -1,9 +1,96 @@
 <?php
 
 namespace zend\cache\plugin;
+use \zend\cache\InvalidArgumentAxception as InvalidArgumentAxception;
 
 class AutomaticOptimize extends PluginAbstract
 {
+
+    /**
+     * Automatic optimizing factor
+     *
+     * @var int
+     */
+    protected $_optimizingFactor = 0;
+
+    /**
+     * Get automatic optimizing factor
+     *
+     * @return int
+     */
+    public function getOptimizingFactor()
+    {
+        return $this->_optimizingFactor;
+    }
+
+    /**
+     * Set automatic optimizing factor
+     *
+     * @param int $factor
+     * @return \zend\cache\plugin\AutomaticOptimize
+     */
+    public function setOptimizingFactor($factor)
+    {
+        $factor = (int)$factor;
+        if ($factor < 0) {
+            throw new InvalidArgumentAxception("Invalid optimizing factor '{$factor}': must be greater or equal 0");
+        }
+        $this->_optimizingFactor = $factor;
+
+        return $this;
+    }
+
+    public function remove($key = null, array $options = array())
+    {
+        $ret = $this->remove($key, $options);
+
+        if ($ret === true) {
+            $this->_optimizeByFactor($this->getOptimizingFactor(), $options);
+        }
+
+        return $ret;
+    }
+
+    public function removeMulti(array $keys, array $options = array())
+    {
+        $ret = $this->removeMulti($keys, $options);
+
+        if ($ret === true) {
+            $this->_optimizeByFactor($this->getOptimizingFactor(), $options);
+        }
+
+        return $ret;
+    }
+
+    public function clear($match, array $options = array())
+    {
+        $ret = $this->clear($match, $options);
+
+        if ($ret === true) {
+            $this->_optimizeByFactor($this->getOptimizingFactor(), $options);
+        }
+
+        return $ret;
+    }
+
+    /**
+     * Call optimize by factor
+     *
+     * @param int $factor
+     * @param array $options
+     * @return boolean
+     */
+    protected function _optimizeByFactor($factor, array $options)
+    {
+        if ($factor > 0) {
+            $rand = mt_rand(1, $factor);
+            if ($rand == 1) {
+                return $this->optimize($options);
+            }
+        }
+
+        return true;
+    }
 
     // automatic optimizing support
 
