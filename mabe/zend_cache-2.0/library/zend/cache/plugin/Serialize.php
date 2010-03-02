e11bf3005ce7ab8cbbe0f454cffc92f12cb85900
mabe <mabe@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2010-03-02 11:51:56 +0000

    mabe user branch: zend cache 2.0: serialize plugin
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/user@21294 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- mabe/zend_cache-2.0/library/zend/cache/plugin/Serialize.php
+++ mabe/zend_cache-2.0/library/zend/cache/plugin/Serialize.php
@@ -17,7 +17,7 @@ class Serialize extends PluginAbstract
     public function getSerializer()
     {
         if ($this->_serializer === null) {
-            Serializer::getDefaultAdapter();
+            return Serializer::getDefaultAdapter();
         }
 
         return $this->_serializer;
@@ -28,6 +28,11 @@ class Serialize extends PluginAbstract
         $this->_serializer = $serializer;
     }
 
+    public function resetSerializer()
+    {
+        $this->_serializer = null;
+    }
+
     public function getCapabilities()
     {
         $capabilities = $this->_innerAdapter->getCapabilities();
@@ -35,6 +40,142 @@ class Serialize extends PluginAbstract
         return $capabilities;
     }
 
-    // un-/serialize data
+    public function set($value, $key = null, array $options = array())
+    {
+        $value = $this->getSerializer()->serialize();
+        $this->getAdapter()->set($value, $key, $options);
+    }
+
+    public function setMulti(array $keyValuePairs, array $options = array())
+    {
+        $serializer = $this->getSerializer();
+        foreach ($keyValuePairs as &$value) {
+            $value = $serializer->serialize($value);
+        }
+
+        return $this->getAdapter()->setMulti($keyValuePairs, $options);
+    }
+
+    public function add($value, $key = null, array $options = array())
+    {
+        $value = $this->getSerializer()->serialize();
+        $this->getAdapter()->add($value, $key, $options);
+    }
+
+    public function addMulti(array $keyValuePairs, array $options = array())
+    {
+        $serializer = $this->getSerializer();
+        foreach ($keyValuePairs as &$value) {
+            $value = $serializer->serialize($value);
+        }
+
+        return $this->getAdapter()->addMulti($keyValuePairs, $options);
+    }
+
+    public function replace($value, $key = null, array $options = array())
+    {
+        $value = $this->getSerializer()->serialize();
+        $this->getAdapter()->replace($value, $key, $options);
+    }
+
+    public function replaceMulti(array $keyValuePairs, array $options = array())
+    {
+        $serializer = $this->getSerializer();
+        foreach ($keyValuePairs as &$value) {
+            $value = $serializer->serialize($value);
+        }
+
+        return $this->getAdapter()->replaceMulti($keyValuePairs, $options);
+    }
+
+    public function get($key = null, array $options = array())
+    {
+        $rs = $this->getAdapter()->get($key, $options);
+        return $this->getSerializer()->unserialize($rs);
+    }
+
+    public function getMulti(array $keys, array $options = array())
+    {
+        $rsList = $this->getAdapter()->getMulti($keys, $options);
+
+        $serializer = $this->getSerializer();
+        foreach ($rsList as &$value) {
+            $value = $serializer->unserialize($value);
+        }
+
+        return $rsList;
+    }
+
+    public function increment($value, $key = null, array $options = array())
+    {
+        $adapter    = $this->getAdapter();
+        $serializer = $this->getSerializer();
+        $stored     = $adapter->get($key, $options);
+        $value      = (int)$value;
+
+        if ($stored === false) {
+            $value = $serializer->serialize($value);
+        } else {
+            $stored = $serializer->unserialize($stored);
+            $value  = $serializer->serialize($value + $stored);
+        }
+
+        return $adapter->set($value, $key, $options);
+    }
+
+    public function incrementMulti(array $keyValuePairs, array $options = array())
+    {
+        $adapter    = $this->getAdapter();
+        $serializer = $this->getSerializer();
+
+        $storedList = $adapter->getMulti(array_keys($keyValuePairs), $options);
+        foreach ($keyValuePairs as $key => &$value) {
+            $value = (int)$value;
+            if (!isset($storedList[$key])) {
+                $value = $serializer->serialize($value);
+            } else {
+                $stored = $serializer->unserialize($storedList[$key]);
+                $value  = $serializer->serialize($value + $stored);
+            }
+        }
+
+        return $this->setMulti($keyValuePairs, $options);
+    }
+
+    public function decrement($value, $key = null, array $options = array())
+    {
+        $adapter    = $this->getAdapter();
+        $serializer = $this->getSerializer();
+        $stored     = $adapter->get($key, $options);
+        $value      = (int)$value;
+
+        if ($stored === false) {
+            $value = $serializer->serialize($value);
+        } else {
+            $stored = $serializer->unserialize($stored);
+            $value  = $serializer->serialize($value - $stored);
+        }
+
+        return $adapter->set($value, $key, $options);
+    }
+
+    public function decrementMulti(array $keyValuePairs, array $options = array())
+    {
+        $adapter    = $this->getAdapter();
+        $serializer = $this->getSerializer();
+
+        $storedList = $adapter->getMulti(array_keys($keyValuePairs), $options);
+        foreach ($keyValuePairs as $key => &$value) {
+            $value = (int)$value;
+            if (!isset($storedList[$key])) {
+                $value = $serializer->serialize($value);
+            } else {
+                $stored = $serializer->unserialize($storedList[$key]);
+                $value  = $serializer->serialize($value - $stored);
+            }
+        }
+
+        return $this->setMulti($keyValuePairs, $options);
+    }
 
 }
