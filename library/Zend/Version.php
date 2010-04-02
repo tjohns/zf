a4f72a45ccaf7cd73f458cab9329faea9f5db1cb
matthew <matthew@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2010-04-02 12:05:02 +0000

    Update to next expected mini release version
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/release-1.10@21736 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Version.php
+++ library/Zend/Version.php
@@ -32,7 +32,7 @@ final class Zend_Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    const VERSION = '1.10.2';
+    const VERSION = '1.10.4';
 
     /**
      * Compare the specified Zend Framework version string $version
