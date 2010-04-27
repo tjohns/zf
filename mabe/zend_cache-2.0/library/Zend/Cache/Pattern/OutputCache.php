b618c3937af97d4628b6a95222c99c5cd96fa0c0
mabe <mabe@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2010-04-27 06:20:30 +0000

    user branch: zend cache 2.0:  - implemented pattern OutputCache
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/user@22008 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- mabe/zend_cache-2.0/library/Zend/Cache/Pattern/OutputCache.php
+++ mabe/zend_cache-2.0/library/Zend/Cache/Pattern/OutputCache.php
@@ -1,10 +1,63 @@
 <?php
 
 namespace Zend\Cache\Pattern;
+use \Zend\Cache\RuntimeException;
 
 class OutputCache extends AbstractPattern
 {
 
-    // old Zend_Cache_Frontend_Output
+    protected $_keyStack = array();
+
+    /**
+     * Start output cache
+     *
+     * @param  string  $key      Key
+     * @param  array   $options  Output start options (ttl | validate | output)
+     * @return mixed   True if the cache is hit or if output disabled the cached data, false else
+     * @throws Zend_Cache_Exception
+     */
+    public function start($key, array $options = array())
+    {
+        if (!is_string($key)) {
+            throw new InvalidArgumentException('The key must be a string');
+        }
+
+        $rs = $this->getStorage()->get($key, $options);
+        if ($rs !== false) {
+            if ( !isset($options['output']) || $options['output'] ) {
+                echo $rs;
+                return true;
+            } else {
+                return $rs;
+            }
+        }
+
+        ob_start();
+        ob_implicit_flush(false);
+        $this->_keyStack[] = $key;
+        return false;
+    }
+
+    /**
+     * Stop output cache
+     *
+     * @param  array   $options   Ouput end options (ttl | priority | output | forcedata )
+     * @throws Zend_Cache_Exception
+     */
+    public function end(array $options = array())
+    {
+        $key = array_pop($this->_keyStack);
+        if ($key === null) {
+            throw new RuntimeException('use of end() without a start()');
+        }
+
+        $data = ob_get_contents();
+        ob_end_clean();
+
+        $this->getStorage()->set($key, $data, $options);
+        if (!isset($options['output']) || $options['output']) {
+            echo $data;
+        }
+    }
 
 }
