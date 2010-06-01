02cf0f908f4aa320e805217a5421bb20f50fa930
mabe <mabe@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2010-06-01 23:25:52 +0000

    user branch: zend cache 2.0:  - prototype of CaptureCache
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/user@22348 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- mabe/zend_cache-2.0/library/Zend/Cache/Pattern/CaptureCache.php
+++ mabe/zend_cache-2.0/library/Zend/Cache/Pattern/CaptureCache.php
@@ -6,6 +6,62 @@ namespace Zend\Cache\Pattern;
 class CaptureCache implements PatternInterface
 {
 
+    /**
+     * Public directory
+     *
+     * @var string
+     */
+    protected $_publicDir = null;
+
+    /**
+     * File extension
+     *
+     * @var string
+     */
+    protected $_fileExtension = '.html';
+
+    /**
+     * Lock files on writing
+     *
+     * @var boolean
+     */
+    protected $_fileLocking = true;
+
+    /**
+     * The index filename (without extension)
+     *
+     * @var string
+     */
+    protected $_indexFilename = 'index';
+
+    /**
+     * Page identifier
+     *
+     * @var null|string
+     */
+    protected $_pageId = null;
+
+    /**
+     * Storage for tagging
+     *
+     * @var null|\Zend\Cache\Storage\Adaptable
+     */
+    protected $_tagStorage = null;
+
+    /**
+     * Cache item key to store tags
+     *
+     * @var string
+     */
+    protected $_tagKey = 'ZendCachePatternCaptureCache_Tags';
+
+    /**
+     * Tags
+     *
+     * @var array
+     */
+    protected $_tags = array();
+
     public function __construct($options)
     {
         Options::setConstructorOptions($this, $options);
@@ -19,18 +75,286 @@ class CaptureCache implements PatternInterface
 
     public function getOptions()
     {
-        return array();
+        return array(
+            'publicDir'     => $this->getPublicDir(),
+            'fileExtension' => $this->getFileException(),
+            'fileLocking'   => $this->getFileLocking(),
+            'tagStorage'    => $this->getTagStorage(),
+        );
     }
 
-    /*
-     * old Zend_Cache_Frontend_Capture
-     * and Zend_Cache_Backend_Static
+    public function setPublicDir($dir)
+    {
+        $this->_publicDir = $dir;
+        return $this;
+    }
+
+    public function getPublicDir()
+    {
+        return $this->_publicDir;
+    }
+
+    public function setIndexFilename($filename)
+    {
+        $this->_indexFilename = (string)$filename;
+        return $this;
+    }
+
+    public function getIndexFilename()
+    {
+        return $this->_indexFilename;
+    }
+
+    public function setFileExtension($extension)
+    {
+        $this->_fileExtension = (string)$extension;
+        return $this;
+    }
+
+    public function getFileExtension()
+    {
+        return $this->_fileExtension;
+    }
+
+    public function setFileLocking($flag)
+    {
+        $this->_fileLocking = (boolean)$flag;
+        return $this;
+    }
+
+    public function getFileLocking()
+    {
+        return $this->_fileLocking;
+    }
+
+    /**
+     * Set a storage for tagging
      *
-     * From ZF 1.x docs:
-     * This backend works in concert with Zend_Cache_Frontend_Capture (the two
-     * must be used together) to save the output from requests as static files.
-     * This means the static files are served directly on subsequent requests
-     * without any involvement of PHP or Zend Framework at all.
+     * @param \Zend\Cache\Storage\Adaptable $storage
+     * @return \Zend\Cache\Pattern\CaptureCache
      */
+    public function setTagStorage(Adaptable $storage)
+    {
+        $this->_tagStorage = $storage;
+        return $this;
+    }
+
+    /**
+     * Get the storage for tagging
+     *
+     * @return null|\Zend\Cache\Storage\Adaptable
+     */
+    public function getTagStorage()
+    {
+        return $this->_tagStorage;
+    }
+
+    /**
+     * Set cache item key to store tags
+     *
+     * @param $tagKey string
+     * @return Zend\Cache\Pattern\CaptureCache
+     */
+    public function setTagKey($tagKey)
+    {
+        $tagKey = (string)$tagKey;
+        if (!isset($tagKey[0])) { // strlen($tagKey) == 0
+            throw new InvalidArgumentException("Invalid tag key '{$tagKey}'");
+        }
+
+        $this->_tagKey = $tagKey;
+        return $this;
+    }
+
+    /**
+     * Get cache item key to store tags
+     *
+     * @return string
+     */
+    public function getTagKey()
+    {
+        return $this->_tagKey;
+    }
+
+    /**
+     * Set tags to store
+     *
+     * @param array $tags
+     * @return Zend\Cache\Pattern\CaptureCache
+     */
+    public function setTags(array $tags)
+    {
+        $this->_tags = $tags;
+        return $this;
+    }
+
+    /**
+     * Get tags to store
+     *
+     * @return array
+     */
+    public function getTags()
+    {
+        return $this->_tags;
+    }
+
+    /**
+     * Start the cache
+     *
+     * @param string $pageId  Page identifier
+     * @param array  $options Options
+     * @return boolean false
+     */
+    public function start($pageId = null, array $options = array())
+    {
+        if ($this->_pageId !== null) {
+            throw new RuntimeException("Capturing already stated with page id '{$this->_pageId}'");
+        }
+
+        if (isset($options['tags'])) {
+            $this->setTags($options['tags']);
+            unset($options['tags']);
+        }
+
+        if ($this->getTags() && !$this->getTagStorage()) {
+            throw new RuntimeException('Tags are defined but missing a tag storage');
+        }
+
+        if (!isset($pageId[0])) { // strlen($pageId) == 0
+            $pageId = $this->_detectPageId();
+        }
+
+        ob_start(array($this, '_flush'));
+        ob_implicit_flush(false);
+        $this->_pageId = $pageId;
+
+        return false;
+    }
+
+    public function get(/*TODO*/)
+    {
+        // TODO
+    }
+
+    public function exists(/*TODO*/)
+    {
+        // TODO
+    }
+
+    public function remove($pageId = null, array $options = array())
+    {
+        if (!isset($pageId[0])) {
+            $pageId = $this->_detectPageId();
+        }
+
+        // TODO
+    }
+
+    public function clear(/*TODO*/)
+    {
+        // TODO
+    }
+
+    /**
+     * Determine the page to save from the request
+     *
+     * @return string
+     */
+    protected function _detectPageId()
+    {
+        return $_SERVER['REQUEST_URI'];
+    }
+
+    /**
+     * callback for output buffering
+     *
+     * @param  string $output Buffered output
+     * @return boolean FALSE means original input is sent to the browser.
+     */
+    protected function _flush($output)
+    {
+        $this->_save($output);
+
+        // http://php.net/manual/function.ob-start.php
+        // -> If output_callback  returns FALSE original input is sent to the browser.
+        return false;
+    }
+
+    protected function _save($output)
+    {
+        $fileName = basename($this->_pageId);
+        if ( !isset($fileName[0]) ) { // strlen($fileName) == 0
+            $fileName = $this->getIndexFilename();
+            $pathName = rtrim(realpath($this->getPublicDir()), DIRECTORY_SEPARATOR);
+        } else {
+            $fileName.= $this->getFileExtension();
+            $pathName = rtrim(realpath($this->getPublicDir()), DIRECTORY_SEPARATOR)
+                      . DIRECTORY_SEPARATOR . rtrim(dirname($this->_pageId), '/');
+
+            // convert requested "/" to the valid local directory separator
+            if ('/' != DIRECTORY_SEPARATOR) {
+                str_replace('/', DIRECTORY_SEPARATOR, $pathName);
+            }
+        }
+
+        if (!file_exists($pathName)) {
+            $oldUmask = umask($this->getDirectoryUmask());
+            if (!@mkdir($pathName, 0777, true)) {
+                $lastErr = error_get_last();
+                throw new RuntimeException(
+                    "Can't create directory '{$pathName}': {$lastErr['message']}"
+                );
+            }
+        }
+
+        if ($oldUmask !== null) { // $oldUmask could be set on create directory
+            umask($this->getFileUmask());
+        } else {
+            $oldUmask = umask($this->getFileUmask());
+        }
+        $file = $pathName . DIRECTORY_SEPARATOR . $fileName;
+        $this->_putFileContent($file, $output);
+
+        $tagStorage = $this->getTagStorage();
+        if ($tagStorage) {
+            $tagKey     = $this->getTagKey();
+            $tagIndex = $tagStorage->getTagStorage()->get($tagKey);
+            if (!$tagIndex) {
+                $tagIndex = null;
+            }
+
+            if ($this->_tags) {
+                $tagIndex[$file] = &$this->_tags;
+            } elseif ($tagIndex) {
+                unset($tagIndex[$file]);
+            }
+
+            if ($tagIndex !== null) {
+                $this->getTagStorage()->set($tagIndex, $tagKey);
+            }
+        }
+    }
+
+    /**
+     * Write content to a file
+     *
+     * @param  string $file  File complete path
+     * @param  string $data  Data to write
+     * @throws RuntimeException
+     */
+    protected function _putFileContent($file, $data)
+    {
+        $flags = FILE_BINARY; // since PHP 6 but defined as 0 in PHP 5.3
+        if ($this->getFileLocking()) {
+            $flags = $flags | LOCK_EX;
+        }
+
+        $put = @file_put_contents($file, $data, $flags);
+        if ( $put < strlen((binary)$data) ) {
+            $lastErr = error_get_last();
+            @unlink($file); // remove old or incomplete written file
+            throw new RuntimeException($lastErr['message']);
+        }
+    }
 
 }
